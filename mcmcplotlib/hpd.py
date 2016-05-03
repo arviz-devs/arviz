4b06454a51701951d773dabb94ed9d34a9402660
aloctavodia <aloctavodia@gmail.com>
2016-05-03 10:52:51 -0300

    cleaning and minor modifications

---
+++ mcmcplotlib/hpd.py
@@ -0,0 +1,59 @@
+from __future__ import division
+import numpy as np
+import scipy.stats.kde as kde
+
+def hpd_grid(sample, alpha=0.05, roundto=2):
+    """Calculate highest posterior density (HPD) of array for given alpha. 
+    The HPD is the minimum width Bayesian credible interval (BCI). 
+    The function works for multimodal distributions, returning more than one mode
+
+    Parameters
+    ----------
+    
+    sample : Numpy array or python list
+        An array containing MCMC samples
+    alpha : float
+        Desired probability of type I error (defaults to 0.05)
+    roundto: integer
+        Number of digits after the decimal point for the results
+
+    Returns
+    ----------
+    hpd: array with the lower 
+          
+    """
+    sample = np.asarray(sample)
+    sample = sample[~np.isnan(sample)]
+    # get upper and lower bounds
+    l = np.min(sample)
+    u = np.max(sample)
+    density = kde.gaussian_kde(sample)
+    x = np.linspace(l, u, 2000)
+    y = density.evaluate(x)
+    #y = density.evaluate(x, l, u) waitting for PR to be accepted
+    xy_zipped = zip(x, y/np.sum(y))
+    xy = sorted(xy_zipped, key=lambda x: x[1], reverse=True)
+    xy_cum_sum = 0
+    hdv = []
+    for val in xy:
+        xy_cum_sum += val[1]
+        hdv.append(val[0])
+        if xy_cum_sum >= (1-alpha):
+            break
+    hdv.sort()
+    diff = (u-l)/20  # differences of 5%
+    hpd = []
+    hpd.append(round(min(hdv), roundto))
+    for i in range(1, len(hdv)):
+        if hdv[i]-hdv[i-1] >= diff:
+            hpd.append(round(hdv[i-1], roundto))
+            hpd.append(round(hdv[i], roundto))
+    hpd.append(round(max(hdv), roundto))
+    ite = iter(hpd)
+    hpd = list(zip(ite, ite))
+    modes = []
+    for value in hpd:
+         x_hpd = x[(x > value[0]) & (x < value[1])]
+         y_hpd = y[(x > value[0]) & (x < value[1])]
+         modes.append(round(x_hpd[np.argmax(y_hpd)], roundto))
+    return hpd, x, y, modes
--- mcmcplotlib/plot_post.py
+++ mcmcplotlib/plot_post.py
@@ -2,80 +2,97 @@ from __future__ import division
 import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
-from hdi import hdi_grid
+from hpd import hpd_grid
 
 
-def plot_post(param_sample_vec, cred_mass=0.95, comp_val=None,
-              ROPE=None, ylab='', xlab='parameter', fontsize=14, labelsize=14,
-              title='', framealpha=1, show_mode=True, bins=50, kde_plot=True, 
-              roundto=2):
-    """
-    Write me!
-    """        
-    # colors taken from the default seaborn color pallete
-    blue, green, red = [(0.2980392156862745, 0.4470588235294118, 
-    0.6901960784313725), (0.3333333333333333, 0.6588235294117647, 
-    0.40784313725490196), (0.7686274509803922, 0.3058823529411765, 
-    0.3215686274509804)]
+def plot_post(sample, alpha=0.05, show_mode=True, kde_plot=True, bins=50, 
+    ROPE=None, comp_val=None, roundto=2):
+    """Plot posterior and HPD
 
-    post_summary = {'mean':0,'median':0,'mode':0, 'hdi_mass':0,'hdi_low':0,
-                   'hdi_high':0, 'comp_val':0, 'pc_gt_comp_val':0, 'ROPE_low':0,
+    Parameters
+    ----------
+
+    sample : Numpy array or python list
+        An array containing MCMC samples
+    alpha : float
+        Desired probability of type I error (defaults to 0.05)
+    show_mode: Bool
+        If True the legend will show the mode(s) value(s), if false the mean(s)
+        will be displayed
+    kde_plot: Bool
+        If True the posterior will be displayed using a Kernel Density Estimation
+        otherwise an histogram will be used
+    bins: integer
+        Number of bins used for the histogram, only works when kde_plot is False
+    ROPE: list or numpy array
+        Lower and upper values of the Region Of Practical Equivalence
+    comp_val: float
+        Comparison value
+        
+
+    Returns
+    -------
+
+    post_summary : dictionary
+        Containing values with several summary statistics
+
+    """       
+
+    post_summary = {'mean':0,'median':0,'mode':0, 'alpha':0,'hpd_low':0,
+                   'hpd_high':0, 'comp_val':0, 'pc_gt_comp_val':0, 'ROPE_low':0,
                    'ROPE_high':0, 'pc_in_ROPE':0}
 
-    post_summary['mean'] = round(np.mean(param_sample_vec), roundto)
-    post_summary['median'] = round(np.median(param_sample_vec), roundto)
-    post_summary['hdi_mass'] = cred_mass
+    post_summary['mean'] = round(np.mean(sample), roundto)
+    post_summary['median'] = round(np.median(sample), roundto)
+    post_summary['alpha'] = alpha
 
-    # Compute the HDI, KDE and mode for the posterior
-    hdi, x, y, modes = hdi_grid(param_sample_vec, cred_mass, roundto)
-    post_summary['hdi'] = hdi
+    # Compute the hpd, KDE and mode for the posterior
+    hpd, x, y, modes = hpd_grid(sample, alpha, roundto)
+    post_summary['hpd'] = hpd
     post_summary['mode'] = modes
 
     ## Plot KDE.
     if kde_plot:
-            plt.plot(x, y, color=blue)
+            plt.plot(x, y, color='k', lw=2)
     ## Plot histogram.
     else:
-        plt.hist(param_sample_vec, normed=True, bins=bins, facecolor=blue, 
+        plt.hist(sample, normed=True, bins=bins, facecolor='b', 
         edgecolor='w')
 
-    ## Display mean or mode:
+    ## Display mode or mean:
     if show_mode:
         string = '{:g} ' * len(post_summary['mode'])
         plt.plot(0, label='mode =' + string.format(*post_summary['mode']), alpha=0)
     else:
         plt.plot(0, label='mean = {:g}'.format(post_summary['mean']), alpha=0)
-    ## Display the comparison value.
-    if comp_val is not None:
-        pc_gt_comp_val = round(100 * np.sum(param_sample_vec > comp_val)/len(param_sample_vec), roundto)
-        pc_lt_comp_val = round(100 - pc_gt_comp_val, roundto)
-        plt.axvline(comp_val, ymax=.75, color=green,
-                 linestyle='--', linewidth=4,
-                 label='{:g}% < {:g} < {:g}%'.format(pc_lt_comp_val, comp_val, pc_gt_comp_val))
-        post_summary['comp_val'] = comp_val
-        post_summary['pc_gt_comp_val'] = pc_gt_comp_val
+
+    ## Display the hpd.
+    hpd_label = ''
+    for value in hpd:
+        plt.plot(value, [0, 0], linewidth=10, color='b')
+        hpd_label = hpd_label +  '{:g} {:g}\n'.format(round(value[0], roundto), round(value[1], roundto)) 
+    plt.plot(0, 0, linewidth=4, color='b', label='hpd {:g}%\n{}'.format((1-alpha)*100, hpd_label))
     ## Display the ROPE.
     if ROPE is not None:
-        pc_in_ROPE = round(np.sum((param_sample_vec > ROPE[0]) & (param_sample_vec < ROPE[1]))/len(param_sample_vec)*100, roundto)
-        plt.axvline(ROPE[0], ymax=.75, color=red, linewidth=4,
-                label='{:g}% in ROPE'.format(pc_in_ROPE))
-        plt.axvline(ROPE[1], ymax=.75, color=red, linewidth=4)
+        pc_in_ROPE = round(np.sum((sample > ROPE[0]) & (sample < ROPE[1]))/len(sample)*100, roundto)
+        plt.plot(ROPE, [0, 0], linewidth=20, color='r', alpha=0.75)
+        plt.plot(0, 0, linewidth=4, color='r', label='{:g}% in ROPE'.format(pc_in_ROPE))
         post_summary['ROPE_low'] = ROPE[0] 
         post_summary['ROPE_high'] = ROPE[1] 
         post_summary['pc_in_ROPE'] = pc_in_ROPE
+    ## Display the comparison value.
+    if comp_val is not None:
+        pc_gt_comp_val = round(100 * np.sum(sample > comp_val)/len(sample), roundto)
+        pc_lt_comp_val = round(100 - pc_gt_comp_val, roundto)
+        plt.axvline(comp_val, ymax=.75, color='g', linewidth=4, alpha=0.75,
+            label='{:g}% < {:g} < {:g}%'.format(pc_lt_comp_val, 
+                                                comp_val, pc_gt_comp_val))
+        post_summary['comp_val'] = comp_val
+        post_summary['pc_gt_comp_val'] = pc_gt_comp_val
 
-    ## Display the HDI.
-    hdi_label = ''
-    for value in hdi:
-        plt.plot(value, [0, 0], linewidth=8, color='k')
-        hdi_label = hdi_label +  '{:g} {:g}\n'.format(round(value[0], roundto), round(value[1], roundto)) 
-    plt.plot(0, 0, linewidth=8, color='k', label='HDI {:g}%\n{}'.format(cred_mass*100, hdi_label))
-
-    plt.xlabel(xlab, fontsize=fontsize)
-    plt.ylabel(ylab, fontsize=fontsize)
-    plt.title(title, fontsize=fontsize)
-    plt.legend(loc=0, fontsize=labelsize, framealpha=framealpha)
+    plt.legend(loc=0, framealpha=1)
     frame = plt.gca()
     frame.axes.get_yaxis().set_ticks([])
     return post_summary
+    
 
