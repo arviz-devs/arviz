88426c88c679d5207d681656e87149cd9e9e81dc
Volodymyr Kazantsev <vladimir_kazantsev@productmadness.com>
2016-05-16 21:54:37 +0100

    setup.py added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='mcmcplotlib',
+    version='0.0.1',
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'scipy'
+    ],
+)
