29b816d5a9b7800d5c15b94bf1e3e5e760d58b50
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2017-03-18 00:08:06 +0200

    Update plot_post.py
    fix problems with `{:.Ng}` --> `"{:g}".format(round(x, N))`. This forces N digits after decimal point.

--- mcmcplotlib/plot_post.py
+++ mcmcplotlib/plot_post.py
@@ -61,22 +61,22 @@ def plot_post(sample, alpha=0.05, show_mode=True, kde_plot=True, bins=50,
 
     ## Display mode or mean:
     if show_mode:
-        string = '{:.{roundto}g} ' * len(post_summary['mode'])
-        plt.plot(0, label='mode =' + string.format(*post_summary['mode'], roundto=roundto).rstrip(), alpha=0)
+        string = '{:g} ' * len(post_summary['mode'])
+        plt.plot(0, label='mode =' + string.format(*[round(item, roundto) for item in post_summary['mode']]).rstrip(), alpha=0)
     else:
-        plt.plot(0, label='mean = {:.{roundto}g}'.format(post_summary['mean'], roundto=roundto), alpha=0)
+        plt.plot(0, label='mean = {:g}'.format(round(post_summary['mean'], roundto)), alpha=0)
 
     ## Display the hpd.
     hpd_label = ''
     for value in hpd:
         plt.plot(value, [0, 0], linewidth=10, color='b')
-        hpd_label = hpd_label +  '{:.{roundto}g} {:.{roundto}g}'.format(value[0], value[1], roundto=roundto) 
-    plt.plot(0, 0, linewidth=4, color='b', label='hpd {:.{roundto}g}%\n{}'.format((1-alpha)*100, hpd_label, roundto=roundto))
+        hpd_label = hpd_label +  '{:g} {:g}'.format(round(value[0], roundto), round(value[1], roundto)) 
+    plt.plot(0, 0, linewidth=4, color='b', label='hpd {:g}%\n{}'.format(round((1-alpha)*100, roundto), hpd_label))
     ## Display the ROPE.
     if ROPE is not None:
         pc_in_ROPE = np.sum((sample > ROPE[0]) & (sample < ROPE[1]))/len(sample)*100
         plt.plot(ROPE, [0, 0], linewidth=20, color='r', alpha=0.75)
-        plt.plot(0, 0, linewidth=4, color='r', label='{:.{roundto}g}% in ROPE'.format(pc_in_ROPE, roundto=roundto))
+        plt.plot(0, 0, linewidth=4, color='r', label='{:g}% in ROPE'.format(round(pc_in_ROPE, roundto)))
         post_summary['ROPE_low'] = ROPE[0] 
         post_summary['ROPE_high'] = ROPE[1] 
         post_summary['pc_in_ROPE'] = pc_in_ROPE
@@ -85,9 +85,9 @@ def plot_post(sample, alpha=0.05, show_mode=True, kde_plot=True, bins=50,
         pc_gt_comp_val = 100 * np.sum(sample > comp_val)/len(sample)
         pc_lt_comp_val = 100 - pc_gt_comp_val
         plt.axvline(comp_val, ymax=.75, color='g', linewidth=4, alpha=0.75,
-            label='{:.{roundto}g}% < {:.{roundto}g} < {:.{roundto}g}%'.format(pc_lt_comp_val, 
-                                                                              comp_val, pc_gt_comp_val, 
-                                                                              roundto=roundto))
+            label='{:g}% < {:g} < {:g}%'.format(round(pc_lt_comp_val, roundto), 
+                                                round(comp_val, roundto),
+                                                round(pc_gt_comp_val, roundto)))
         post_summary['comp_val'] = comp_val
         post_summary['pc_gt_comp_val'] = pc_gt_comp_val
 
