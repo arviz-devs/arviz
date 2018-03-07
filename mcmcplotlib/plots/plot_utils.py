2e86cb9ba9e132ef6b0c16a89adbadae43ed048d
aloctavodia <aloctavodia@gmail.com>
2018-03-07 10:57:32 -0300

    add traceplot and helper functions

---
+++ mcmcplotlib/plots/plot_utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def identity_transform(x):
+    """f(x) = x"""
+    return x
+
+
+def get_axis(ax, default_rows, default_columns, **default_kwargs):
+    """Verifies the provided axis is of the correct shape, and creates one if needed.
+
+    Args:
+        ax: matplotlib axis or None
+        default_rows: int, expected rows in axis
+        default_columns: int, expected columns in axis
+        **default_kwargs: keyword arguments to pass to plt.subplot
+
+    Returns:
+        axis, or raises an error
+    """
+
+    default_shape = (default_rows, default_columns)
+    if ax is None:
+        _, ax = plt.subplots(*default_shape, **default_kwargs)
+    elif ax.shape != default_shape:
+        raise ValueError('Subplots with shape %r required' % (default_shape,))
+    return ax
+
+
+def make_2d(a):
+    """Ravel the dimensions after the first."""
+    a = np.atleast_2d(a.T).T
+    # flatten out dimensions beyond the first
+    n = a.shape[0]
+    newshape = np.product(a.shape[1:]).astype(int)
+    a = a.reshape((n, newshape), order='F')
+    return a
---
+++ mcmcplotlib/utils/utils.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+
+
+def trace_to_dataframe(trace, combined=True):
+    """Convert trace to Pandas DataFrame.
+
+    Parameters
+    ----------
+    trace : trace
+        At this point it only supports PyMC3's MultiTrace Object
+    combined : lalala
+    """
+    if type(trace).__name__ == 'MultiTrace':
+
+        var_shapes = trace._straces[0].var_shapes
+        varnames = var_shapes.keys()
+
+        flat_names = {v: _create_flat_names(
+            v, var_shapes[v]) for v in varnames}
+
+        var_dfs = []
+        for v in varnames:
+            if combined:
+                vals = trace.get_values(v, combine=combined)
+                flat_vals = vals.reshape(vals.shape[0], -1)
+                var_dfs.append(pd.DataFrame(flat_vals, columns=flat_names[v]))
+            else:
+                vals = trace.get_values(v, combine=combined)
+                for va in vals:
+                    flat_vals = va.reshape(va.shape[0], -1)
+                    var_dfs.append(pd.DataFrame(
+                        flat_vals, columns=flat_names[v]))
+
+    elif isinstance == pd.DataFrame:
+        return trace
+
+    else:
+        raise ValueError('The trace object is not supported')
+
+    return pd.concat(var_dfs, axis=1)
+
+
+def _create_flat_names(varname, shape):
+    """Return flat variable names for `varname` of `shape`.
+    Examples
+    --------
+    >>> create_flat_names('x', (5,))
+    ['x__0', 'x__1', 'x__2', 'x__3', 'x__4']
+    >>> create_flat_names('x', (2, 2))
+    ['x__0_0', 'x__0_1', 'x__1_0', 'x__1_1']
+    """
+    if not shape:
+        return [varname]
+    labels = (np.ravel(xs).tolist() for xs in np.indices(shape))
+    labels = (map(str, xs) for xs in labels)
+    return ['{}__{}'.format(varname, '_'.join(idxs)) for idxs in zip(*labels)]
+
+
+def expand_variable_names(trace, varnames):
+    """
+    expand the name of variables to include multidimensional variables
+    """
+    tmp = []
+    for vtrace in pd.unique(trace.columns):
+        for v in varnames:
+            if '{}__'.format(v) in vtrace or v in vtrace:
+                tmp.append(vtrace)
+    return np.unique(tmp)
