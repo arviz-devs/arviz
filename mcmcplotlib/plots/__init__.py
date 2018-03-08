7cc796dbaab3373040f9e762468822045cb57993
Osvaldo Martin <aloctavodia@gmail.com>
2018-03-08 12:11:11 -0300

    Energyplot (#20)
    * add energyplot
    
    * add energyplot

--- mcmcplotlib/plots/__init__.py
+++ mcmcplotlib/plots/__init__.py
@@ -1,3 +1,4 @@
 from .kdeplot import kdeplot
 from .densityplot import densityplot
 from .traceplot import traceplot
+from .energyplot import energyplot
--- mcmcplotlib/tests/test_plots.py
+++ mcmcplotlib/tests/test_plots.py
@@ -1,10 +1,12 @@
 from pandas import DataFrame
 import numpy as np
-from ..plots import densityplot, traceplot
+from ..plots import densityplot, traceplot, energyplot
 
 
 def test_plots():
     trace = DataFrame({'a': np.random.rand(100)})
+    trace_energy = DataFrame({'energy': np.random.rand(100)})
 
     densityplot(trace)
     traceplot(trace)
+    energyplot(trace_energy)
