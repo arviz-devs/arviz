9ba8305e63f94c398e3d44de4963b1faa4d8568d
aloctavodia <aloctavodia@gmail.com>
2018-03-17 12:02:24 -0300

    add bfmi

--- mcmcplotlib/stats/__init__.py
+++ mcmcplotlib/stats/__init__.py
@@ -1,2 +1,2 @@
-from .stats import hpd, r2_score, summary
-from .diagnostics import gelman_rubin, effective_n, geweke
+from .stats import bfmi, hpd, r2_score, summary
+from .diagnostics import effective_n, gelman_rubin, geweke
