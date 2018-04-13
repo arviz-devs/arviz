46f082531d5d59d3f3a4befe7e6c9973ca862860
aloctavodia <aloctavodia@gmail.com>
2018-04-13 10:04:59 -0300

    add numpydoc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
     'matplotlib.sphinxext.only_directives',
-    'matplotlib.sphinxext.plot_directive']
+    'matplotlib.sphinxext.plot_directive',
+    'numpydoc']
 
 
 # Add any paths that contain templates here, relative to this directory.
