6d8ea64253c10f226de0b014162f8f4c2a57b66e
Osvaldo Martin <aloctavodia@gmail.com>
2018-04-25 12:37:05 -0300

    copy styles with pip (#60)
    * copy install works with pip
    
    * update darkgrid and new style, whitegrid

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
+from setuptools.command.develop import develop
+import sys
+import shutil
+import os
+from matplotlib import get_configdir
 
+
+def copy_styles():
+    sd = os.path.join(get_configdir(), "stylelib")
+    source = os.path.dirname(os.path.realpath(__file__))
+    lsd = os.path.join(source, 'arviz', 'plots', 'styles')
+    styles = [f for f in os.listdir(lsd)]
+    if not os.path.isdir(sd):
+        os.makedirs(sd)
+    for s in styles:
+        shutil.copy(os.path.join(lsd, s), os.path.join(sd, s))
+
+class DevelopStyles(develop):
+    def run(self):
+        copy_styles()
+        develop.run(self)
 
 class InstallStyles(install):
     def run(self):
-        import sys
-        import shutil
-        import os
-        from matplotlib import get_configdir
-
-        sd = os.path.dirname(os.path.realpath(__file__))
-        lsd = os.path.join(sd, 'arviz', 'plots', 'styles')
-        styles = [f for f in os.listdir(lsd)]
-        if not os.path.isdir(sd):
-            os.makedirs(sd)
-        for s in styles:
-            shutil.copy(os.path.join(lsd, s), os.path.join(sd, s))
+        copy_styles()
         install.run(self)
 
 
@@ -31,5 +40,8 @@ setup(
                       'scipy',
                       'pandas'],
     include_package_data=True,
-    cmdclass={'install': InstallStyles},
+    cmdclass={
+        'develop': DevelopStyles,
+        'install': InstallStyles,
+    },
 )
