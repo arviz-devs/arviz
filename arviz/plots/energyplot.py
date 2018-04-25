e0caa90af509101d0d4bb64ab8c6f89aac8ce535
aloctavodia <aloctavodia@gmail.com>
2018-04-25 15:30:38 -0300

    use tuple for color_shade and shade, remove frame

--- arviz/plots/energyplot.py
+++ arviz/plots/energyplot.py
@@ -4,8 +4,8 @@ from .kdeplot import kdeplot
 from ..utils.utils import get_stats
 
 
-def energyplot(trace, kind='kde', figsize=None, legend=True, shade=None, color_shade=None, bw=4.5,
-               frame=True, kwargs_shade=None, ax=None, **kwargs):
+def energyplot(trace, kind='kde', figsize=None, legend=True, shade=(1, .75),
+               color_shade=('C0', 'C7'), bw=4.5, kwargs_shade=None, ax=None, **kwargs):
     """Plot energy transition distribution and marginal energy distribution in
     order to diagnose poor exploration by HMC algorithms.
 
@@ -30,8 +30,6 @@ def energyplot(trace, kind='kde', figsize=None, legend=True, shade=None, color_s
         Bandwidth scaling factor for the KDE. Should be larger than 0. The higher this number the
         smoother the KDE will be. Defaults to 4.5 which is essentially the same as the Scott's rule
         of thumb (the default rule used by SciPy). Only works if `kind='kde'`
-    frame : bool
-        Flag for plotting frame around figure
     ax : axes
         Matplotlib axes.
     kwargs_shade : dicts, optional
@@ -43,12 +41,6 @@ def energyplot(trace, kind='kde', figsize=None, legend=True, shade=None, color_s
     """
 
     energy = get_stats(trace, 'energy')
-
-    if color_shade is None:
-        color_shade = ('C0', 'C7')
-
-    if shade is None:
-        shade = (1, .75)
 
     if figsize is None:
         figsize = (8, 6)
@@ -77,10 +69,6 @@ def energyplot(trace, kind='kde', figsize=None, legend=True, shade=None, color_s
 
     ax.set_xticks([])
     ax.set_yticks([])
-
-    if not frame:
-        for spine in ax.spines.values():
-            spine.set_visible(False)
 
     if legend:
         ax.legend()
