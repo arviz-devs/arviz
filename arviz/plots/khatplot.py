5e6418616c5db184a05bd242621f79088f885a3b
Agustina Arroyuelo <agustinaarroyuelo@gmail.com>
2018-05-15 11:12:34 -0300

    Additional diagnostics for loo (#77)
    * additional diagnostics for loo
    
    * added textsize parameters for khatplot
    
    * added warning messages
    
    * changed kappa symbol

---
+++ arviz/plots/khatplot.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from .plot_utils import _scale_text
+
+
+def khatplot(ks, figsize=None, textsize=None, ax=None, hlines_kwargs=None, **kwargs):
+    R"""
+    Plot Paretto tail indices.
+
+    Parameters
+    ----------
+    ks : array
+      Paretto tail indices.
+    figsize : figure size tuple
+      If None, size is (8, 4)
+    textsize: int
+      Text size for labels. If None it will be autoscaled based on figsize.
+    ax: axes
+      Matplotlib axes
+    hlines_kwargs: dictionary
+      Aditional keywords passed to ax.hlines
+
+    Returns
+    -------
+    ax : axes
+      Matplotlib axes.
+    """
+
+    if figsize is None:
+        figsize = (8, 5)
+
+    if hlines_kwargs is None:
+        hlines_kwargs = {}
+
+    textsize, linewidth, ms = _scale_text(figsize, textsize=textsize)
+
+    if ax is None:
+        _, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.hlines([0, .5, .7, 1], xmin=-1, xmax=len(ks)+1,
+              alpha=.25, **hlines_kwargs)
+
+    alphas = .5 + .5*(ks > .5)
+    rgba_c = np.zeros((len(ks), 4))
+    rgba_c[:, 2] = .8
+    rgba_c[:, 3] = alphas
+    ax.scatter(np.arange(len(ks)), ks, c=rgba_c, marker='+', linewidth=ms)
+    ax.set_xlabel('Data point', fontsize=textsize)
+    ax.set_ylabel(r'Shape parameter κ', fontsize=textsize)
+    ax.tick_params(labelsize=textsize)
+    return ax
