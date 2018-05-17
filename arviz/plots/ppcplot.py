46a70446ad9c9b0bce9950ed2565b58063201d15
aloctavodia <aloctavodia@gmail.com>
2018-05-17 15:16:24 -0300

    fix zorder

---
+++ arviz/plots/ppcplot.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from .kdeplot import kdeplot
+from .plot_utils import _scale_text, _create_axes_grid
+
+
+def ppcplot(data, ppc_sample, kind='kde', mean=True, figsize=None, textsize=None, ax=None):
+    """
+    Plot for Posterior Predictive Checks
+
+    Parameters
+    ----------
+    data : Array-like
+        Observed values
+    ppc_samples : dict
+        Posterior predictive check samples
+    kind : str
+        Type of plot to display (kde or cumulative)
+    mean : bool
+        Whether or not to plot the mean ppc distribution. Defaults to True
+    figsize : figure size tuple
+        If None, size is (6, 5)
+    textsize: int
+        Text size for labels. If None it will be auto-scaled based on figsize.
+    ax: axes
+        Matplotlib axes
+
+    Returns
+    -------
+    ax : matplotlib axes
+    """
+    if figsize is None:
+        figsize = (6, 5)
+
+    if ax is None:
+        _, ax = _create_axes_grid(figsize, ppc_sample)
+
+    textsize, linewidth, _ = _scale_text(figsize, textsize)
+
+    for ax_, (var, ppss) in zip(np.atleast_1d(ax), ppc_sample.items()):
+        if kind == 'kde':
+            kdeplot(data, color='k', lw=linewidth, label='{}'.format(var), ax=ax_, zorder=3)
+            for pps in ppss:
+                kdeplot(pps, alpha=0.2, color='C5', lw=linewidth, ax=ax_)
+            ax_.plot([], color='C5', label='{}_pps'.format(var))
+            if mean:
+                kdeplot(ppss, color='C0', ls='--', lw=linewidth, label='mean {}_pps'.format(var),
+                        ax=ax_)
+            ax_.set_xlabel(var, fontsize=textsize)
+            ax_.set_yticks([])
+
+        elif kind == 'cumulative':
+            ax_.plot(*_ecdf(data), color='k', lw=linewidth, label='{}'.format(var), zorder=3)
+            for pps in ppss:
+                ax_.plot(*_ecdf(pps), alpha=0.2, color='C5', lw=linewidth)
+            ax_.plot([], color='C5', label='{}_pps'.format(var))
+            if mean:
+                ax_.plot(*_ecdf(ppss.flatten()), color='C0', ls='--', lw=linewidth,
+                         label='mean {}_pps'.format(var))
+            ax_.set_xlabel(var, fontsize=textsize)
+            ax_.set_yticks([0, 0.5, 1])
+        ax_.legend(fontsize=textsize)
+        ax_.tick_params(labelsize=textsize)
+
+    return ax
+
+
+def _ecdf(data):
+    len_data = len(data)
+    data_s = np.sort(data)
+    cdf = np.arange(1, len_data + 1) / len_data
+    return data_s, cdf
