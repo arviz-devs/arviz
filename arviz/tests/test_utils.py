905fb5d91e10a968e02b761751ed7479f6d4a763
Colin Carroll <colcarroll@gmail.com>
2018-08-11 21:27:14 +0200

    reorder imports

--- arviz/tests/test_utils.py
+++ arviz/tests/test_utils.py
@@ -37,7 +37,7 @@ class TestUtils():
     def test_save_and_load(self, tmpdir_factory):
         filename = tmpdir_factory.mktemp('traces').join('trace.gzip')
         save_trace(self.trace, filename=filename)
-        trl0 = load_trace(filename)
+        trl0 = load_trace(str(filename))
         tr = trace_to_dataframe(self.trace, combined=False)
         save_trace(tr, filename=filename)
         trl1 = load_trace(filename)
