1110406e9752c68ff502acfc4e1fdafb152c54d1
Colin Carroll <colcarroll@gmail.com>
2018-08-12 10:05:34 +0200

    Make old python compatible

--- arviz/tests/test_utils.py
+++ arviz/tests/test_utils.py
@@ -35,9 +35,9 @@ class TestUtils():
         assert_equal(self.trace['b'][1000:], df_fc['b'].iloc[:, 1])
 
     def test_save_and_load(self, tmpdir_factory):
-        filename = tmpdir_factory.mktemp('traces').join('trace.gzip')
+        filename = str(tmpdir_factory.mktemp('traces').join('trace.gzip'))
         save_trace(self.trace, filename=filename)
-        trl0 = load_trace(str(filename))
+        trl0 = load_trace(filename)
         tr = trace_to_dataframe(self.trace, combined=False)
         save_trace(tr, filename=filename)
         trl1 = load_trace(filename)
