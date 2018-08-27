53e17ad4200e75dd59172dbcc6effb8ea8fd81c6
canyon <ravinsdrive@gmail.com>
2018-08-26 17:31:57 -0700

    Add violinplot example to gallery

---
+++ examples/violinplot.py
@@ -0,0 +1,13 @@
+"""
+Violinplot
+=========
+
+_thumb: .2, .8
+"""
+import arviz as az
+
+az.style.use('arviz-darkgrid')
+
+non_centered = az.load_arviz_data('non_centered_eight')
+az.violintraceplot(non_centered, var_names=["mu", "tau"], textsize=8)
+
