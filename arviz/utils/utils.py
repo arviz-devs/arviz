6c5b4e8e5c2c4e4f485ed5e46f18d18ba9ec136c
Colin Carroll <colcarroll@gmail.com>
2018-09-09 19:31:15 -0400

    Remove radon example for now

--- arviz/utils/utils.py
+++ arviz/utils/utils.py
@@ -386,16 +386,6 @@ def load_arviz_data(dataset):
             ''',
             'path': os.path.join(data_path, 'non_centered_eight.nc')
         },
-        'radon': {
-            'description': '''
-                Contextual effects model for radon contamination in Minnesota, from
-                Gelman and Hill (2006), adapted from a PyMC3 example by Chris
-                Fonnesbeck (https://docs.pymc.io/notebooks/multilevel_modeling.html).
-                Four chains, 500 draws each, fit with NUTS in PyMC3. Features county
-                names as coordinates.
-            ''',
-            'path': os.path.join(data_path, 'radon.nc')
-        },
     }
     if dataset in datasets_available:
         return InferenceData.from_netcdf(datasets_available[dataset]['path'])
--- examples/ppcplot_cumulative.py
+++ examples/ppcplot_cumulative.py
@@ -8,5 +8,5 @@ import arviz as az
 
 az.style.use('arviz-darkgrid')
 
-data = az.load_arviz_data('radon')
+data = az.load_arviz_data('non_centered_eight')
 az.ppcplot(data, alpha=0.03, kind='cumulative', figsize=(12, 6), textsize=14)
