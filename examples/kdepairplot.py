c44e9a61f55852b297c751fff6557d9dace77bab
Ravin Kumar <ravinsdrive@gmail.com>
2018-09-16 09:25:57 -0700

    Add example for kdepairplot (#238)

---
+++ examples/kdepairplot.py
@@ -0,0 +1,14 @@
+"""
+KDE Pair Plot
+=========
+
+_thumb: .2, .5
+"""
+import arviz as az
+
+az.style.use('arviz-darkgrid')
+
+centered = az.load_arviz_data('centered_eight')
+
+coords = {'school': ['Choate', 'Deerfield']}
+az.pairplot(centered, var_names=['theta', 'mu', 'tau'], kind="kde", coords=coords, divergences=True)
