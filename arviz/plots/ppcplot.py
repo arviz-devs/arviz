c2ccae9e4d92ccd2fe08d5fb5c3ca0b34c81c9aa
Ravin Kumar <ravinsdrive@gmail.com>
2018-09-20 11:34:39 -0700

    Kde docs (#260)
    * Update plot_kde example
    
    * Add plots to plot_kde documentation
    
    * Fix typo in ppcplot documentation
    
    * Fix linting errors

--- arviz/plots/ppcplot.py
+++ arviz/plots/ppcplot.py
@@ -27,7 +27,7 @@ def plot_ppc(data, kind='density', alpha=0.2, mean=True, figsize=None, textsize=
         Text size for labels. If None it will be auto-scaled based on figsize.
     data_pairs : dict
         Dictionary containing relations between observed data and posterior predictive data.
-        Dictionary struture:
+        Dictionary structure:
             Key = data var_name
             Value = posterior predictive var_name
         Example: `data_pairs = {'y' : 'y_hat'}`
--- examples/plot_kde.py
+++ examples/plot_kde.py
@@ -5,11 +5,15 @@ KDE Plot
 _thumb: .2, .8
 """
 import arviz as az
-import matplotlib.pyplot as plt
 import numpy as np
 
 az.style.use('arviz-darkgrid')
 
-ax = az.plot_kde(np.random.gumbel(size=100), label='100 gumbel samples', rug=True,
+data = az.load_arviz_data('centered_eight')
+
+# Combine posterior draws for from xarray of (4,500) to ndarray (2000,)
+y_hat = np.concatenate(data.posterior_predictive["obs"].values)
+
+ax = az.plot_kde(y_hat, label='Estimated Effect of SAT Prep', rug=True,
                  plot_kwargs={'linewidth': 5, 'color': 'black'},
                  rug_kwargs={'color': 'black'})
