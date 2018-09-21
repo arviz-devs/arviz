277c1a9afa224d729a45ee5dc90f2da4ab37341f
Osvaldo Martin <aloctavodia@gmail.com>
2018-09-21 10:42:41 -0300

    clean namespace (#263)

--- arviz/data/__init__.py
+++ arviz/data/__init__.py
@@ -1,4 +1,4 @@
-#pylint: disable=wildcard-import
+# pylint: disable=wildcard-import
 """Code for loading and manipulating data structures."""
 from .inference_data import InferenceData
 from .io_netcdf import load_data, save_data, load_arviz_data
@@ -6,3 +6,8 @@ from .base import numpy_to_data_array, dict_to_dataset
 from .converters import convert_to_dataset, convert_to_inference_data
 from .io_pymc3 import from_pymc3
 from .io_pystan import from_pystan
+
+
+__all__ = ['InferenceData', 'load_data', 'save_data', 'load_arviz_data', 'numpy_to_data_array',
+           'dict_to_dataset', 'convert_to_dataset', 'convert_to_inference_data', 'from_pymc3',
+           'from_pystan']
