395f1819df1d95ed9f1a4029bb427d36a718967d
aloctavodia <aloctavodia@gmail.com>
2018-09-21 08:09:01 -0300

    clean namespace

--- arviz/stats/__init__.py
+++ arviz/stats/__init__.py
@@ -2,3 +2,7 @@
 """Statistical tests and diagnostics for ArviZ."""
 from .stats import *
 from .diagnostics import *
+
+
+__all__ = ['bfmi', 'compare', 'hpd', 'loo', 'psislw', 'r2_score', 'summary', 'waic', 'effective_n',
+           'gelman_rubin', 'geweke', 'autocorr']
