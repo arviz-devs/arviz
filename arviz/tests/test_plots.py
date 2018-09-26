5f7be089150a2dab837051b5e6edcced410dd235
Ravin Kumar <ravinsdrive@gmail.com>
2018-09-25 20:42:37 -0700

    Rename fixture to be more direct

--- arviz/tests/test_plots.py
+++ arviz/tests/test_plots.py
@@ -13,10 +13,9 @@ from ..plots import (plot_density, plot_trace, plot_energy, plot_posterior,
 
 
 @pytest.fixture(scope='module')
-def plot_inputs(request):
-    class PlotInputs:
+def models(request):
+    class Models:
         models = load_cached_models(draws=500, chains=2)
-
         pymc3_model, pymc3_fit = models['pymc3']
         stan_model, stan_fit = models['pystan']
 
@@ -24,14 +23,14 @@ def plot_inputs(request):
         plt.close('all')
 
     request.addfinalizer(fin)
-    return PlotInputs()
+    return Models()
 
 
 @pytest.fixture(scope='module')
-def pymc3_sample_ppc(plot_inputs):
+def pymc3_sample_ppc(models):
 
-    with plot_inputs.pymc3_model:
-        return pm.sample_ppc(plot_inputs.pymc3_fit, 100)
+    with models.pymc3_model:
+        return pm.sample_ppc(models.pymc3_fit, 100)
 
 
 @pytest.fixture(scope='module')
@@ -46,8 +45,8 @@ def df_trace():
 
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
-def test_plot_density(plot_inputs, obj_attr):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_density(models, obj_attr):
+    obj = getattr(models, obj_attr)
     axes = plot_density(obj)
     assert axes.shape[0] >= 18
     assert axes.shape[1] == 1
@@ -55,8 +54,8 @@ def test_plot_density(plot_inputs, obj_attr):
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
 @pytest.mark.parametrize('combined', [True, False])
-def test_plot_trace(plot_inputs, combined, obj_attr):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_trace(models, combined, obj_attr):
+    obj = getattr(models, obj_attr)
     axes = plot_trace(obj, var_names=('mu', 'tau'), combined=combined, lines=[('mu', {}, [1, 2])])
     assert axes.shape == (2, 2)
 
@@ -68,8 +67,8 @@ def test_plot_trace(plot_inputs, combined, obj_attr):
                                                  combined=True), (2,)),
                                            (dict(kind='ridgeplot', r_hat=False, n_eff=False), (1,))
                                            ])
-def test_plot_forest(plot_inputs, obj_attrs, args_expected):
-    obj = [getattr(plot_inputs, obj_attr) for obj_attr in obj_attrs]
+def test_plot_forest(models, obj_attrs, args_expected):
+    obj = [getattr(models, obj_attr) for obj_attr in obj_attrs]
     args, expected = args_expected
     _, axes = plot_forest(obj, **args)
     assert axes.shape == expected
@@ -77,8 +76,8 @@ def test_plot_forest(plot_inputs, obj_attrs, args_expected):
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
 @pytest.mark.parametrize('kind', ['kde', 'hist'])
-def test_plot_energy(plot_inputs, obj_attr, kind):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_energy(models, obj_attr, kind):
+    obj = getattr(models, obj_attr)
     assert plot_energy(obj, kind=kind)
 
 
@@ -88,15 +87,15 @@ def test_plot_parallel_raises_valueerror(df_trace):  # pylint: disable=invalid-n
 
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
-def test_plot_parallel_exception(plot_inputs, obj_attr):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_parallel_exception(models, obj_attr):
+    obj = getattr(models, obj_attr)
     assert plot_parallel(obj)
 
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
 @pytest.mark.parametrize('kind', ['scatter', 'hexbin'])
-def test_plot_joint(plot_inputs, obj_attr, kind):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_joint(models, obj_attr, kind):
+    obj = getattr(models, obj_attr)
     plot_joint(obj, var_names=('mu', 'tau'), kind=kind)
 
 
@@ -112,30 +111,30 @@ def test_plot_joint(plot_inputs, obj_attr, kind):
                                     {"kind":'hexbin', "var_names": ['theta'],
                                      "coords":{'theta_dim_0': [0, 1]},
                                      "plot_kwargs":{'cmap': 'viridis'}, "textsize": 20}])
-def test_plot_pair(plot_inputs, obj_attr, kwargs):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_pair(models, obj_attr, kwargs):
+    obj = getattr(models, obj_attr)
     ax, _ = plot_pair(obj, **kwargs)
     assert ax
 
 
 @pytest.mark.parametrize('kind', ['density', 'cumulative'])
-def test_plot_ppc(plot_inputs, pymc3_sample_ppc, kind):
-    data = from_pymc3(trace=plot_inputs.pymc3_fit,
+def test_plot_ppc(models, pymc3_sample_ppc, kind):
+    data = from_pymc3(trace=models.pymc3_fit,
                       posterior_predictive=pymc3_sample_ppc)
     axes = plot_ppc(data, kind=kind)
     assert axes
 
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
-def test_plot_violin(plot_inputs, obj_attr):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_violin(models, obj_attr):
+    obj = getattr(models, obj_attr)
     axes = plot_violin(obj)
     assert axes.shape[0] >= 18
 
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
-def test_plot_autocorr_uncombined(plot_inputs, obj_attr):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_autocorr_uncombined(models, obj_attr):
+    obj = getattr(models, obj_attr)
     axes = plot_autocorr(obj, combined=False)
     assert axes.shape[0] == 1
     assert (axes.shape[1] == 36 and obj_attr == "pymc3_fit" or
@@ -143,8 +142,8 @@ def test_plot_autocorr_uncombined(plot_inputs, obj_attr):
 
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
-def test_plot_autocorr_combined(plot_inputs, obj_attr):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_autocorr_combined(models, obj_attr):
+    obj = getattr(models, obj_attr)
     axes = plot_autocorr(obj, combined=True)
     assert axes.shape[0] == 1
     assert (axes.shape[1] == 18 and obj_attr == "pymc3_fit" or
@@ -152,15 +151,15 @@ def test_plot_autocorr_combined(plot_inputs, obj_attr):
 
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
-def test_plot_posterior(plot_inputs, obj_attr):
-    obj = getattr(plot_inputs, obj_attr)
+def test_plot_posterior(models, obj_attr):
+    obj = getattr(models, obj_attr)
     axes = plot_posterior(obj, var_names=('mu', 'tau'), rope=(-2, 2), ref_val=0)
     assert axes.shape == (2,)
 
 
 @pytest.mark.parametrize("obj_attr", ["pymc3_fit", "stan_fit"])
 @pytest.mark.parametrize("point_estimate", ('mode', 'mean', 'median'))
-def test_point_estimates(plot_inputs, obj_attr, point_estimate):
-    obj = getattr(plot_inputs, obj_attr)
+def test_point_estimates(models, obj_attr, point_estimate):
+    obj = getattr(models, obj_attr)
     axes = plot_posterior(obj, var_names=('mu', 'tau'), point_estimate=point_estimate)
     assert axes.shape == (2,)
