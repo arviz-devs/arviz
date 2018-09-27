8aec07ca5e1ede9e5a243998223558054ea316a3
Ravin Kumar <ravinsdrive@gmail.com>
2018-09-26 21:49:19 -0700

    Simplify test_data further

--- arviz/tests/test_data.py
+++ arviz/tests/test_data.py
@@ -50,6 +50,7 @@ class TestNumpyToDataArray():
         assert inference_data.foo.draw.shape == shape[1:2]
         assert inference_data.foo[var_name].shape == shape
 
+
 class TestConvertToDataset():
     def setup_method(self):
         # pylint: disable=attribute-defined-outside-init
@@ -133,7 +134,6 @@ def test_dict_to_dataset():
     assert set(dataset.b.coords) == {'chain', 'draw', 'c'}
 
 
-
 def test_convert_to_dataset_idempotent():
     first = convert_to_dataset(np.random.randn(100))
     second = convert_to_dataset(first)
@@ -167,7 +167,19 @@ def test_convert_to_dataset_bad(tmpdir):
         convert_to_dataset(filename, group='bar')
 
 
-class CheckNetCDFUtils(BaseArvizTest):
+class TestDictNetCDFUtils(BaseArvizTest):
+
+    @classmethod
+    def setup_class(cls):
+        # Data of the Eight Schools Model
+        cls.data = eight_schools_params()
+        cls.draws, cls.chains = 500, 2
+        _, stan_fit = load_cached_models(cls.draws, cls.chains)['pystan']
+        stan_dict = stan_fit.extract(stan_fit.model_pars, permuted=False)
+        cls.obj = {}
+        for name, vals in stan_dict.items():
+            if name not in {'y_hat', 'log_lik'}:  # extra vars
+                cls.obj[name] = np.swapaxes(vals, 0, 1)
 
     def check_var_names_coords_dims(self, dataset):
         assert set(dataset.data_vars) == {'mu', 'tau', 'theta_tilde', 'theta'}
@@ -198,22 +210,7 @@ class CheckNetCDFUtils(BaseArvizTest):
         )
 
 
-class TestDictNetCDFUtils(CheckNetCDFUtils):
-
-    @classmethod
-    def setup_class(cls):
-        # Data of the Eight Schools Model
-        cls.data = eight_schools_params()
-        cls.draws, cls.chains = 500, 2
-        _, stan_fit = load_cached_models(cls.draws, cls.chains)['pystan']
-        stan_dict = stan_fit.extract(stan_fit.model_pars, permuted=False)
-        cls.obj = {}
-        for name, vals in stan_dict.items():
-            if name not in {'y_hat', 'log_lik'}:  # extra vars
-                cls.obj[name] = np.swapaxes(vals, 0, 1)
-
-
-class TestPyMC3NetCDFUtils(CheckNetCDFUtils):
+class TestPyMC3NetCDFUtils(BaseArvizTest):
 
     @classmethod
     def setup_class(cls):
