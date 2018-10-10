907f0dc0d9a7563c1df24b0f8f566ca9b3216184
Ravin Kumar <ravinsdrive@gmail.com>
2018-10-10 15:26:25 -0700

    Remove pylint as linter and replace with black

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,6 +8,6 @@ echo "Checking documentation..."
 python -m pydocstyle --convention=numpy ${SRC_DIR}/arviz/
 echo "Success!"
 
-echo "Checking code style..."
-python -m pylint arviz/
+echo "Checking code style with black..."
+python -m black -l 100 --check arviz/
 echo "Success!"
