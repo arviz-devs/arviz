0b9ba6b33aa420e3a760f15361d1177afbf02e28
Ravin Kumar <ravinsdrive@gmail.com>
2018-10-13 14:42:12 -0700

    Reduce test io with fixture

--- arviz/tests/test_stats.py
+++ arviz/tests/test_stats.py
@@ -7,6 +7,18 @@ from arviz import load_arviz_data
 from ..stats import bfmi, compare, hpd, r2_score, waic, psislw, summary
 
 
+@pytest.fixture(scope="session")
+def centered_eight():
+    centered_eight = load_arviz_data("centered_eight")
+    return centered_eight
+
+
+@pytest.fixture(scope="session")
+def non_centered_eight():
+    non_centered_eight = load_arviz_data("non_centered_eight")
+    return non_centered_eight
+
+
 def test_bfmi():
     energy = np.array([1, 2, 3, 4])
     assert_almost_equal(bfmi(energy), 0.8)
@@ -25,27 +37,22 @@ def test_r2_score():
     assert_almost_equal(res.rvalue ** 2, r2_score(y, res.intercept + res.slope * x).r2, 2)
 
 
-class TestCompare:
-    @classmethod
-    def setup_class(cls):
-        cls.centered = load_arviz_data("centered_eight")
-        cls.non_centered = load_arviz_data("non_centered_eight")
+@pytest.mark.parametrize("method", ["stacking", "BB-pseudo-BMA", "pseudo-BMA"])
+def test_compare_same(centered_eight, non_centered_eight, method):
+    data_dict = {"first": centered_eight, "second": centered_eight}
 
-    @pytest.mark.parametrize("method", ["stacking", "BB-pseudo-BMA", "pseudo-BMA"])
-    def test_compare_same(self, method):
-        data_dict = {"first": self.centered, "second": self.centered}
+    weight = compare(data_dict, method=method)["weight"]
+    assert_almost_equal(weight[0], weight[1])
+    assert_almost_equal(np.sum(weight), 1.0)
 
-        weight = compare(data_dict, method=method)["weight"]
-        assert_almost_equal(weight[0], weight[1])
-        assert_almost_equal(np.sum(weight), 1.0)
 
-    @pytest.mark.parametrize("ic", ["waic", "loo"])
-    @pytest.mark.parametrize("method", ["stacking", "BB-pseudo-BMA", "pseudo-BMA"])
-    def test_compare_different(self, ic, method):
-        model_dict = {"centered": self.centered, "non_centered": self.non_centered}
-        weight = compare(model_dict, ic=ic, method=method)["weight"]
-        assert weight["non_centered"] > weight["centered"]
-        assert_almost_equal(np.sum(weight), 1.0)
+@pytest.mark.parametrize("ic", ["waic", "loo"])
+@pytest.mark.parametrize("method", ["stacking", "BB-pseudo-BMA", "pseudo-BMA"])
+def test_compare_different(centered_eight, non_centered_eight, ic, method):
+    model_dict = {"centered": centered_eight, "non_centered": non_centered_eight}
+    weight = compare(model_dict, ic=ic, method=method)["weight"]
+    assert weight["non_centered"] > weight["centered"]
+    assert_almost_equal(np.sum(weight), 1.0)
 
 
 @pytest.mark.parametrize("var_names_expected", ((None, 10), ("mu", 1), (["mu", "tau"], 2)))
@@ -57,27 +64,23 @@ def test_summary_var_names(var_names_expected):
 
 
 @pytest.mark.parametrize("include_circ", [True, False])
-def test_summary_include_circ(include_circ):
-    centered = load_arviz_data("centered_eight")
-    summary(centered, include_circ=include_circ)
+def test_summary_include_circ(centered_eight, include_circ):
+    assert summary(centered_eight, include_circ=include_circ) is not None
 
 
 @pytest.mark.parametrize("fmt", ["wide", "long", "xarray"])
-def test_summary_fmt(fmt):
-    centered = load_arviz_data("centered_eight")
-    summary(centered, fmt=fmt)
+def test_summary_fmt(centered_eight, fmt):
+    assert summary(centered_eight, fmt=fmt) is not None
 
 
-def test_summary_bad_fmt():
-    centered = load_arviz_data("centered_eight")
+def test_summary_bad_fmt(centered_eight):
     with pytest.raises(TypeError):
-        summary(centered, fmt="bad_fmt")
+        summary(centered_eight, fmt="bad_fmt")
 
 
-def test_waic():
+def test_waic(centered_eight):
     """Test widely available information criterion calculation"""
-    centered = load_arviz_data("centered_eight")
-    waic(centered)
+    assert waic(centered_eight) is not None
 
 
 def test_psis():
