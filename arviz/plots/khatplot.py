11f1a9d0eafe8c17fd3eea20f2b15ae31accd3af
MFreidank <freidankm@yahoo.de>
2018-10-17 12:15:46 +0200

    fix: markersize argument is unused for khat plots

--- arviz/plots/khatplot.py
+++ arviz/plots/khatplot.py
@@ -6,7 +6,7 @@ from .plot_utils import _scale_fig_size
 
 
 def plot_khat(
-    khats, figsize=None, textsize=None, markersize=5, ax=None, hlines_kwargs=None, **kwargs
+    khats, figsize=None, textsize=None, markersize=None, ax=None, hlines_kwargs=None, **kwargs
 ):
     """
     Plot Pareto tail indices.
@@ -21,7 +21,7 @@ def plot_khat(
         Text size scaling factor for labels, titles and lines. If None it will be autoscaled based
         on figsize.
     markersize: int
-        markersize for scatter plot. Defaults to 5
+        markersize for scatter plot. Defaults to `None` in which case it will be chosen based on autoscaling for figsize.
     ax: axes, opt
       Matplotlib axes
     hlines_kwargs: dictionary
@@ -37,9 +37,12 @@ def plot_khat(
     if hlines_kwargs is None:
         hlines_kwargs = {}
 
-    (figsize, ax_labelsize, _, xt_labelsize, linewidth, markersize) = _scale_fig_size(
+    (figsize, ax_labelsize, _, xt_labelsize, linewidth, autoscaled_markersize) = _scale_fig_size(
         figsize, textsize
     )
+
+    if markersize is None:
+        markersize = autoscaled_markersize
 
     if ax is None:
         _, ax = plt.subplots(1, 1, figsize=figsize)
