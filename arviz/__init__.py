9046b786f920875c1fc2b1af9e325459a009fcb8
Thummanoon Kunanuntakij <gentlerainsky@gmail.com>
2018-10-19 18:35:02 +0700

    change to library style logging

--- arviz/__init__.py
+++ arviz/__init__.py
@@ -1,7 +1,6 @@
 # pylint: disable=wildcard-import,invalid-name,wrong-import-position
 """ArviZ is a library for exploratory analysis of Bayesian models."""
 __version__ = "0.2.1"
-from matplotlib import get_backend as mpl_get_backend
 from matplotlib.pyplot import style
 
 from .data import *
@@ -10,11 +9,5 @@ from .stats import *
 
 import logging
 
-_log = logging.getLogger('arviz')
 if not logging.root.handlers:
-    _log.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    _log.addHandler(handler)
-    _log.info('matplotlib backend:' + mpl_get_backend())
+    logging.getLogger(__name__).addHandler(logging.NullHandler())
