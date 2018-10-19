f23940e6495005bf2814916d798acb35bd2fb90f
Thummanoon Kunanuntakij <gentlerainsky@gmail.com>
2018-10-19 19:23:03 +0700

    reorder import module

--- arviz/__init__.py
+++ arviz/__init__.py
@@ -1,13 +1,13 @@
 # pylint: disable=wildcard-import,invalid-name,wrong-import-position
 """ArviZ is a library for exploratory analysis of Bayesian models."""
 __version__ = "0.2.1"
+
+import logging
 from matplotlib.pyplot import style
 
 from .data import *
 from .plots import *
 from .stats import *
-
-import logging
 
 if not logging.root.handlers:
     logging.getLogger(__name__).addHandler(logging.NullHandler())
