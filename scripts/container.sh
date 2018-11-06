fadd17e0124ed79c5f85de58afa106a4a179ae06
Ravin Kumar <ravinsdrive@gmail.com>
2018-11-05 21:39:26 -0800

    Start of docker testing (#380)
    * Create dockerfile for testing arviz
    
    WIP: Add g++ and gfortran
    
    WIP: get Conda environment working
    
    Add build-essential to docker build
    
    Change xarray to pip install due to conda installing older version
    
    Delete cached models in container
    
    Add command to delete cached models
    
    Consolidate container file into one
    
    Modify dockerfile bash to activate environment on load
    
    WIP: Kinda working minimal-notebook
    
    More docker edits
    
    Decrease size of docker image
    
    * Update README for container
    
    * Skip .bashrc creation on docker build
    
    * Remove unneeded echo statements
    
    * Update README
    
    * Update package installation
    
    * Fix bug of removing cached files

---
+++ scripts/container.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+SRC_DIR=${SRC_DIR:-`pwd`}
+
+# Build container for use of testing or notebook
+if [[ $* == *--build* ]]; then
+    echo "Building Docker Image"
+    docker build \
+        -t arviz \
+        -f $SRC_DIR/scripts/Dockerfile \
+        --build-arg SRC_DIR=. $SRC_DIR \
+        --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
+        --build-arg PYSTAN_VERSION=${PYSTAN_VERSION}\
+        --rm
+fi
