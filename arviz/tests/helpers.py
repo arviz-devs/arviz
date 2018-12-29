aa8ba21110a9330d9243206958d9eae225494f0b
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2018-12-29 05:04:15 +0200

    Update .travis.yml (#467)
    * Update .travis.yml
    
    * Use matrix in travis
    
    * Delete sampler from pyro posterior (enable pickle)
    
    * squeeze 0.3 output
    
    * replace newline marker with _
    
    * check if pyro posterior has sampler and squeeze observed data
    
    * Undo unneeded fix

--- arviz/tests/helpers.py
+++ arviz/tests/helpers.py
@@ -133,6 +133,8 @@ def pyro_centered_schools(data, draws, chains):
     posterior.kernel = None
     posterior.run = None
     posterior.logger = None
+    if hasattr(posterior, "sampler"):
+        posterior.sampler = None
     return posterior
 
 
