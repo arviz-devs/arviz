c462d8f7cc81d81b0fce881b49139ba4f0c6c0f4
Ravin Kumar <ravinsdrive@gmail.com>
2019-01-16 15:00:05 -0800

    Actually revert np.where

--- arviz/stats/stats.py
+++ arviz/stats/stats.py
@@ -464,7 +464,7 @@ def psislw(log_weights, reff=1.0):
         xcutoff = max(x[x_sort_ind[cutoff_ind]], cutoffmin)
 
         expxcutoff = np.exp(xcutoff)
-        tailinds = np.where(x > xcutoff)
+        tailinds, = np.where(x > xcutoff)  # pylint: disable=unbalanced-tuple-unpacking
         x_tail = x[tailinds]
         tail_len = len(x_tail)
         if tail_len <= 4:
