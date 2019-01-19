ef1c72bdbcd1e611170cd9de499905c2f08c7ae9
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2019-01-19 19:16:45 +0200

    az.concat and __add__ - Simple group level addition (#547)
    * implement simple grouplevel addition
    
    * fix test
    
    * add tests
    
    * redo tests
    
    * Fix test
    
    * test logic
    
    * Fix
    
    * Fix typo
    
    * Fix edgecase

--- arviz/tests/test_data.py
+++ arviz/tests/test_data.py
@@ -9,6 +9,7 @@ import pymc3 as pm
 import pytest
 
 from arviz import (
+    concat,
     convert_to_inference_data,
     convert_to_dataset,
     from_cmdstan,
@@ -163,6 +164,99 @@ def test_make_attrs():
     attrs = make_attrs(attrs=extra_attrs)
     assert "key" in attrs
     assert attrs["key"] == "Value"
+
+
+def test_addition():
+    idata1 = from_dict(
+        posterior={"A": np.random.randn(2, 10, 2), "B": np.random.randn(2, 10, 5, 2)}
+    )
+    idata2 = from_dict(prior={"C": np.random.randn(2, 10, 2), "D": np.random.randn(2, 10, 5, 2)})
+    new_idata = idata1 + idata2
+    assert new_idata is not None
+    assert hasattr(new_idata, "posterior")
+    assert hasattr(new_idata, "prior")
+    assert hasattr(new_idata.posterior, "A")
+    assert hasattr(new_idata.posterior, "B")
+    assert hasattr(new_idata.prior, "C")
+    assert hasattr(new_idata.prior, "D")
+
+
+@pytest.mark.parametrize("copy", [True, False])
+@pytest.mark.parametrize("inplace", [True, False])
+@pytest.mark.parametrize("sequence", [True, False])
+def test_concat(copy, inplace, sequence):
+    idata1 = from_dict(
+        posterior={"A": np.random.randn(2, 10, 2), "B": np.random.randn(2, 10, 5, 2)}
+    )
+    if copy and inplace:
+        original_idata1_posterior_id = id(idata1.posterior)
+    idata2 = from_dict(prior={"C": np.random.randn(2, 10, 2), "D": np.random.randn(2, 10, 5, 2)})
+    idata3 = from_dict(observed_data={"E": np.random.randn(100), "F": np.random.randn(2, 100)})
+    # basic case
+    assert concat(idata1, idata2, copy=True, inplace=False) is not None
+    if sequence:
+        new_idata = concat((idata1, idata2, idata3), copy=copy, inplace=inplace)
+    else:
+        new_idata = concat(idata1, idata2, idata3, copy=copy, inplace=inplace)
+    if inplace:
+        assert new_idata is None
+        new_idata = idata1
+    assert new_idata is not None
+    assert hasattr(new_idata, "posterior")
+    assert hasattr(new_idata, "prior")
+    assert hasattr(new_idata, "observed_data")
+    assert hasattr(new_idata.posterior, "A")
+    assert hasattr(new_idata.posterior, "B")
+    assert hasattr(new_idata.prior, "C")
+    assert hasattr(new_idata.prior, "D")
+    assert hasattr(new_idata.observed_data, "E")
+    assert hasattr(new_idata.observed_data, "F")
+    if copy:
+        if inplace:
+            assert id(new_idata.posterior) == original_idata1_posterior_id
+        else:
+            assert id(new_idata.posterior) != id(idata1.posterior)
+        assert id(new_idata.prior) != id(idata2.prior)
+        assert id(new_idata.observed_data) != id(idata3.observed_data)
+    else:
+        assert id(new_idata.posterior) == id(idata1.posterior)
+        assert id(new_idata.prior) == id(idata2.prior)
+        assert id(new_idata.observed_data) == id(idata3.observed_data)
+
+
+@pytest.mark.parametrize("copy", [True, False])
+@pytest.mark.parametrize("inplace", [True, False])
+@pytest.mark.parametrize("sequence", [True, False])
+def test_concat_edgecases(copy, inplace, sequence):
+    idata = from_dict(posterior={"A": np.random.randn(2, 10, 2), "B": np.random.randn(2, 10, 5, 2)})
+    empty = concat()
+    assert empty is not None
+    if sequence:
+        new_idata = concat([idata], copy=copy, inplace=inplace)
+    else:
+        new_idata = concat(idata, copy=copy, inplace=inplace)
+    if inplace:
+        assert new_idata is None
+        new_idata = idata
+    else:
+        assert new_idata is not None
+    assert hasattr(new_idata, "posterior")
+    assert hasattr(new_idata.posterior, "A")
+    assert hasattr(new_idata.posterior, "B")
+    if copy and not inplace:
+        assert id(new_idata.posterior) != id(idata.posterior)
+    else:
+        assert id(new_idata.posterior) == id(idata.posterior)
+
+
+def test_concat_bad():
+    with pytest.raises(TypeError):
+        concat("hello", "hello")
+    idata = from_dict(posterior={"A": np.random.randn(2, 10, 2), "B": np.random.randn(2, 10, 5, 2)})
+    with pytest.raises(TypeError):
+        concat(idata, np.array([1, 2, 3, 4, 5]))
+    with pytest.raises(NotImplementedError):
+        concat(idata, idata)
 
 
 class TestNumpyToDataArray:
