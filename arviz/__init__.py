acf4a98c9209e7486f9e78a85c77185ebe9374c8
Colin Carroll <colcarroll@gmail.com>
2019-02-22 09:20:30 -0500

    Bump minor version

--- arviz/__init__.py
+++ arviz/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wildcard-import,invalid-name,wrong-import-position
 """ArviZ is a library for exploratory analysis of Bayesian models."""
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 import os
 import logging
