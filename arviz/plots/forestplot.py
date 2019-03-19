a22f7e181a145f1fc8ad0ff0e9a28b77e47e32f2
GWeindel <gabriel.weindel@gmail.com>
2019-03-19 16:32:48 +0100

    Pylint fix
    Removed previous created comma for line 42 and just created a single line for summary of forestplot()

--- arviz/plots/forestplot.py
+++ arviz/plots/forestplot.py
@@ -39,7 +39,7 @@ def plot_forest(
     ridgeplot_overlap=2,
     figsize=None,
 ):
-    """Forest plot to compare credible intervals from a number of distributions, generates a forest plot of 100*(credible_interval)% credible intervals from a trace or list of traces.
+    """Forest plot to compare credible intervals from a number of distributions. Generates a forest plot of 100*(credible_interval)% credible intervals from a trace or list of traces.
 
     Parameters
     ----------
