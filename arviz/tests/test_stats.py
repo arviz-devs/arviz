ca698c72cecc488ebb138f06a133ec83a463ba38
Ari Hartikainen <ari.hartikainen@aalto.fi>
2019-05-11 09:06:32 +0300

    fix tests

--- arviz/tests/test_stats.py
+++ arviz/tests/test_stats.py
@@ -8,8 +8,8 @@ from xarray import Dataset, DataArray
 
 
 from ..data import load_arviz_data, from_dict, convert_to_inference_data, concat
-from ..stats import bfmi, compare, hpd, loo, r2_score, waic, psislw, summary
-from ..stats.stats import _gpinv, _mc_error, _logsumexp
+from ..stats import compare, hpd, loo, r2_score, waic, psislw, summary
+from ..stats.stats import _gpinv
 
 
 @pytest.fixture(scope="session")
@@ -268,7 +268,7 @@ def test_loo_warning(centered_eight):
 def test_psislw():
     data = load_arviz_data("centered_eight")
     pareto_k = loo(data, pointwise=True, reff=0.7)["pareto_k"]
-    log_likelihood = data.sample_stats.log_likelihood
+    log_likelihood = data.sample_stats.log_likelihood  # pylint: disable=no-member
     n_samples = log_likelihood.chain.size * log_likelihood.draw.size
     new_shape = (n_samples,) + log_likelihood.shape[2:]
     log_likelihood = log_likelihood.values.reshape(*new_shape)
@@ -286,68 +286,8 @@ def test_gpinv(probs, kappa, sigma):
     assert len(_gpinv(probs, kappa, sigma)) == len(probs)
 
 
-@pytest.mark.parametrize("ary_dtype", [np.float64, np.float32, np.int32, np.int64])
-@pytest.mark.parametrize("axis", [None, 0, 1, (-2, -1)])
-@pytest.mark.parametrize("b", [None, 0, 1 / 100, 1 / 101])
-@pytest.mark.parametrize("keepdims", [True, False])
-def test_logsumexp_b(ary_dtype, axis, b, keepdims):
-    """Test ArviZ implementation of logsumexp.
-
-    Test also compares against Scipy implementation.
-    Case where b=None, they are equal. (N=len(ary))
-    Second case where b=x, and x is 1/(number of elements), they are almost equal.
-
-    Test tests against b parameter.
-    """
-    np.random.seed(17)
-    ary = np.random.randn(100, 101).astype(ary_dtype)  # pylint: disable=no-member
-    assert _logsumexp(ary=ary, axis=axis, b=b, keepdims=keepdims, copy=True) is not None
-    ary = ary.copy()
-    assert _logsumexp(ary=ary, axis=axis, b=b, keepdims=keepdims, copy=False) is not None
-    out = np.empty(5)
-    assert _logsumexp(ary=np.random.randn(10, 5), axis=0, out=out) is not None
-
-    # Scipy implementation
-    scipy_results = logsumexp(ary, b=b, axis=axis, keepdims=keepdims)
-    arviz_results = _logsumexp(ary, b=b, axis=axis, keepdims=keepdims)
-
-    assert_array_almost_equal(scipy_results, arviz_results)
-
-
-@pytest.mark.parametrize("ary_dtype", [np.float64, np.float32, np.int32, np.int64])
-@pytest.mark.parametrize("axis", [None, 0, 1, (-2, -1)])
-@pytest.mark.parametrize("b_inv", [None, 0, 100, 101])
-@pytest.mark.parametrize("keepdims", [True, False])
-def test_logsumexp_b_inv(ary_dtype, axis, b_inv, keepdims):
-    """Test ArviZ implementation of logsumexp.
-
-    Test also compares against Scipy implementation.
-    Case where b=None, they are equal. (N=len(ary))
-    Second case where b=x, and x is 1/(number of elements), they are almost equal.
-
-    Test tests against b_inv parameter.
-    """
-    np.random.seed(17)
-    ary = np.random.randn(100, 101).astype(ary_dtype)  # pylint: disable=no-member
-    assert _logsumexp(ary=ary, axis=axis, b_inv=b_inv, keepdims=keepdims, copy=True) is not None
-    ary = ary.copy()
-    assert _logsumexp(ary=ary, axis=axis, b_inv=b_inv, keepdims=keepdims, copy=False) is not None
-    out = np.empty(5)
-    assert _logsumexp(ary=np.random.randn(10, 5), axis=0, out=out) is not None
-
-    if b_inv != 0:
-        # Scipy implementation when b_inv != 0
-        if b_inv is not None:
-            b_scipy = 1 / b_inv
-        else:
-            b_scipy = None
-        scipy_results = logsumexp(ary, b=b_scipy, axis=axis, keepdims=keepdims)
-        arviz_results = _logsumexp(ary, b_inv=b_inv, axis=axis, keepdims=keepdims)
-
-        assert_array_almost_equal(scipy_results, arviz_results)
-
-
-def test_multidimenional_log_likelihood():
+@pytest.mark.parametrize("func", [loo, waic])
+def test_multidimensional_log_likelihood(func):
     np.random.seed(17)
     llm = np.random.rand(4, 23, 15, 2)
     ll1 = llm.reshape(4, 23, 15 * 2)
@@ -360,14 +300,12 @@ def test_multidimenional_log_likelihood():
     dsm = convert_to_inference_data(statsm, group="sample_stats")
     ds1 = convert_to_inference_data(stats1, group="sample_stats")
     dsp = convert_to_inference_data(post, group="posterior")
+
     dsm = concat(dsp, dsm)
     ds1 = concat(dsp, ds1)
-    lrm = loo(dsm)
-    lr1 = loo(ds1)
-    assert (lr1 == lrm).all()
-    assert_array_almost_equal(lrm[:4], lr1[:4])
 
-    wrm = waic(dsm)
-    wr1 = waic(ds1)
-    assert (wr1 == wrm).all()
-    assert_array_almost_equal(wrm[:4], wr1[:4])
+    frm = func(dsm)
+    fr1 = func(ds1)
+
+    assert (fr1 == frm).all()
+    assert_array_almost_equal(frm[:4], fr1[:4])
