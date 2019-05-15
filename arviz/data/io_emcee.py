8d4e2c780bfe145f994eb1c5ce7977c4a1b176a6
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2019-05-15 16:46:00 +0200

    Correct typo

--- arviz/data/io_emcee.py
+++ arviz/data/io_emcee.py
@@ -327,7 +327,7 @@ def from_emcee(
         :context: close-figs
 
         >>> if emcee.__version__[0] == '3':
-        >>>     ess=(draws-burnin)/sampler.get_autocorr_time(quiet=True, discard=burnin, thin=thin)
+        >>>     ess=(draws-burnin)/sampler_blobs.get_autocorr_time(quiet=True, discard=burnin, thin=thin)
         >>> else:
         >>>     # to avoid error while generating the docs, the ess value is hard coded, it
         >>>     # should be calculated with:
