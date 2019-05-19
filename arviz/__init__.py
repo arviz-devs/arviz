c8943b0c08ea9e644856e8a1ab2c3b655bb84713
Ravin Kumar <ravinsdrive@gmail.com>
2019-05-19 14:06:00 -0700

    Increment version number to 0.4.0

--- arviz/__init__.py
+++ arviz/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wildcard-import,invalid-name,wrong-import-position
 """ArviZ is a library for exploratory analysis of Bayesian models."""
-__version__ = "0.3.3"
+__version__ = "0.4.0"
 
 import os
 import logging
