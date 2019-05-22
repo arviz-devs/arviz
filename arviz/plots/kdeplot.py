4d2a33a02ae1aea63723065905ac26fd6c031025
Banzee <aniruddhab8@gmail.com>
2019-05-22 23:34:37 +0530

    Initial Commit

--- arviz/plots/kdeplot.py
+++ arviz/plots/kdeplot.py
@@ -4,9 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import gaussian, convolve, convolve2d  # pylint: disable=no-name-in-module
 from scipy.sparse import coo_matrix
+from numba import njit
 import xarray as xr
 from ..data.inference_data import InferenceData
-from ..utils import conditional_jit
 from .plot_utils import _scale_fig_size
 
 
@@ -262,6 +262,7 @@ def plot_kde(
     return ax
 
 
+@njit(cache=True)
 def _fast_kde(x, cumulative=False, bw=4.5, xmin=None, xmax=None):
     """Fast Fourier transform-based Gaussian kernel density estimate (KDE).
 
@@ -331,12 +332,13 @@ def _fast_kde(x, cumulative=False, bw=4.5, xmin=None, xmax=None):
     return density, xmin, xmax
 
 
-@conditional_jit
+@njit(cache=True)
 def _histogram(x, n_bins, range_hist=None):
     grid, _ = np.histogram(x, bins=n_bins, range=range_hist)
     return grid
 
 
+@njit(cache=True)
 def _fast_kde_2d(x, y, gridsize=(128, 128), circular=False):
     """
     2D fft-based Gaussian kernel density estimate (KDE).
