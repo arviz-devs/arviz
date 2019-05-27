9d0fdb18f798dcb89d6ceee1f524157b402309bb
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2019-05-28 00:38:03 +0200

    Add xlabel option

--- arviz/plots/pointwiseelpdplot.py
+++ arviz/plots/pointwiseelpdplot.py
@@ -3,6 +3,7 @@ import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 from matplotlib.ticker import NullFormatter
+import matplotlib.ticker as mticker
 
 from ..data import convert_to_inference_data
 from .plot_utils import _scale_fig_size
@@ -13,6 +14,7 @@ def plot_pointwise_elpd(
     idata_dict,
     ic="waic",
     color=None,
+    xlabels=False,
     figsize=None,
     textsize=None,
     coords=None,
@@ -34,6 +36,8 @@ def plot_pointwise_elpd(
         Colors of the scatter plot, if color is a str all dots will have the same color,
         if it is the size of the observations, each dot will have the specified color,
         otherwise, it will be interpreted as a list of the dims to be used for the color code
+    xlabels : bool, optional
+        Use coords as xticklabels
     figsize : figure size tuple, optional
         If None, size is (8 + numvars, 8 + numvars)
     textsize: int, optional
@@ -58,8 +62,9 @@ def plot_pointwise_elpd(
     ic = ic.lower()
     if ic not in valid_ics:
         raise ValueError(
-            ("Information Criteria type {} not recognized."
-             "IC must be in {}").format(ic, valid_ics)
+            ("Information Criteria type {} not recognized." "IC must be in {}").format(
+                ic, valid_ics
+            )
         )
 
     if coords is None:
@@ -68,8 +73,7 @@ def plot_pointwise_elpd(
     if plot_kwargs is None:
         plot_kwargs = {}
         plot_kwargs.setdefault("marker", ".")
-        plot_kwargs.setdefault("lw", 0)
-
+        # plot_kwargs.setdefault("lw", 0)
 
     def get_pointwise_as_dataarray(idata, coords=coords, ic=ic, scale=scale):
         if ic == "waic":
@@ -80,46 +84,50 @@ def plot_pointwise_elpd(
         dims = [dim for dim in like_dataarray.dims if dim not in ["chain", "draw"]]
         present_coords = {dim: like_dataarray.coords.indexes[dim] for dim in dims}
         elpd_dataarray = xr.DataArray(pointwise_elpd, dims=dims, coords=present_coords)
-        print(elpd_dataarray)
-        print(coords)
         elpd_dataarray = elpd_dataarray.sel(**coords)
-        print(elpd_dataarray)
         return elpd_dataarray
-
 
     # Make sure all objects in idata_dict are InferenceData
     idata_dict = {key: convert_to_inference_data(idata) for key, idata in idata_dict.items()}
     numvars = len(idata_dict)
     models = list(idata_dict.keys())
     pointwise_data = [get_pointwise_as_dataarray(idata_dict[model]) for model in models]
-
+    xdata = np.arange(pointwise_data[0].size)
+    if xlabels:
+        coord_labels = pointwise_data[0].coords.to_index().values
+        if isinstance(coord_labels[0], tuple):
+            fmt = ", ".join(["{}" for _ in coord_labels[0]])
+            coord_labels[:] = [fmt.format(*x) for x in coord_labels]
+        else:
+            coord_labels[:] = ["{}".format(s) for s in coord_labels]
 
     if numvars < 2:
         raise Exception("Number of models to compare must be 2 or greater.")
 
     if numvars == 2:
-        (figsize, ax_labelsize, _, xt_labelsize, _, _) = _scale_fig_size(
+        (figsize, ax_labelsize, titlesize, xt_labelsize, _, _) = _scale_fig_size(
             figsize, textsize, numvars - 1, numvars - 1
         )
 
         if ax is None:
-            _, ax = plt.subplots(figsize=figsize, constrained_layout=True)
+            fig, ax = plt.subplots(figsize=figsize, constrained_layout=True)
 
-        ax.plot(pointwise_data[0]-pointwise_data[1], **plot_kwargs)
+        ax.scatter(xdata, pointwise_data[0] - pointwise_data[1], **plot_kwargs)
 
-        ax.set_xlabel("{}".format(models[0]), fontsize=ax_labelsize, wrap=True)
-        ax.set_ylabel("{}".format(models[1]), fontsize=ax_labelsize, wrap=True)
+        ax.set_title("{} - {}".format(*models), fontsize=titlesize, wrap=True)
+        ax.set_ylabel("ELPD difference", fontsize=ax_labelsize, wrap=True)
         ax.tick_params(labelsize=xt_labelsize)
+        if xlabels:
+            set_xticklabels(ax, coord_labels)
+            fig.autofmt_xdate()
 
     else:
-        (figsize, ax_labelsize, _, xt_labelsize, _, _) = _scale_fig_size(
+        (figsize, ax_labelsize, titlesize, xt_labelsize, _, _) = _scale_fig_size(
             figsize, textsize, numvars - 2, numvars - 2
         )
 
         if ax is None:
-            _, ax = plt.subplots(
-                numvars - 1, numvars - 1, figsize=figsize, constrained_layout=True
-            )
+            fig, ax = plt.subplots(numvars - 1, numvars - 1, figsize=figsize, constrained_layout=True)
 
         for i in range(0, numvars - 1):
             var1 = pointwise_data[i]
@@ -131,21 +139,36 @@ def plot_pointwise_elpd(
 
                 var2 = pointwise_data[j + 1]
 
-                ax[j, i].plot(var1-var2, **plot_kwargs)
+                ax[j, i].scatter(xdata, var1 - var2, **plot_kwargs)
 
                 if j + 1 != numvars - 1:
                     ax[j, i].axes.get_xaxis().set_major_formatter(NullFormatter())
-                else:
-                    ax[j, i].set_xlabel(
-                        "{}".format(models[i]), fontsize=ax_labelsize, wrap=True
-                    )
+                    ax[j, i].set_xticks([])
+                elif xlabels:
+                    set_xticklabels(ax[j, i], coord_labels)
+
                 if i != 0:
                     ax[j, i].axes.get_yaxis().set_major_formatter(NullFormatter())
+                    ax[j, i].set_yticks([])
                 else:
-                    ax[j, i].set_ylabel(
-                        "{}".format(models[j + 1]), fontsize=ax_labelsize, wrap=True
-                    )
+                    ax[j, i].set_ylabel("ELPD difference", fontsize=ax_labelsize, wrap=True)
 
                 ax[j, i].tick_params(labelsize=xt_labelsize)
+                ax[j, i].set_title("{} - {}".format(models[i], models[j+1]), fontsize=titlesize, wrap=True)
+        if xlabels:
+            fig.autofmt_xdate()
 
     return ax
+
+
+def set_xticklabels(ax, coord_labels):
+    xlim = ax.get_xlim()
+    ax.xaxis.get_major_locator().set_params(nbins=9, steps=[1, 2, 5, 10])
+    xticks = ax.get_xticks().astype(np.int64)
+    xticks = xticks[(xticks > xlim[0]) & (xticks < xlim[1])]
+    if len(xticks) > len(coord_labels):
+        ax.set_xticks(np.arange(len(coord_labels)))
+        ax.set_xticklabels(coord_labels)
+    else:
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(coord_labels[xticks])
