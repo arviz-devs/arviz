94df0f3837eb301412111e83c6c67cbd11eca94d
Banzee <aniruddhab8@gmail.com>
2019-06-17 21:10:11 +0530

    Added tests

--- arviz/tests/test_utils.py
+++ arviz/tests/test_utils.py
@@ -134,3 +134,66 @@ def test_conditional_jit_numba_decorator_keyword(monkeypatch):
 )
 def test_format_sig_figs(value, default, expected):
     assert format_sig_figs(value, default=default) == expected
+
+
+@pytest.fixture(scope="function")
+def utils_with_numba_import_fail(monkeypatch):
+    """Patch numba in utils so when its imported it raises ImportError"""
+    failed_import = Mock()
+    failed_import.side_effect = ImportError
+
+    from arviz import utils
+
+    monkeypatch.setattr(utils.importlib, "import_module", failed_import)
+    return utils
+
+
+def test_utils_fixture(utils_with_numba_import_fail):
+    """Test of utils fixture to ensure mock is applied correctly"""
+
+    # If Numba doesn't exist in dev environment this will raise an ImportError
+    import numba  # pylint: disable=unused-import,W0612
+
+    with pytest.raises(ImportError):
+        utils_with_numba_import_fail.importlib.import_module("numba")
+
+
+def test_conditional_vect_numba_decorator():
+    """Tests to see if Numba is used.
+
+    Test can be distinguished from test_conditional_jit_decorator_no_numba
+    by use of debugger or coverage tool
+    """
+    from arviz import utils
+
+    @utils.conditional_vect
+    def func():
+        return "Numba used"
+
+    assert func() == "Numba used"
+
+
+def test_conditional_jit_numba_decorator_keyword(monkeypatch):
+    """Checks else statement and JIT keyword argument"""
+    from arviz import utils
+
+    # Mock import lib to return numba with hit method which returns a function that returns kwargs
+    numba_mock = Mock()
+    monkeypatch.setattr(utils.importlib, "import_module", lambda x: numba_mock)
+
+    def vectorize(**kwargs):
+        """overwrite numba.jit function"""
+        return lambda x: (x(), kwargs)
+
+    numba_mock.vect = vectorize
+
+    @utils.conditional_vect(keyword_argument="A keyword argument")
+    def placeholder_func():
+        """This function does nothing"""
+        return "output"
+
+    # pylint: disable=unpacking-non-sequence
+    function_results, wrapper_result = placeholder_func
+    assert wrapper_result == {"keyword_argument": "A keyword argument"}
+    assert function_results == "output"
+
