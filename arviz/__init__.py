cafd55561bc1fd3f8d1e2e6c74e0607b0a70eca5
Banzee <aniruddhab8@gmail.com>
2019-06-22 02:44:40 +0530

    Fixed init

--- arviz/__init__.py
+++ arviz/__init__.py
@@ -4,7 +4,6 @@ __version__ = "0.4.1"
 
 import os
 import logging
-import importlib
 from matplotlib.pyplot import style
 
 
@@ -15,28 +14,6 @@ style.core.reload_library()
 
 # Configure logging before importing arviz internals
 _log = logging.getLogger("arviz")
-
-
-def numba_check():
-    """Check if numba is installed."""
-    numba = importlib.util.find_spec("numba")
-    return numba is not None
-
-
-class Numba:
-    """A class to toggle numba states."""
-
-    numba_flag = numba_check()
-
-    @classmethod
-    def disable_numba(cls):
-        """To disable numba."""
-        cls.numba_flag = False
-
-    @classmethod
-    def enable_numba(cls):
-        """To enable numba."""
-        cls.numba_flag = True
 
 
 if not logging.root.handlers:
