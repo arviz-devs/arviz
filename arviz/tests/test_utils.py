f9bb998030d24e8071f8d4cc2cdaf90811818a84
Banzee <aniruddhab8@gmail.com>
2019-06-28 20:04:36 +0530

    Added plots

--- arviz/tests/test_utils.py
+++ arviz/tests/test_utils.py
@@ -3,10 +3,13 @@ Tests for arviz.utils.
 """
 # pylint: disable=redefined-outer-name, no-member
 from unittest.mock import Mock
+import importlib
 import numpy as np
 import pytest
-from ..utils import _var_names, format_sig_figs
+
+from ..utils import _var_names, format_sig_figs, numba_check, Numba, _numba_var
 from ..data import load_arviz_data, from_dict
+from ..stats.stats_utils import stats_variance_2d as svar
 
 
 @pytest.fixture(scope="session")
@@ -81,6 +84,20 @@ def test_conditional_jit_decorator_no_numba(utils_with_numba_import_fail):
     assert func() == "Numba not used"
 
 
+def test_conditional_vect_decorator_no_numba(utils_with_numba_import_fail):
+    """Tests to see if Numba vectorize code block is skipped with Import Failure
+
+    Test can be distinguished from test_conditional_vect__numba_decorator
+    by use of debugger or coverage tool
+    """
+
+    @utils_with_numba_import_fail.conditional_vect
+    def func():
+        return "Numba not used"
+
+    assert func() == "Numba not used"
+
+
 def test_conditional_jit_numba_decorator():
     """Tests to see if Numba is used.
 
@@ -91,9 +108,9 @@ def test_conditional_jit_numba_decorator():
 
     @utils.conditional_jit
     def func():
-        return "Numba used"
+        return True
 
-    assert func() == "Numba used"
+    assert func()
 
 
 def test_conditional_jit_numba_decorator_keyword(monkeypatch):
@@ -134,16 +151,83 @@ def test_conditional_jit_numba_decorator_keyword(monkeypatch):
 )
 def test_format_sig_figs(value, default, expected):
     assert format_sig_figs(value, default=default) == expected
-    
+
+
+def test_conditional_vect_numba_decorator():
+    """Tests to see if Numba is used.
+
+    Test can be distinguished from test_conditional_jit_decorator_no_numba
+    by use of debugger or coverage tool
+    """
+    from arviz import utils
+
+    @utils.conditional_vect
+    def func(a_a, b_b):
+        return a_a + b_b
+
+    value_one = np.random.randn(10)
+    value_two = np.random.randn(10)
+    assert np.allclose(func(value_one, value_two), value_one + value_two)
+
+
+def test_conditional_vect_numba_decorator_keyword(monkeypatch):
+    """Checks else statement and vect keyword argument"""
+    from arviz import utils
+
+    # Mock import lib to return numba with hit method which returns a function that returns kwargs
+    numba_mock = Mock()
+    monkeypatch.setattr(utils.importlib, "import_module", lambda x: numba_mock)
+
+    def vectorize(**kwargs):
+        """overwrite numba.vectorize function"""
+        return lambda x: (x(), kwargs)
+
+    numba_mock.vectorize = vectorize
+
+    @utils.conditional_vect(keyword_argument="A keyword argument")
+    def placeholder_func():
+        """This function does nothing"""
+        return "output"
+
+    # pylint: disable=unpacking-non-sequence
+    function_results, wrapper_result = placeholder_func
+    assert wrapper_result == {"keyword_argument": "A keyword argument"}
+    assert function_results == "output"
+
+
 def test_numba_check():
-    test_data = np.random.rand(200).reshape(10, 20)
+    """Test for numba_check"""
+    numba = importlib.util.find_spec("numba")
+    flag = numba is not None
+    assert flag == numba_check()
 
-    def placeholder(x):
-        """A simple function to test numba enhancement"""
-        dummy = 0
-        for i in range(0, x.shape[0]):
-            dummy = dummy + np.log10(x[i, i])
-        return dummy
 
-    assert numba_check(placeholder, test_data) >= 1
->>>>>>>  # Added tests.
+def test_numba_utils():
+    """Test for class Numba."""
+    flag = Numba.numba_flag
+    assert flag == numba_check()
+    Numba.disable_numba()
+    val = Numba.numba_flag
+    assert not val
+    Numba.enable_numba()
+    val = Numba.numba_flag
+    assert val
+    assert flag == Numba.numba_flag
+
+
+@pytest.mark.parametrize("axis", (0, 1))
+@pytest.mark.parametrize("ddof", (0, 1))
+def test_numba_var(axis, ddof):
+    """Method to test numba_var."""
+    flag = Numba.numba_flag
+    data_1 = np.random.randn(100, 100)
+    data_2 = np.random.rand(100)
+    with_numba_1 = _numba_var(svar, np.var, data_1, axis=axis, ddof=ddof)
+    with_numba_2 = _numba_var(svar, np.var, data_2, ddof=ddof)
+    Numba.disable_numba()
+    non_numba_1 = _numba_var(svar, np.var, data_1, axis=axis, ddof=ddof)
+    non_numba_2 = _numba_var(svar, np.var, data_2, ddof=ddof)
+    Numba.enable_numba()
+    assert flag == Numba.numba_flag
+    assert np.allclose(with_numba_1, non_numba_1)
+    assert np.allclose(with_numba_2, non_numba_2)
