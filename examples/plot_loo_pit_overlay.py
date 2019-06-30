528d7095b96e62d473f887710c80f874bb31a825
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2019-06-30 02:38:28 +0200

    Add examples

---
+++ examples/plot_loo_pit_overlay.py
@@ -0,0 +1,13 @@
+"""
+LOO-PIT Overlay Plot
+=========
+
+_thumb: .5, .7
+"""
+import arviz as az
+
+az.style.use('arviz-darkgrid')
+
+idata = az.load_arviz_data('non_centered_eight')
+
+az.plot_loo_pit(idata=idata, y="obs", color="indigo")
