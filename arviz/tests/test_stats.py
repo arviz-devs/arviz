f8ace6e7c7ffd2593289966db52bb97f93000e9e
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2019-07-07 02:26:52 +0200

    Reduce apply_test_function test cases

--- arviz/tests/test_stats.py
+++ arviz/tests/test_stats.py
@@ -429,25 +429,25 @@ def test_loo_pit_bad_input_shape(incompatibility):
             loo_pit(y=y, y_hat=y_hat[:, :100], log_weights=log_weights)
 
 
-@pytest.mark.parametrize("group", ["both", "observed_data", "posterior_predictive"])
-@pytest.mark.parametrize(
-    "var_names",
-    [
-        None,
-        {"observed_data": "obs"},
-        {"both": "obs"},
-        {"posterior_predictive": "obs"},
-        {"both": "obs", "posterior": ["theta", "mu"]},
-    ],
-)
 @pytest.mark.parametrize("pointwise", [True, False])
-@pytest.mark.parametrize("out_data_shape", [None, "shape"])
-@pytest.mark.parametrize("out_pp_shape", [None, "shape"])
 @pytest.mark.parametrize("inplace", [True, False])
+@pytest.mark.parametrize("kwargs", [
+    {},
+    {"group": "posterior_predictive", "var_names": {"posterior_predictive": "obs"}},
+    {"group": "observed_data", "var_names": {"both": "obs"}, "out_data_shape": "shape"},
+    {"var_names": {"both": "obs", "posterior": ["theta", "mu"]}},
+    {"group": "observed_data", "out_name_data": "T_name"},
+])
 def test_apply_test_function(
-    centered_eight, group, var_names, pointwise, out_data_shape, out_pp_shape, inplace
+    centered_eight, pointwise, inplace, kwargs
 ):
+    """Test some usual call cases of apply_test_function"""
     centered_eight = deepcopy(centered_eight)
+    group = kwargs.get("group", "both")
+    var_names = kwargs.get("var_names", None)
+    out_data_shape = kwargs.get("out_data_shape", None)
+    out_pp_shape = kwargs.get("out_pp_shape", None)
+    out_name_data = kwargs.get("out_name_data", "T")
     if out_data_shape == "shape":
         out_data_shape = (8,) if pointwise else ()
     if out_pp_shape == "shape":
@@ -459,6 +459,7 @@ def test_apply_test_function(
         group=group,
         var_names=var_names,
         pointwise=pointwise,
+        out_name_data=out_name_data,
         out_data_shape=out_data_shape,
         out_pp_shape=out_pp_shape,
     )
@@ -466,9 +467,9 @@ def test_apply_test_function(
         assert idata is idata_out
 
     if group == "both":
-        test_dict = {"observed_data": "T", "posterior_predictive": ["T"]}
+        test_dict = {"observed_data": ["T"], "posterior_predictive": ["T"]}
     else:
-        test_dict = {group: "T"}
+        test_dict = {group: [kwargs.get("out_name_data", "T")]}
 
     fails = check_multiple_attrs(test_dict, idata_out)
     assert not fails
