05b02a903efbbe11b55f87cba1c7b3228ce2806e
Banzee <aniruddhab8@gmail.com>
2019-07-15 21:25:47 +0530

    Added benchmark tests

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -2,6 +2,9 @@
 # See "Writing benchmarks" in the asv docs for more information.
 import numpy as np
 from scipy.stats import circstd
+from scipy.sparse import coo_matrix
+import scipy.signal as ss
+import warnings
 
 
 class Hist:
@@ -85,33 +88,246 @@ class CircStd:
             return circstd(data)
 
 
-class Cov:
-    def time_numpy_cov(self):
+class Fast_Kde_1d:
+    def time_fast_kde(self):
+
         try:
-            data = np.random.randn(1000, 100)
+            x = np.random.randn(10000, 100)
+            import numba
+
+            def _fast_kde(x, cumulative=False, bw=4.5, xmin=None, xmax=None):
+                x = np.asarray(x, dtype=float)
+                x = x[np.isfinite(x)]
+                if x.size == 0:
+                    warnings.warn("kde plot failed, you may want to check your data")
+                    return np.array([np.nan]), np.nan, np.nan
+
+                len_x = len(x)
+                n_points = 200 if (xmin or xmax) is None else 500
+
+                if xmin is None:
+                    xmin = np.min(x)
+                if xmax is None:
+                    xmax = np.max(x)
+
+                assert np.min(x) >= xmin
+                assert np.max(x) <= xmax
+
+                log_len_x = np.log(len_x) * bw
+
+                n_bins = min(int(len_x ** (1 / 3) * log_len_x * 2), n_points)
+                if n_bins < 2:
+                    warnings.warn("kde plot failed, you may want to check your data")
+                    return np.array([np.nan]), np.nan, np.nan
+
+                d_x = (xmax - xmin) / (n_bins - 1)
+                grid = _histogram(x, n_bins, range_hist=(xmin, xmax))
+
+                scotts_factor = len_x ** (-0.2)
+                kern_nx = int(scotts_factor * 2 * np.pi * log_len_x)
+                kernel = ss.gaussian(kern_nx, scotts_factor * log_len_x)
+
+                npad = min(n_bins, 2 * kern_nx)
+                grid = np.concatenate([grid[npad:0:-1], grid, grid[n_bins : n_bins - npad : -1]])
+                density = ss.convolve(grid, kernel, mode="same", method="direct")[
+                    npad : npad + n_bins
+                ]
+                norm_factor = len_x * d_x * (2 * np.pi * log_len_x ** 2 * scotts_factor ** 2) ** 0.5
+
+                density /= norm_factor
+
+                if cumulative:
+                    density = density.cumsum() / density.sum()
+
+                return density, xmin, xmax
+
+            @numba.njit(cache=True)
+            def _histogram(x, n_bins, range_hist=None):
+                grid, _ = np.histogram(x, bins=n_bins, range=range_hist)
+                return grid
+
+            return _fast_kde(x)
+
+        except ImportError:
+
+            x = np.random.randn(10000, 100)
+
+            def _fast_kde(x, cumulative=False, bw=4.5, xmin=None, xmax=None):
+                x = np.asarray(x, dtype=float)
+                x = x[np.isfinite(x)]
+                if x.size == 0:
+                    warnings.warn("kde plot failed, you may want to check your data")
+                    return np.array([np.nan]), np.nan, np.nan
+
+                len_x = len(x)
+                n_points = 200 if (xmin or xmax) is None else 500
+
+                if xmin is None:
+                    xmin = np.min(x)
+                if xmax is None:
+                    xmax = np.max(x)
+
+                assert np.min(x) >= xmin
+                assert np.max(x) <= xmax
+
+                log_len_x = np.log(len_x) * bw
+
+                n_bins = min(int(len_x ** (1 / 3) * log_len_x * 2), n_points)
+                if n_bins < 2:
+                    warnings.warn("kde plot failed, you may want to check your data")
+                    return np.array([np.nan]), np.nan, np.nan
+
+                d_x = (xmax - xmin) / (n_bins - 1)
+                grid = _histogram(x, n_bins, range_hist=(xmin, xmax))
+
+                scotts_factor = len_x ** (-0.2)
+                kern_nx = int(scotts_factor * 2 * np.pi * log_len_x)
+                kernel = ss.gaussian(kern_nx, scotts_factor * log_len_x)
+
+                npad = min(n_bins, 2 * kern_nx)
+                grid = np.concatenate([grid[npad:0:-1], grid, grid[n_bins : n_bins - npad : -1]])
+                density = ss.convolve(grid, kernel, mode="same", method="direct")[
+                    npad : npad + n_bins
+                ]
+                norm_factor = len_x * d_x * (2 * np.pi * log_len_x ** 2 * scotts_factor ** 2) ** 0.5
+
+                density /= norm_factor
+
+                if cumulative:
+                    density = density.cumsum() / density.sum()
+
+                return density, xmin, xmax
+
+            def _histogram(x, n_bins, range_hist=None):
+                grid, _ = np.histogram(x, bins=n_bins, range=range_hist)
+                return grid
+
+            return _fast_kde(x)
+
+
+class Fast_KDE_2d:
+    def time_fast_kde_2d(self):
+        try:
+            x = np.random.randn(10000, 100)
+            y = np.random.randn(10000, 100)
             import numba
 
             @numba.njit
-            def cov():
+            def _cov(data):
                 return np.cov(data)
 
-        except ImportError:
-            data = np.random.randn(1000, 100)
-            return np.cov(data)
-
-
-class Stack:
-    def time_stack(self):
-        try:
-            x = np.random.randn(10000, 1000)
-            y = np.random.randn(10000, 1000)
-            import numba
-
             @numba.njit
-            def stack(x, y):
-                return np.stack((x, y))
+            def _stack(x, y):
+                return np.vstack((x, y))
+
+            def _fast_kde_2d(x, y, gridsize=(128, 128), circular=False):
+                x = np.asarray(x, dtype=float)
+                x = x[np.isfinite(x)]
+                y = np.asarray(y, dtype=float)
+                y = y[np.isfinite(y)]
+
+                xmin, xmax = x.min(), x.max()
+                ymin, ymax = y.min(), y.max()
+
+                len_x = len(x)
+                weights = np.ones(len_x)
+                n_x, n_y = gridsize
+
+                d_x = (xmax - xmin) / (n_x - 1)
+                d_y = (ymax - ymin) / (n_y - 1)
+
+                xyi = _stack(x, y).T
+                xyi -= [xmin, ymin]
+                xyi /= [d_x, d_y]
+                xyi = np.floor(xyi, xyi).T
+
+                scotts_factor = len_x ** (-1 / 6)
+                cov = _cov(xyi)
+                std_devs = np.diag(cov ** 0.5)
+                kern_nx, kern_ny = np.round(scotts_factor * 2 * np.pi * std_devs)
+
+                inv_cov = np.linalg.inv(cov * scotts_factor ** 2)
+
+                x_x = np.arange(kern_nx) - kern_nx / 2
+                y_y = np.arange(kern_ny) - kern_ny / 2
+                x_x, y_y = np.meshgrid(x_x, y_y)
+
+                kernel = _stack(x_x.flatten(), y_y.flatten())
+                kernel = np.dot(inv_cov, kernel) * kernel
+                kernel = np.exp(-kernel.sum(axis=0) / 2)
+                kernel = kernel.reshape((int(kern_ny), int(kern_nx)))
+
+                boundary = "wrap" if circular else "symm"
+
+                grid = coo_matrix((weights, xyi), shape=(n_x, n_y)).toarray()
+                grid = ss.convolve2d(grid, kernel, mode="same", boundary=boundary)
+
+                norm_factor = np.linalg.det(2 * np.pi * cov * scotts_factor ** 2)
+                norm_factor = len_x * d_x * d_y * norm_factor ** 0.5
+
+                grid /= norm_factor
+
+                return grid, xmin, xmax, ymin, ymax
+
+            return _fast_kde_2d(x, y)
 
         except ImportError:
-            x = np.random.randn(10000, 1000)
-            y = np.random.randn(10000, 1000)
-            return np.stack((x, y))
+            x = np.random.randn(10000, 100)
+            y = np.random.randn(10000, 100)
+
+            def _cov(data):
+                return np.cov(data)
+
+            def _stack(x, y):
+                return np.vstack((x, y))
+
+            def _fast_kde_2d(x, y, gridsize=(128, 128), circular=False):
+                x = np.asarray(x, dtype=float)
+                x = x[np.isfinite(x)]
+                y = np.asarray(y, dtype=float)
+                y = y[np.isfinite(y)]
+
+                xmin, xmax = x.min(), x.max()
+                ymin, ymax = y.min(), y.max()
+
+                len_x = len(x)
+                weights = np.ones(len_x)
+                n_x, n_y = gridsize
+
+                d_x = (xmax - xmin) / (n_x - 1)
+                d_y = (ymax - ymin) / (n_y - 1)
+
+                xyi = _stack(x, y).T
+                xyi -= [xmin, ymin]
+                xyi /= [d_x, d_y]
+                xyi = np.floor(xyi, xyi).T
+
+                scotts_factor = len_x ** (-1 / 6)
+                cov = _cov(xyi)
+                std_devs = np.diag(cov ** 0.5)
+                kern_nx, kern_ny = np.round(scotts_factor * 2 * np.pi * std_devs)
+
+                inv_cov = np.linalg.inv(cov * scotts_factor ** 2)
+
+                x_x = np.arange(kern_nx) - kern_nx / 2
+                y_y = np.arange(kern_ny) - kern_ny / 2
+                x_x, y_y = np.meshgrid(x_x, y_y)
+
+                kernel = _stack(x_x.flatten(), y_y.flatten())
+                kernel = np.dot(inv_cov, kernel) * kernel
+                kernel = np.exp(-kernel.sum(axis=0) / 2)
+                kernel = kernel.reshape((int(kern_ny), int(kern_nx)))
+
+                boundary = "wrap" if circular else "symm"
+
+                grid = coo_matrix((weights, xyi), shape=(n_x, n_y)).toarray()
+                grid = ss.convolve2d(grid, kernel, mode="same", boundary=boundary)
+
+                norm_factor = np.linalg.det(2 * np.pi * cov * scotts_factor ** 2)
+                norm_factor = len_x * d_x * d_y * norm_factor ** 0.5
+
+                grid /= norm_factor
+
+                return grid, xmin, xmax, ymin, ymax
+
+            return _fast_kde_2d(x, y)
