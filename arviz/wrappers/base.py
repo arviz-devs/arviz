24f329ffcae1f73351ba166c1660fa250448f3dc
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2019-07-29 14:10:56 +0200

    modify api for pystan compatibility

--- arviz/wrappers/base.py
+++ arviz/wrappers/base.py
@@ -1,4 +1,8 @@
+import numpy as np
+
+
 from ..data import InferenceData
+from ..stats import wrap_xarray_ufunc as _wrap_xarray_ufunc
 
 
 class SamplingWrapper:
@@ -17,20 +21,24 @@ class SamplingWrapper:
     idata_orig: InferenceData, optional
         Original InferenceData object.
     log_like_fun: callable, optional
-        For simple cases where the pointwise log likelihood is a Python function, this function will be used to
-        calculate the log likelihood. Otherwise, ``point_log_likelihood`` method must be implemented.
+        For simple cases where the pointwise log likelihood is a Python function, this
+        function will be used to calculate the log likelihood. Otherwise,
+        ``point_log_likelihood`` method must be implemented.
     sample_kwargs: dict, optional
-        Sampling kwargs are stored as class attributes for their usage in the ``sample`` method.
+        Sampling kwargs are stored as class attributes for their usage in the ``sample``
+        method.
     idata_kwargs: dict, optional
         kwargs are stored as class attributes to be used in the ``get_inference_data`` method.
     """
 
-    def __init__(self, model, idata_orig=None, log_like_fun=None, sample_kwargs=None, idata_kwargs=None):
+    def __init__(
+        self, model, idata_orig=None, log_like_fun=None, sample_kwargs=None, idata_kwargs=None
+    ):
         self.model = model
 
         if not isinstance(idata_orig, InferenceData) or idata_orig is not None:
             raise TypeError("idata_orig must be of InferenceData type or None")
-        self.idata_orig = idata
+        self.idata_orig = idata_orig
 
         if log_like_fun is None or callable(log_like_fun):
             self.log_like_fun = log_like_fun
@@ -42,6 +50,9 @@ class SamplingWrapper:
 
     def sel_observations(self, idx):
         """Select a subset of the observations in idata_orig.
+
+        *Not implemented*: This method must be implemented by the SamplingWrapper subclasses.
+        It is documented here to show its format and call signature.
 
         Parameters
         ----------
@@ -60,6 +71,9 @@ class SamplingWrapper:
     def sample(self, modified_observed_data):
         """Sample ``self.model`` on the ``modified_observed_data`` subset.
 
+        *Not implemented*: This method must be implemented by the SamplingWrapper subclasses.
+        It is documented here to show its format and call signature.
+
         Parameters
         ----------
         modified_observed_data
@@ -74,6 +88,9 @@ class SamplingWrapper:
 
     def get_inference_data(self, fitted_model):
         """Convert the ``fitted_model`` to an InferenceData object.
+
+        *Not implemented*: This method must be implemented by the SamplingWrapper subclasses.
+        It is documented here to show its format and call signature.
 
         Parameters
         ----------
@@ -99,12 +116,44 @@ class SamplingWrapper:
         Returns
         -------
         point_log_likelihood: float
-            Value of the log likelihood of ``observation`` given ``parameters`` according to ``self.model``"""
+            Value of the log likelihood of ``observation`` given ``parameters``
+            according to ``self.model``"""
         if self.log_like_fun is None:
-            raise NotImplementedError("If log_like_fun is None, point_log_likelihood method must "
-                                      "be implemented for each subclass")
-        else:
-            return self.log_like_fun(observation, parameters)
+            raise NotImplementedError(
+                "If log_like_fun is None, point_log_likelihood method must "
+                "be implemented for each subclass"
+            )
+        return self.log_like_fun(observation, parameters)
+
+    def log_likelihood__i(self, excluded_obs, idata__i):
+        r"""Get the log likelilhood samples :math:`\log p_{post(-i)}(y_i)`.
+
+        Calculate the log likelihood of the data contained in excluded_obs using the
+        model fitted with this data excluded, the results of which are stored in ``idata__i``.
+
+        Parameters
+        ----------
+        excluded_obs
+            Observations for which to calculate their log likelihood
+        idata__i: InferenceData
+            Inference results of refitting the data excluding some observations.
+
+        Returns
+        -------
+        log_likelihood: xr.Dataarray
+            Log likelihood of ``excluded_obs`` evaluated at each of the posterior samples
+            stored in ``idata__i``.
+        """
+        ndraws = idata__i.posterior.dims["draw"]
+        nchains = idata__i.posterior.dims["chain"]
+        log_like_idx = _wrap_xarray_ufunc(
+            lambda pars: self.point_log_likelihood(excluded_obs, pars),
+            idata__i.posterior.to_array(),
+            func_kwargs={"out": np.empty((nchains, ndraws))},
+            ufunc_kwargs={"n_dims": 1, "ravel": False},
+            input_core_dims=[["variable"]],
+        )
+        return log_like_idx
 
     def _check_implemented_methods(self, methods):
         """Check that all methods listed are implemented.
@@ -118,13 +167,17 @@ class SamplingWrapper:
         methods: list
             Check all elements in methods are implemented.
         """
-        supported_methods = ("sel_observations", "sample", "get_inference_data", "point_log_likelihood")
+        supported_methods = (
+            "sel_observations",
+            "sample",
+            "get_inference_data",
+            "point_log_likelihood",
+        )
         bad_methods = [method for method in methods if method not in supported_methods]
         if bad_methods:
             raise ValueError(
                 "Not all method(s) in {} supported. Supported methods in SamplingWrapper "
                 "subclasses are:{}".format(bad_methods, supported_methods)
-
             )
         not_implemented = []
         for method in supported_methods[:-1]:
@@ -133,13 +186,13 @@ class SamplingWrapper:
                     getattr(self, method)(1)
                 except NotImplementedError:
                     not_implemented.append(method)
-                except:
+                except:  # pylint: disable=bare-except
                     pass
         if "point_log_likelihood" in methods:
             try:
                 self.point_log_likelihood(1, 1)
             except NotImplementedError:
                 not_implemented.append("point_log_likelihood")
-            except:
+            except:  # pylint: disable=bare-except
                 pass
         return not_implemented
