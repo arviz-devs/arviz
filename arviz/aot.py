5f72827849ed2b800a85bcd820451059c1010097
Banzee <aniruddhab8@gmail.com>
2019-08-10 16:50:54 +0530

    More diffs

--- arviz/aot.py
+++ arviz/aot.py
@@ -1,11 +1,11 @@
 from numba.pycc import CC
-import numba
 import numpy as np
+import numba
 
 module = CC("numba-compilations")
 
 
-@numba.njit
+@module.export("stats_variance_1d", "(f8[:],i8)")
 def stats_variance_1d(data, ddof=0):
     a_a, b_b = 0, 0
     for i in data:
@@ -16,33 +16,47 @@ def stats_variance_1d(data, ddof=0):
     return var
 
 
-@module.export("stats_variance_2d", "f8[:](f8[:],i8,i8)")
+@numba.njit
+def stats_variance_1d_2d(data, ddof=0):
+    a_a, b_b = 0, 0
+    for i in data:
+        a_a = a_a + i
+        b_b = b_b + i * i
+    var = b_b / (len(data)) - ((a_a / (len(data))) ** 2)
+    var = var * (len(data) / (len(data) - ddof))
+    return var
+
+
+@module.export("stats_variance_2d", "f8[:](f8[:,:],i8,i8)")
 def stats_variance_2d(data, ddof=0, axis=1):
-    if data.ndim == 1:
-        return stats_variance_1d(data, ddof=ddof)
     a_a, b_b = data.shape
     if axis == 1:
         var = np.zeros(a_a)
         for i in range(a_a):
-            var[i] = stats_variance_1d(data[i], ddof=ddof)
+            var[i] = stats_variance_1d_2d(data[i], ddof=ddof)
         return var
     else:
         var = np.zeros(b_b)
         for i in range(b_b):
-            var[i] = stats_variance_1d(data[:, i], ddof=ddof)
+            var[i] = stats_variance_1d_2d(data[:, i], ddof=ddof)
         return var
 
 
 # Remember to flatten the array before when using this method
-@module.export("histogram_stats", "f8[:](f8[:],f8[:]")
+@module.export("histogram_stats", "i8[:](f8[:],f8[:])")
+@module.export("histogram_stats", "f8[:](f8[:],f8[:])")
+@module.export("histogram_stats", "f8[:](f8[:],i8[:])")
+@module.export("histogram_stats", "f8[:](f8[:],f8)")
+@module.export("histogram_stats", "f8[:](f8[:],i8)")
 def histogram_stats(data, bins):
-    return np.histogram(data, bins=bins)[0]
+    grid, _ = np.histogram(data, bins=bins)
+    return grid
 
 
 # Remember to flatten the array before when using this method
-@module.export("histogram_kde", "f8[:](f8[:],i8,(f8,f8)")
-def histogram_plots(x, n_bins, range_hist=None):
-    grid, _ = np.histogram(x, bins=n_bins, range=range_hist)
+@module.export("histogram_kde", "f8[:](f8[:],i8,f8,f8)")
+def histogram_plots(x, n_bins, xmax, xmin):
+    grid, _ = np.histogram(x, bins=n_bins, range=(xmax, xmin))
     return grid
 
 
@@ -60,3 +74,7 @@ def dot(x, y):
 def full(shape):
     """Jitting numpy full."""
     return np.full(shape, np.nan)
+
+
+if __name__ == "__main__":
+    module.compile()
