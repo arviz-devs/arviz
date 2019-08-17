0b7d41a56544b6d0632f04c67085bbb20e8bb250
Banzee <aniruddhab8@gmail.com>
2019-08-17 15:54:33 +0530

    lint

--- arviz/aot.py
+++ arviz/aot.py
@@ -4,8 +4,10 @@ import numba
 
 module = CC("numba_compilations")
 
+
 @module.export("stats_variance_1d", "(f8[:],i8)")
 def stats_variance_1d(data, ddof=0):
+    """Pre compiled method to get 1d variance."""
     a_a, b_b = 0, 0
     for i in data:
         a_a = a_a + i
@@ -16,7 +18,7 @@ def stats_variance_1d(data, ddof=0):
 
 
 @numba.njit
-def stats_variance_1d_2d(data, ddof=0):
+def _stats_variance_1d_2d(data, ddof=0):
     a_a, b_b = 0, 0
     for i in data:
         a_a = a_a + i
@@ -28,16 +30,17 @@ def stats_variance_1d_2d(data, ddof=0):
 
 @module.export("stats_variance_2d", "f8[:](f8[:,:],i8,i8)")
 def stats_variance_2d(data, ddof=0, axis=1):
+    """Pre compiled method to get 2d variance."""
     a_a, b_b = data.shape
     if axis == 1:
         var = np.zeros(a_a)
         for i in range(a_a):
-            var[i] = stats_variance_1d_2d(data[i], ddof=ddof)
+            var[i] = _stats_variance_1d_2d(data[i], ddof=ddof)
         return var
     else:
         var = np.zeros(b_b)
         for i in range(b_b):
-            var[i] = stats_variance_1d_2d(data[:, i], ddof=ddof)
+            var[i] = _stats_variance_1d_2d(data[:, i], ddof=ddof)
         return var
 
 
