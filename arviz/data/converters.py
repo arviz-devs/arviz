7fcc89aec4aac1666447148704a4d05aa2f52d71
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2019-10-24 00:33:27 +0300

    Update CmdStanPy class

--- arviz/data/converters.py
+++ arviz/data/converters.py
@@ -68,14 +68,14 @@ def convert_to_inference_data(obj, *, group="posterior", coords=None, dims=None,
         else:
             return InferenceData.from_netcdf(obj)
     elif (
-        obj.__class__.__name__ in {"StanFit4Model", "StanFit"}
+        obj.__class__.__name__ in {"StanFit4Model", "CmdStanMCMC"}
         or obj.__class__.__module__ == "stan.fit"
     ):
         if group == "sample_stats":
             kwargs["posterior"] = kwargs.pop(group)
         elif group == "sample_stats_prior":
             kwargs["prior"] = kwargs.pop(group)
-        if obj.__class__.__name__ == "StanFit":
+        if obj.__class__.__name__ == "CmdStanMCMC":
             return from_cmdstanpy(**kwargs)
         else:  # pystan or pystan3
             return from_pystan(**kwargs)
