e4cbe7a0db9508b5e1f74a752814faa4385f5688
ahartikainen <ahartikainen@github.com>
2019-11-11 23:10:02 +0200

    implement trace, dist, kde examples for bokeh

---
+++ examples/bokeh_plot_dist.py
@@ -0,0 +1,23 @@
+"""
+Dist Plot Bokeh
+===============
+
+_thumb: .2, .8
+"""
+import bokeh.plotting as bkp
+from bokeh.layouts import row
+import numpy as np
+import arviz as az
+
+a = np.random.poisson(4, 1000)
+b = np.random.normal(0, 1, 1000)
+
+figure_kwargs = dict(height=1000, width=400, output_backend="webgl")
+ax_poisson = bkp.figure(**figure_kwargs)
+ax_normal = bkp.figure(**figure_kwargs)
+
+az.plot_dist(a, color="black", label="Poisson", ax=ax_poisson, backend="bokeh", show=False)
+az.plot_dist(b, color="red", label="Gaussian", ax=ax_normal, backend="bokeh", show=False)
+
+axes = row(ax_poisson, ax_normal)
+bkp.show(axes)
