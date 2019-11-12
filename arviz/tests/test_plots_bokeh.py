c0c7201a0b400cb485a4f8750ce5207a3a5f7762
ahartikainen <ahartikainen@github.com>
2019-11-13 01:19:21 +0200

    update bokeh kw

--- arviz/tests/test_plots_bokeh.py
+++ arviz/tests/test_plots_bokeh.py
@@ -85,9 +85,9 @@ def test_plot_kde(continuous_model):
     "kwargs",
     [
         {"cumulative": True},
-        {"cumulative": True, "plot_kwargs": {"linestyle": "--"}},
+        {"cumulative": True, "plot_kwargs": {"line_style": "dashed"}},
         {"rug": True},
-        {"rug": True, "rug_kwargs": {"alpha": 0.2}},
+        {"rug": True, "rug_kwargs": {"line_alpha": 0.2}},
     ],
 )
 def test_plot_kde_cumulative(continuous_model, kwargs):
