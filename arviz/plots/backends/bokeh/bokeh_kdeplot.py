b95230d7174e8838d5530d9a562269afbf337ad5
ahartikainen <ahartikainen@github.com>
2019-11-16 02:12:00 +0200

    implement contour for bokeh

--- arviz/plots/backends/bokeh/bokeh_kdeplot.py
+++ arviz/plots/backends/bokeh/bokeh_kdeplot.py
@@ -1,8 +1,12 @@
 """Bokeh KDE Plot."""
 import bokeh.plotting as bkp
-from bokeh.models import ColumnDataSource, Dash
-from bokeh.palettes import Viridis11
+from bokeh.models import ColumnDataSource, Dash, Range1d
+from bokeh.palettes import Viridis
+import matplotlib._contour as _contour
+from matplotlib.colors import rgb2hex
 import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+from numbers import Integral
 import numpy as np
 import warnings
 
@@ -85,29 +89,93 @@ def _plot_kde_bokeh(
     else:
         if contour_kwargs is None:
             contour_kwargs = {}
-        contour_kwargs.setdefault("colors", "0.5")
         if contourf_kwargs is None:
             contourf_kwargs = {}
         if pcolormesh_kwargs is None:
             pcolormesh_kwargs = {}
 
+        g_s = complex(gridsize[0])
+        x_x, y_y = np.mgrid[xmin:xmax:g_s, ymin:ymax:g_s]
+
         if contour:
-            warnings.warn(
-                "contour and contourf are not yet impelented on bokeh backend."
-                "Use matplotlib interface to get these objects. Falls back to mpl",
-                UserWarning,
+
+            scaled_density, *scaled_density_args = _scale_axis(density)
+
+            contour_generator = _contour.QuadContourGenerator(
+                x_x, y_y, scaled_density, None, True, 0
             )
-        ax.image(
-            image=[density.T],
-            x=xmin,
-            y=ymin,
-            dw=(xmax - xmin) / density.shape[0],
-            dh=(ymax - ymin) / density.shape[1],
-            palette=Viridis11,
-            **pcolormesh_kwargs
-        )
-        ax.x_range.range_padding = ax.y_range.range_padding = 0
+
+            if "levels" in contour_kwargs:
+                levels = contour_kwargs.get("levels")
+            elif "levels" in contourf_kwargs:
+                levels = contourf_kwargs.get("levels")
+            else:
+                levels = 11
+
+            if isinstance(levels, Integral):
+                levels_scaled = np.linspace(0, 1, levels)
+                levels = _rescale_axis(levels_scaled, scaled_density_args)
+            else:
+                levels_scaled_nonclip = _scale_axis(np.asarray(levels), scaled_density_args)
+                levels_scaled = np.clip(levels_scaled_nonclip, 0, 1)
+
+            cmap = contourf_kwargs.pop("cmap", "viridis")
+            if isinstance(cmap, str):
+                cmap = get_cmap("viridis")
+            colors = [rgb2hex(item) for item in cmap(np.linspace(0, 1, len(levels_scaled) + 1))]
+
+            contour_kwargs.update(contourf_kwargs)
+            contour_kwargs.setdefault("line_color", "black")
+            contour_kwargs.setdefault("line_alpha", 0.25)
+            contour_kwargs.setdefault("fill_alpha", 1)
+
+            for i, (level, level_upper, color) in enumerate(
+                zip(levels_scaled[:-1], levels_scaled[1:], colors[1:])
+            ):
+                if not fill_last and (i == 0):
+                    continue
+                vertices, _ = contour_generator.create_filled_contour(level, level_upper)
+                for seg in vertices:
+                    ax.patch(*seg.T, fill_color=color, **contour_kwargs)
+
+            if fill_last:
+                ax.background_fill_color = colors[0]
+
+            ax.xgrid.grid_line_color = None
+            ax.ygrid.grid_line_color = None
+
+            ax.x_range = Range1d(xmin, xmax)
+            ax.y_range = Range1d(ymin, ymax)
+
+        else:
+            ax.image(
+                image=[density.T],
+                x=xmin,
+                y=ymin,
+                dw=(xmax - xmin) / density.shape[0],
+                dh=(ymax - ymin) / density.shape[1],
+                palette=Viridis[256],
+                **pcolormesh_kwargs
+            )
+            ax.x_range.range_padding = ax.y_range.range_padding = 0
 
     if show:
         bkp.show(ax)
     return ax
+
+
+def _scale_axis(arr, args=None):
+    if args:
+        amin, amax = args
+    else:
+        amin, amax = arr.min(), arr.max()
+    scaled_arr = arr - amin
+    scaled_arr /= amax - amin
+    return scaled_arr, amin, amax
+
+
+def _rescale_axis(arr, args):
+    amin, amax = args
+    rescaled_arr = arr * (amax - amin)
+    rescaled_arr += amin
+    return rescaled_arr
