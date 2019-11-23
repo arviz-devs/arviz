d1e7947275c09e830534a18a604641de7e530f48
ahartikainen <ahartikainen@github.com>
2019-11-23 02:23:43 +0200

    add elpd bokeh minimal tests

--- arviz/tests/test_plots_bokeh.py
+++ arviz/tests/test_plots_bokeh.py
@@ -16,11 +16,12 @@ from ..plots import (
     plot_autocorr,
     plot_compare,
     plot_density,
+    plot_elpd,
     plot_trace,
     plot_kde,
     plot_dist,
 )
-from ..stats import compare
+from ..stats import compare, loo, waic
 
 rcParams["data.load"] = "eager"
 
@@ -253,3 +254,28 @@ def test_plot_compare_no_ic(models):
 
     assert "comp_df must contain one of the following" in str(err.value)
     assert "['waic', 'loo']" in str(err.value)
+
+
+@pytest.mark.parametrize(
+    "kwargs", [{}, {"ic": "loo"}, {"xlabels": True, "scale": "log"},],
+)
+@pytest.mark.parametrize("add_model", [False, True])
+@pytest.mark.parametrize("use_elpddata", [False, True])
+def test_plot_elpd(models, add_model, use_elpddata, kwargs):
+    model_dict = {"Model 1": models.model_1, "Model 2": models.model_2}
+    if add_model:
+        model_dict["Model 3"] = create_model(seed=12)
+
+    if use_elpddata:
+        ic = kwargs.get("ic", "waic")
+        scale = kwargs.get("scale", "deviance")
+        if ic == "waic":
+            model_dict = {k: waic(v, scale=scale, pointwise=True) for k, v in model_dict.items()}
+        else:
+            model_dict = {k: loo(v, scale=scale, pointwise=True) for k, v in model_dict.items()}
+
+    axes = plot_elpd(model_dict, backend="bokeh", show=False, **kwargs)
+    assert np.any(axes)
+    if add_model:
+        assert axes.shape[0] == axes.shape[1]
+        assert axes.shape[0] == len(model_dict) - 1
