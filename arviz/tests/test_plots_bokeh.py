dc728e63d834705d2f9d7c8bffb4d449aa2845b0
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2019-11-23 16:42:34 +0100

    Add multidim tests on bokeh

--- arviz/tests/test_plots_bokeh.py
+++ arviz/tests/test_plots_bokeh.py
@@ -279,3 +279,27 @@ def test_plot_elpd(models, add_model, use_elpddata, kwargs):
     if add_model:
         assert axes.shape[0] == axes.shape[1]
         assert axes.shape[0] == len(model_dict) - 1
+
+@pytest.mark.parametrize(
+    "kwargs", [{}, {"ic": "loo"}, {"xlabels": True, "scale": "log"},],
+)
+@pytest.mark.parametrize("add_model", [False, True])
+@pytest.mark.parametrize("use_elpddata", [False, True])
+def test_plot_elpd_multidim(multidim_models, add_model, use_elpddata, kwargs):
+    model_dict = {"Model 1": multidim_models.model_1, "Model 2": multidim_models.model_2}
+    if add_model:
+        model_dict["Model 3"] = create_multidimensional_model(seed=12)
+
+    if use_elpddata:
+        ic = kwargs.get("ic", "waic")
+        scale = kwargs.get("scale", "deviance")
+        if ic == "waic":
+            model_dict = {k: waic(v, scale=scale, pointwise=True) for k, v in model_dict.items()}
+        else:
+            model_dict = {k: loo(v, scale=scale, pointwise=True) for k, v in model_dict.items()}
+
+    axes = plot_elpd(model_dict, backend="bokeh", show=False, **kwargs)
+    assert np.any(axes)
+    if add_model:
+        assert axes.shape[0] == axes.shape[1]
+        assert axes.shape[0] == len(model_dict) - 1
