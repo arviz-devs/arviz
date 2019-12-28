a1c9b3b765ca9fced33c079967de451ce4398631
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2019-12-28 14:09:56 +0200

    Bokeh interactive legend for pair_plot 2 variable case (#976)
    * add legend for bokeh pairplot 2 var case and add helper functions visible
    
    * move helper codes one level
    
    * fix
    
    * lint

--- arviz/plots/backends/__init__.py
+++ arviz/plots/backends/__init__.py
@@ -1,1 +1,42 @@
+# pylint: disable=no-member,invalid-name,redefined-outer-name
 """ArviZ plotting backends."""
+
+
+def output_notebook(*args, **kwargs):
+    """Wrap bokeh.plotting.output_notebook."""
+    import bokeh.plotting as bkp
+
+    return bkp.output_notebook(*args, **kwargs)
+
+
+def output_file(*args, **kwargs):
+    """Wrap bokeh.plotting.output_file."""
+    import bokeh.plotting as bkp
+
+    return bkp.output_file(*args, **kwargs)
+
+
+def _copy_docstring(lib, function):
+    """Extract docstring from function."""
+    import importlib
+
+    try:
+        module = importlib.import_module(lib)
+        func = getattr(module, function)
+        doc = func.__doc__
+    except ImportError:
+        doc = "Failed to import function {} from {}".format(function, lib)
+
+    return doc
+
+
+def ColumnDataSource(*args, **kwargs):
+    """Wrap bokeh.models.ColumnDataSource."""
+    from bokeh.models import ColumnDataSource
+
+    return ColumnDataSource(*args, **kwargs)
+
+
+output_notebook.__doc__ += "\n\n" + _copy_docstring("bokeh.plotting", "output_notebook")
+output_file.__doc__ += "\n\n" + _copy_docstring("bokeh.plotting", "output_file")
+ColumnDataSource.__doc__ += "\n\n" + _copy_docstring("bokeh.models", "ColumnDataSource")
--- arviz/plots/backends/bokeh/__init__.py
+++ arviz/plots/backends/bokeh/__init__.py
@@ -1,4 +1,4 @@
-# pylint: disable=no-member,invalid-name,redefined-outer-name, wrong-import-position
+# pylint: disable=wrong-import-position
 """Bokeh Plotting Backend."""
 from packaging import version
 
@@ -46,34 +46,6 @@ from .violinplot import plot_violin
 from ....rcparams import rcParams
 
 
-def output_notebook(*args, **kwargs):
-    """Wrap bokeh.plotting.output_notebook."""
-    import bokeh.plotting as bkp
-
-    return bkp.output_notebook(*args, **kwargs)
-
-
-def output_file(*args, **kwargs):
-    """Wrap bokeh.plotting.output_file."""
-    import bokeh.plotting as bkp
-
-    return bkp.output_file(*args, **kwargs)
-
-
-def copy_docstring(lib, function):
-    """Extract docstring from function."""
-    import importlib
-
-    try:
-        module = importlib.import_module(lib)
-        func = getattr(module, function)
-        doc = func.__doc__
-    except ImportError:
-        doc = "Failed to import {}".format(lib)
-
-    return doc
-
-
 def check_bokeh_version():
     """Check minimum bokeh version."""
     try:
@@ -82,15 +54,3 @@ def check_bokeh_version():
         assert version.parse(bokeh.__version__) >= version.parse("1.4.0")
     except (ImportError, AssertionError):
         raise ImportError("'bokeh' backend needs Bokeh (1.4.0+) installed.")
-
-
-def ColumnDataSource(*args, **kwargs):
-    """Wrap bokeh.models.ColumnDataSource."""
-    from bokeh.models import ColumnDataSource
-
-    return ColumnDataSource(*args, **kwargs)
-
-
-output_notebook.__doc__ += "\n\n" + copy_docstring("bokeh.plotting", "output_notebook")
-output_file.__doc__ += "\n\n" + copy_docstring("bokeh.plotting", "output_file")
-ColumnDataSource.__doc__ += "\n\n" + copy_docstring("bokeh.models", "ColumnDataSource")
