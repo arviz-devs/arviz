3dee7bc67bfc593c203c1456fad6ab3a3ff88fb4
Osvaldo Martin <aloctavodia@gmail.com>
2020-01-20 09:49:12 -0300

    Violinplot:  fix histogram, add rug (#997)
    * fix histogram, add rug
    
    * add rug to bokeh
    
    * remove redundant line, make bokeh plot looks closer to matplotlib, fix scale jitter

--- arviz/plots/backends/matplotlib/violinplot.py
+++ arviz/plots/backends/matplotlib/violinplot.py
@@ -14,9 +14,12 @@ def plot_violin(
     figsize,
     rows,
     cols,
+    sharex,
     sharey,
-    kwargs_shade,
+    shade_kwargs,
     shade,
+    rug,
+    rug_kwargs,
     bw,
     credible_interval,
     linewidth,
@@ -28,24 +31,31 @@ def plot_violin(
 ):
     """Matplotlib violin plot."""
     if ax is None:
-        _, ax = _create_axes_grid(
+        fig, ax = _create_axes_grid(
             len(plotters),
             rows,
             cols,
+            sharex=sharex,
             sharey=sharey,
             figsize=figsize,
             squeeze=False,
             backend_kwargs=backend_kwargs,
         )
+        fig.set_constrained_layout(False)
+        fig.subplots_adjust(wspace=0)
 
     ax = np.atleast_1d(ax)
 
     for (var_name, selection, x), ax_ in zip(plotters, ax.flatten()):
         val = x.flatten()
         if val[0].dtype.kind == "i":
-            cat_hist(val, shade, ax_, **kwargs_shade)
+            dens = cat_hist(val, rug, shade, ax_, **shade_kwargs)
         else:
-            _violinplot(val, shade, bw, ax_, **kwargs_shade)
+            dens = _violinplot(val, rug, shade, bw, ax_, **shade_kwargs)
+
+        if rug:
+            rug_x = -np.abs(np.random.normal(scale=max(dens) / 3.5, size=len(val)))
+            ax_.plot(rug_x, val, **rug_kwargs)
 
         per = np.percentile(val, [25, 75, 50])
         hpd_intervals = hpd(val, credible_interval, multimodal=False)
@@ -66,25 +76,32 @@ def plot_violin(
     return ax
 
 
-def _violinplot(val, shade, bw, ax, **kwargs_shade):
+def _violinplot(val, rug, shade, bw, ax, **shade_kwargs):
     """Auxiliary function to plot violinplots."""
     density, low_b, up_b = _fast_kde(val, bw=bw)
     x = np.linspace(low_b, up_b, len(density))
 
-    x = np.concatenate([x, x[::-1]])
-    density = np.concatenate([-density, density[::-1]])
+    if not rug:
+        x = np.concatenate([x, x[::-1]])
+        density = np.concatenate([-density, density[::-1]])
 
-    ax.fill_betweenx(x, density, alpha=shade, lw=0, **kwargs_shade)
+    ax.fill_betweenx(x, density, alpha=shade, lw=0, **shade_kwargs)
+    return density
 
 
-def cat_hist(val, shade, ax, **kwargs_shade):
+def cat_hist(val, rug, shade, ax, **shade_kwargs):
     """Auxiliary function to plot discrete-violinplots."""
     bins = get_bins(val)
     _, binned_d, _ = histogram(val, bins=bins)
 
     bin_edges = np.linspace(np.min(val), np.max(val), len(bins))
-    centers = 0.5 * (bin_edges + np.roll(bin_edges, 1))[:-1]
     heights = np.diff(bin_edges)
+    centers = bin_edges[:-1] + heights.mean() / 2
 
-    lefts = -0.5 * binned_d
-    ax.barh(centers, binned_d, height=heights, left=lefts, alpha=shade, **kwargs_shade)
+    if rug:
+        left = None
+    else:
+        left = -0.5 * binned_d
+
+    ax.barh(centers, binned_d, height=heights, left=left, alpha=shade, **shade_kwargs)
+    return binned_d
