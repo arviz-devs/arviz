c5641998dc604985a041988373e1870851925990
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2020-02-05 22:02:02 +0100

    type hinting and docstring extension

--- arviz/tests/helpers.py
+++ arviz/tests/helpers.py
@@ -6,10 +6,11 @@ import os
 import pickle
 import sys
 import logging
+from typing import Dict, List, Tuple, Union
 import pytest
 import numpy as np
 
-from ..data import from_dict
+from ..data import from_dict, InferenceData
 
 
 _log = logging.getLogger(__name__)
@@ -157,14 +158,20 @@ def multidim_models():
     return Models()
 
 
-def check_multiple_attrs(test_dict, parent):
+def check_multiple_attrs(
+    test_dict: Dict[str, List[str]], parent: InferenceData
+) -> List[Union[str, Tuple[str, str]]]:
     """Perform multiple hasattr checks on InferenceData objects.
 
     It is thought to first check if the parent object contains a given dataset,
     and then (if present) check the attributes of the dataset.
 
-    Args
-    ----
+    Given the ouput of the function, all missmatches between expectation and reality can
+    be retrieved: a single string indicates a group mismatch and a tuple of strings
+    ``(group, var)`` indicates a mismatch in the variable ``var`` of ``group``.
+
+    Parameters
+    ----------
     test_dict: dict of {str : list of str}
         Its structure should be `{dataset1_name: [var1, var2], dataset2_name: [var]}`.
         A ``~`` at the beggining of a dataset or variable name indicates the name NOT
@@ -177,6 +184,23 @@ def check_multiple_attrs(test_dict, parent):
     list
         List containing the failed checks. It will contain either the dataset_name or a
         tuple (dataset_name, var) for all non present attributes.
+
+    Examples
+    --------
+    The output below indicates that ``posterior`` group was expected but not found, and
+    variables ``a`` and ``b``:
+
+        ["posterior", ("prior", "a"), ("prior", "b")]
+
+    Another example could be the following:
+
+        [("posterior", "a"), "~observed_data", ("sample_stats", "~log_likelihood")]
+
+    In this case, the output indicates that variable ``a`` was not found in ``posterior``
+    as it was expected, however, in the other two cases, the preceding ``~`` (kept from the
+    input negation notation) indicates that ``observed_data`` group should not be present
+    but was found in the InferenceData and that ``log_likelihood`` variable was found
+    in ``sample_stats``, also against what was expected.
 
     """
     failed_attrs = []
