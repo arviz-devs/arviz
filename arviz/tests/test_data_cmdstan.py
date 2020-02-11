3a88f687067e84ae05ba013a45e2bb632b3b2493
ahartikainen <ahartikainen@github.com>
2020-02-11 23:26:56 +0200

    update tests

--- arviz/tests/test_data_cmdstan.py
+++ arviz/tests/test_data_cmdstan.py
@@ -120,10 +120,15 @@ class TestDataCmdStan:
             inference_data = self.get_inference_data(
                 posterior=path,
                 posterior_predictive="y_hat",
+                predictions="y_hat",
                 prior=path,
                 prior_predictive="y_hat",
                 observed_data=observed_data_paths[0],
                 observed_data_var="y",
+                constant_data=observed_data_paths[0],
+                constant_data_var="y",
+                predictions_constant_data=observed_data_paths[0],
+                predictions_constant_data_var="y",
                 log_likelihood="log_lik",
                 coords={"school": np.arange(8)},
                 dims={
@@ -136,11 +141,15 @@ class TestDataCmdStan:
             )
             test_dict = {
                 "posterior": ["mu", "tau", "theta_tilde", "theta"],
+                "posterior_predictive": ["y_hat"],
+                "predictions": ["y_hat"],
                 "prior": ["mu", "tau", "theta_tilde", "theta"],
                 "prior_predictive": ["y_hat"],
                 "sample_stats": ["log_likelihood"],
                 "observed_data": ["y"],
-                "posterior_predictive": ["y_hat"],
+                "constant_data": ["y"],
+                "predictions_constant_data": ["y"],
+                "log_likelihood": ["log_lik"],
             }
             fails = check_multiple_attrs(test_dict, inference_data)
             assert not fails
@@ -157,10 +166,15 @@ class TestDataCmdStan:
             inference_data = self.get_inference_data(
                 posterior=path,
                 posterior_predictive=["y_hat"],
+                predictions=["y_hat"],
                 prior=path,
                 prior_predictive=["y_hat"],
                 observed_data=observed_data_paths[0],
                 observed_data_var=["y"],
+                constant_data=observed_data_paths[0],
+                constant_data_var=["y"],
+                predictions_constant_data=observed_data_paths[0],
+                predictions_constant_data_var=["y"],
                 log_likelihood="log_lik",
                 coords={"school": np.arange(8)},
                 dims={
@@ -173,11 +187,15 @@ class TestDataCmdStan:
             )
             test_dict = {
                 "posterior": ["mu", "tau", "theta_tilde", "theta"],
+                "posterior_predictive": ["y_hat"],
+                "predictions": ["y_hat"],
                 "prior": ["mu", "tau", "theta_tilde", "theta"],
                 "prior_predictive": ["y_hat"],
-                "sample_stats": ["log_likelihood"],
+                "sample_stats": ["divergent"],
                 "observed_data": ["y"],
-                "posterior_predictive": ["y_hat"],
+                "constant_data": ["y"],
+                "predictions_constant_data": ["y"],
+                "log_likelihood": ["log_lik"],
             }
             fails = check_multiple_attrs(test_dict, inference_data)
             assert not fails
@@ -200,17 +218,22 @@ class TestDataCmdStan:
                 prior_predictive=post_pred,
                 observed_data=observed_data_paths[0],
                 observed_data_var=["y"],
-                log_likelihood="log_lik",
-                coords={"school": np.arange(8), "log_lik_dim_0": np.arange(8)},
+                log_likelihood=["log_lik", "y_hat"],
+                coords={
+                    "school": np.arange(8),
+                    "log_lik_dim_0": np.arange(8),
+                    "y_hat": np.arange(8),
+                },
                 dims={"theta": ["school"], "y": ["school"], "y_hat": ["school"], "eta": ["school"]},
             )
             test_dict = {
                 "posterior": ["mu", "tau", "theta_tilde", "theta"],
+                "sample_stats": ["divergent"],
                 "prior": ["mu", "tau", "theta_tilde", "theta"],
                 "prior_predictive": ["y_hat"],
-                "sample_stats": ["log_likelihood"],
                 "observed_data": ["y"],
                 "posterior_predictive": ["y_hat"],
+                "log_likelihood": ["log_lik", "y_hat"],
             }
             fails = check_multiple_attrs(test_dict, inference_data)
             assert not fails
@@ -274,7 +297,7 @@ class TestDataCmdStan:
             test_dict = {
                 "posterior": ["mu", "tau", "theta_tilde", "theta"],
                 "prior": ["mu", "tau", "theta_tilde", "theta"],
-                "sample_stats": ["log_likelihood"],
+                "log_likelihood": ["log_lik"],
                 "observed_data": ["y"],
                 "sample_stats_prior": ["lp"],
             }
