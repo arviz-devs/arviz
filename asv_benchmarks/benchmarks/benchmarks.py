22a49ff448567ac1e95396496ef6992ee8cc985c
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2020-02-25 01:01:11 +0100

    continue benchmark rewrite

--- asv_benchmarks/benchmarks/benchmarks.py
+++ asv_benchmarks/benchmarks/benchmarks.py
@@ -7,8 +7,9 @@ from scipy.stats import circstd
 from scipy.sparse import coo_matrix
 import scipy.signal as ss
 import warnings
+import arviz as az
+from arviz.stats.stats_utils import _circular_standard_deviation
 from arviz.plots.plot_utils import _fast_kde, _fast_kde_2d
-from arviz import Numba
 
 
 class Hist:
@@ -62,103 +63,67 @@ class Variance:
 
 
 class CircStd:
-    def time_circ_std(self):
-        try:
-            data = np.random.randn(10000, 1000)
-            import numba
+    params = (True, False)
+    param_names = "Numba",
 
-            def _circfunc(samples, high, low):
-                samples = np.asarray(samples)
-                if samples.size == 0:
-                    return np.nan, np.nan
-                return samples, _angle(samples, low, high, np.pi)
+    def setup(self, numba_flag):
+        self.data = np.random.randn(10000, 1000)
+        if numba_flag:
+            self.circstd = _circular_standard_deviation
+        else:
+            self.circstd = circstd
 
-            @numba.vectorize(nopython=True)
-            def _angle(samples, low, high, pi=np.pi):
-                ang = (samples - low) * 2.0 * pi / (high - low)
-                return ang
-
-            def _circular_standard_deviation(samples, high=2 * np.pi, low=0, axis=None):
-                pi = np.pi
-                samples, ang = _circfunc(samples, high, low)
-                S = np.sin(ang).mean(axis=axis)
-                C = np.cos(ang).mean(axis=axis)
-                R = np.hypot(S, C)
-                return ((high - low) / 2.0 / pi) * np.sqrt(-2 * np.log(R))
-
-            return _circular_standard_deviation(data)
-        except ImportError:
-            data = np.random.randn(10000, 1000)
-            return circstd(data)
+    def time_circ_std(self, numba_flag):
+        self.circstd(self.data)
 
 
 class Fast_Kde_1d:
-    params = [(True, False), (10**3, 10**5, 10**6)]
+    params = [(True, False), (10**5, 10**6, 10**7)]
     param_names = ("Numba", "n")
 
     def setup(self, numba_flag, n):
-        self.x = np.random.randn(n, 10)
+        self.x = np.random.randn(n//10, 10)
 
     def time_fast_kde_normal(self, numba_flag, n):
         if numba_flag:
-            Numba.enable_numba()
+            az.Numba.enable_numba()
         else:
-            Numba.disable_numba()
+            az.Numba.disable_numba()
 
         _fast_kde(self.x)
 
 class Fast_KDE_2d:
-    params = [(True, False), (10**3, 10**5)]
+    params = [(True, False), (10**5, 10**6)]
     param_names = ("Numba", "n")
 
     def setup(self, numba_flag, n):
-        self.x = np.random.randn(n, 10)
-        self.y = np.random.randn(n, 10)
+        self.x = np.random.randn(n//10, 10)
+        self.y = np.random.randn(n//10, 10)
 
     def time_fast_kde_2d(self, numba_flag, n):
         if numba_flag:
-            Numba.enable_numba()
+            az.Numba.enable_numba()
         else:
-            Numba.disable_numba()
+            az.Numba.disable_numba()
 
         _fast_kde_2d(self.x, self.y)
 
-class Data:
-    def time_2d_custom(self):
-        data = np.random.randn(100000)
+class Atleast_Nd:
+    params = ("az.utils", "numpy")
+    param_names = ("source",)
 
-        def two_de(data):
-            if not isinstance(data, np.ndarray):
-                return np.atleast_2d(data)
-            if data.ndim == 0:
-                result = data.reshape(1, 1)
-            elif data.ndim == 1:
-                result = data[newaxis, :]
-            else:
-                result = data
-            return result
+    def setup(self, source):
+        self.data = np.random.randn(100000)
+        self.x = np.random.randn(100000).tolist()
+        if source == "az.utils":
+            self.atleast_2d = az.utils.two_de
+            self.atleast_1d = az.utils.one_de
+        else:
+            self.atleast_2d = np.atleast_2d
+            self.atleast_1d = np.atleast_1d
 
-        return two_de(data)
+    def time_atleast_2d_array(self, source):
+        self.atleast_2d(self.data)
 
-    def time_numpy_2d(self):
-        data = np.random.randn(100000)
-        return np.atleast_2d(data)
-
-    def time_1d_custom(self):
-        x = np.random.randn(100000).tolist()
-
-        def one_de(x):
-            """Jitting numpy atleast_1d."""
-            if not isinstance(x, np.ndarray):
-                return np.atleast_1d(x)
-            if x.ndim == 0:
-                result = x.reshape(1)
-            else:
-                result = x
-            return result
-
-        return one_de(x)
-
-    def time_numpy_1d(self):
-        x = np.random.randn(100000).tolist()
-        return np.atleast_1d(x)
+    def time_atleast_1d(self, source):
+        self.atleast_1d(self.x)
