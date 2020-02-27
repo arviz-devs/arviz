1f21fc5803164f2358a3244a07bd7670f72b96ec
Ari Hartikainen <ari.hartikainen@ramboll.fi>
2020-02-27 11:59:24 +0200

    add tests for regex validation

--- arviz/tests/base_tests/test_rcparams.py
+++ arviz/tests/base_tests/test_rcparams.py
@@ -138,6 +138,34 @@ def test_make_validate_choice(args, allow_none, typeof):
 
 
 @pytest.mark.parametrize("allow_none", (True, False))
+@pytest.mark.parametrize(
+    "args",
+    [
+        (False, None),
+        (False, "row"),
+        (False, "54row"),
+        (False, "4column"),
+        ("or in regex", "square"),
+    ],
+)
+def test_make_validate_choice_regex(args, allow_none, typeof):
+    accepted_values = {"row", "column"}
+    accepted_values_regex = {"\d*row", "\d*column"}
+    validate_choice = _make_validate_choice_regex(
+        accepted_values, accepted_values_regex, allow_none=allow_none, typeof=str
+    )
+    raise_error, value = args
+    if value is None and not allow_none:
+        raise_error = "or in regex"
+    if raise_error:
+        with pytest.raises(ValueError, match=raise_error):
+            validate_choice(value)
+    else:
+        value_result = validate_choice(value)
+        assert value == value_result
+
+
+@pytest.mark.parametrize("allow_none", (True, False))
 @pytest.mark.parametrize("allow_auto", (True, False))
 @pytest.mark.parametrize("value", [(1, 2), "auto", None, "(1, 4)"])
 def test_make_iterable_validator_none_auto(value, allow_auto, allow_none):
