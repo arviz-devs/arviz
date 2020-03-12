0430fd5be3e85f9ceb429760a1944973f8a21fdc
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-03-12 09:43:08 +0200

    try again python script

--- scripts/codecov_uploader.py
+++ scripts/codecov_uploader.py
@@ -4,9 +4,12 @@ from codecov import main as codecov
 
 for _ in range(10):
     try:
-        codecov(["--token", environ["CODECOV_TOKEN"], "--name", environ["NAME"], "--required"])
-    except Exception:
+        print("Sending coverage information")
+        codecov("--token", environ["CODECOV_TOKEN"], "--name", environ["NAME"], "--required")
+    except Exception as exc:
+        print("Codecov failed: {}".format(exc), flush=True)
         sleep(30)
         continue
     else:
+        print("Codecov upload was successful")
         break
