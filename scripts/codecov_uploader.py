935f6ee3dfe9f9e733db46a2ec6e1aa8e68a6644
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-03-12 11:35:41 +0200

    debug print

--- scripts/codecov_uploader.py
+++ scripts/codecov_uploader.py
@@ -6,11 +6,15 @@ for _ in range(10):
         print("Sending coverage information")
         cmd = ["python", "-m", "codecov", "--token=$(CODECOV_TOKEN)", "--name=$(NAME)", "--required"]
         output = run(cmd, stdout=PIPE, stderr=PIPE)
+        print("\nSTDOUT: \n", output.stdout.decode("utf-8"), "\n")
+        print("\nSTDERR: \n", output.stderr.decode("utf-8"), "\n")
         if "Error:" in output.stdout.decode("utf-8"):
-            print("Error found in stdout")
+            print("Error found in stdout:")
+            print(output.stdout.decode("utf-8"))
             raise ValueError("Upload failed")
         elif "Error:" in output.stderr.decode("utf-8"):
-            print("Error found in stderr")
+            print("Error found in stderr:")
+            print(output.stderr.decode("utf-8"))
             raise ValueError("Upload failed")
     except Exception as exc:
         print("Codecov upload failed: {}".format(exc), flush=True)
