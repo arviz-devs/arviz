a418aaa97db2e53bdb88c20a3841450c698de948
Hector <hectormz.git@gmail.com>
2020-03-22 16:04:26 -0700

    Test `importorskip` for local/ci machines with `monkeypatch`

---
+++ arviz/tests/base_tests/test_helpers.py
@@ -0,0 +1,17 @@
+import pytest
+from _pytest.outcomes import Skipped
+
+from ..helpers import importorskip
+
+
+def test_importorskip_local():
+    """Test ``importorskip`` run on local machine with non-existent module, which should skip."""
+    with pytest.raises(Skipped):
+        importorskip("non-existent-function")
+
+
+def test_importorskip_ci(monkeypatch):
+    """Test ``importorskip`` run on CI machine with non-existent module, which should fail."""
+    monkeypatch.setenv("ARVIZ_CI_MACHINE", 1)
+    with pytest.raises(ModuleNotFoundError):
+        importorskip("non-existent-function")
