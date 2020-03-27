e34022a47761391773f769a64722ae7400918e48
nitishp25 <pasrichanitish2@gmail.com>
2020-03-27 19:44:33 +0530

    update tests and changelog

--- arviz/tests/external_tests/test_data_numpyro.py
+++ arviz/tests/external_tests/test_data_numpyro.py
@@ -27,7 +27,25 @@ class TestDataNumPyro:
 
         return Data
 
-    def get_inference_data(self, data, eight_schools_params):
+    @pytest.fixture(scope="class")
+    def predictions_params(self):
+        """Predictions data for eight schools."""
+        return {
+            "J": 8,
+            "sigma": np.array([5.0, 7.0, 12.0, 4.0, 6.0, 10.0, 3.0, 9.0]),
+        }
+
+    @pytest.fixture(scope="class")
+    def predictions_data(self, data, predictions_params):
+        """Generate predictions for predictions_params"""
+        posterior_samples = data.obj.get_samples()
+        model = data.obj.sampler.model
+        predictions = Predictive(model, posterior_samples)(
+            PRNGKey(2), predictions_params["J"], predictions_params["sigma"]
+        )
+        return predictions
+
+    def get_inference_data(self, data, eight_schools_params, predictions_data, predictions_params):
         posterior_samples = data.obj.get_samples()
         model = data.obj.sampler.model
         posterior_predictive = Predictive(model, posterior_samples)(
@@ -36,26 +54,95 @@ class TestDataNumPyro:
         prior = Predictive(model, num_samples=500)(
             PRNGKey(2), eight_schools_params["J"], eight_schools_params["sigma"]
         )
+        predictions = predictions_data
         return from_numpyro(
             posterior=data.obj,
             prior=prior,
             posterior_predictive=posterior_predictive,
-            coords={"school": np.arange(eight_schools_params["J"])},
-            dims={"theta": ["school"], "eta": ["school"]},
+            predictions=predictions,
+            coords={
+                "school": np.arange(eight_schools_params["J"]),
+                "school_pred": np.arange(predictions_params["J"]),
+            },
+            dims={"theta": ["school"], "eta": ["school"], "obs": ["school"]},
+            pred_dims={"theta": ["school_pred"], "eta": ["school_pred"], "obs": ["school_pred"]},
         )
 
-    def test_inference_data(self, data, eight_schools_params):
-        inference_data = self.get_inference_data(data, eight_schools_params)
+    def test_inference_data(self, data, eight_schools_params, predictions_data, predictions_params):
+        inference_data = self.get_inference_data(data, eight_schools_params, predictions_data, predictions_params)
         test_dict = {
             "posterior": ["mu", "tau", "eta"],
             "sample_stats": ["diverging", "tree_size", "depth"],
             "log_likelihood": ["obs"],
             "posterior_predictive": ["obs"],
+            "predictions": ["obs"],
             "prior": ["mu", "tau", "eta"],
             "prior_predictive": ["obs"],
             "observed_data": ["obs"],
         }
         fails = check_multiple_attrs(test_dict, inference_data)
+        assert not fails
+
+        # test dims
+        dims = inference_data.posterior_predictive.dims["school"]
+        pred_dims = inference_data.predictions.dims["school_pred"]
+        assert dims == 8
+        assert pred_dims == 8
+
+    def test_inference_data_no_posterior(
+        self, data, eight_schools_params, predictions_data, predictions_params
+    ):
+        posterior_samples = data.obj.get_samples()
+        model = data.obj.sampler.model
+        posterior_predictive = Predictive(model, posterior_samples)(
+            PRNGKey(1), eight_schools_params["J"], eight_schools_params["sigma"]
+        )
+        prior = Predictive(model, num_samples=500)(
+            PRNGKey(2), eight_schools_params["J"], eight_schools_params["sigma"]
+        )
+        predictions = predictions_data
+        constant_data = {"J": 8, "sigma": eight_schools_params["sigma"]}
+        predictions_constant_data = predictions_params
+        # only prior
+        inference_data = from_numpyro(prior=prior)
+        test_dict = {"prior": ["mu", "tau", "eta"]}
+        fails = check_multiple_attrs(test_dict, inference_data)
+        assert not fails, "only prior: {}".format(fails)
+        # only posterior_predictive
+        inference_data = from_numpyro(posterior_predictive=posterior_predictive)
+        test_dict = {"posterior_predictive": ["obs"]}
+        fails = check_multiple_attrs(test_dict, inference_data)
+        assert not fails, "only posterior_predictive: {}".format(fails)
+        # only predictions
+        inference_data = from_numpyro(predictions=predictions)
+        test_dict = {"predictions": ["obs"]}
+        fails = check_multiple_attrs(test_dict, inference_data)
+        assert not fails, "only predictions: {}".format(fails)
+        # only constant_data
+        inference_data = from_numpyro(constant_data=constant_data)
+        test_dict = {"constant_data": ["J", "sigma"]}
+        fails = check_multiple_attrs(test_dict, inference_data)
+        assert not fails, "only constant_data: {}".format(fails)
+        # only predictions_constant_data
+        inference_data = from_numpyro(predictions_constant_data=predictions_constant_data)
+        test_dict = {"predictions_constant_data": ["J", "sigma"]}
+        fails = check_multiple_attrs(test_dict, inference_data)
+        assert not fails, "only predictions_constant_data: {}".format(fails)
+        # prior and posterior_predictive
+        idata = from_numpyro(
+            prior=prior,
+            posterior_predictive=posterior_predictive,
+            coords={"school": np.arange(eight_schools_params["J"])},
+            dims={"theta": ["school"], "eta": ["school"]},
+        )
+        test_dict = {"posterior_predictive": ["obs"], "prior": ["mu", "tau", "eta", "obs"]}
+        fails = check_multiple_attrs(test_dict, idata)
+        assert not fails, "prior and posterior_predictive: {}".format(fails)
+
+    def test_inference_data_only_posterior(self, data):
+        idata = from_numpyro(data.obj)
+        test_dict = {"posterior": ["mu", "tau", "eta"], "sample_stats": ["diverging"]}
+        fails = check_multiple_attrs(test_dict, idata)
         assert not fails
 
     def test_multiple_observed_rv(self):
@@ -89,3 +176,48 @@ class TestDataNumPyro:
         #         print(waic_results.waic, waic_results.waic_se)
         assert not fails
         assert not hasattr(inference_data.sample_stats, "log_likelihood")
+
+    def test_inference_data_constant_data(self):
+        import numpyro
+        import numpyro.distributions as dist
+        from numpyro.infer import MCMC, NUTS
+
+        x1 = 10
+        x2 = 12
+        y1 = np.random.randn(10)
+
+        def model_constant_data(x, y1=None):
+            _x = numpyro.sample("x", dist.Normal(1, 3))
+            numpyro.sample("y1", dist.Normal(x * _x, 1), obs=y1)
+
+        nuts_kernel = NUTS(model_constant_data)
+        mcmc = MCMC(nuts_kernel, num_samples=10, num_warmup=2)
+        mcmc.run(PRNGKey(0), x=x1, y1=y1)
+        posterior = mcmc.get_samples()
+        posterior_predictive = Predictive(model_constant_data, posterior)(PRNGKey(1), x1)
+        predictions = Predictive(model_constant_data, posterior)(PRNGKey(2), x2)
+        inference_data = from_numpyro(
+            mcmc,
+            posterior_predictive=posterior_predictive,
+            predictions=predictions,
+            constant_data={"x1": x1},
+            predictions_constant_data={"x2": x2},
+        )
+        test_dict = {
+            "posterior": ["x"],
+            "posterior_predictive": ["y1"],
+            "sample_stats": ["diverging"],
+            "log_likelihood": ["y1"],
+            "predictions": ["y1"],
+            "observed_data": ["y1"],
+            "constant_data": ["x1"],
+            "predictions_constant_data": ["x2"],
+        }
+        fails = check_multiple_attrs(test_dict, inference_data)
+        assert not fails
+
+    def test_inference_data_num_chains(self, predictions_data, chains):
+        predictions = predictions_data
+        inference_data = from_numpyro(predictions=predictions, num_chains=chains)
+        nchains = inference_data.predictions.dims["chain"]
+        assert nchains == chains
