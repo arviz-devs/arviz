1b2ecdd33090c5a9c726d6d3b3c365d5db56f272
Oriol Abril <oriol.abril.pla@gmail.com>
2020-04-06 08:42:02 +0200

    allow overlaid pairplots (#1135)
    * allow overlaid pairplots
    
    * extend testing
    
    * fix tests and black
    
    * black

--- arviz/tests/base_tests/test_plots_matplotlib.py
+++ arviz/tests/base_tests/test_plots_matplotlib.py
@@ -447,6 +447,16 @@ def test_plot_pair_divergences_warning(has_sample_stats):
     assert np.all(ax)
 
 
+@pytest.mark.parametrize(
+    "kwargs", [{}, {"diagonal": True}, {"diagonal": True, "var_names": ["mu", "tau"]}]
+)
+def test_plot_pair_overlaid(models, kwargs):
+    ax = plot_pair(models.model_1, **kwargs)
+    ax2 = plot_pair(models.model_2, ax=ax, **kwargs)
+    assert ax is ax2
+    assert ax.shape
+
+
 @pytest.mark.parametrize("kind", ["kde", "cumulative", "scatter"])
 @pytest.mark.parametrize("alpha", [None, 0.2, 1])
 @pytest.mark.parametrize("animated", [False, True])
