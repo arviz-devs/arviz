543339be381f8105d3b12a95c7d38fde6673e2da
percygautam <gautampiyus@gmail.com>
2020-04-12 00:25:01 +0530

    prior-posterior plot

---
+++ arviz/plots/backends/matplotlib/ppplot.py
@@ -0,0 +1,102 @@
+"""Matplotib Posterior predictive plot."""
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+
+from . import backend_show
+from ...kdeplot import plot_kde
+from ...plot_utils import (
+    _create_axes_grid,
+)
+
+colors = {
+    'prior': "red",
+    'posterior': "blue",
+    'observed': "yellow",
+}
+
+
+def plot_pp(
+        ax,
+        length_plotters,
+        rows,
+        cols,
+        figsize,
+        pp_plotters,
+        linewidth,
+        legend,
+        groups,
+        backend_kwargs,
+        show,
+):
+    """Matplotlib pp plot."""
+    if ax is None:
+        fig, ax = _create_axes_grid(
+            rows * cols, rows, cols, figsize=figsize, backend_kwargs=backend_kwargs
+        )
+        axes = []
+        for i in range(cols, rows * cols, 2 * cols):
+            axes += list(ax[i - cols:i])
+            gs = ax[i].get_gridspec()
+            for j in range(i, i + cols):
+                ax[j].remove()
+            axbig = fig.add_subplot(gs[i // cols, :])
+            axes.append(axbig)
+
+    else:
+        axes = np.ravel(ax)
+        if len(axes) != length_plotters:
+            raise ValueError(
+                "Found {} variables to plot but {} axes instances. They must be equal.".format(
+                    length_plotters, len(axes)
+                )
+            )
+
+    matplotlib.rc('xtick', labelsize=7)
+    matplotlib.rc('ytick', labelsize=7)
+
+    for idx, plotter in enumerate(pp_plotters):
+        group = groups[idx]
+        color = colors[group]
+        for idx2, (var, coord, data,) in enumerate(plotter):
+            _pp_helper(
+                ax=axes[(cols + 1) * idx2 + idx],
+                data=data,
+                group=group,
+                var_name=var,
+                coord_name=coord,
+                color=color,
+                linewidth=linewidth,
+            )
+            _pp_helper(
+                ax=axes[(cols + 1) * (idx2) + cols],
+                data=data,
+                group=group,
+                var_name=var,
+                coord_name=coord,
+                color=color,
+                linewidth=linewidth,
+            )
+
+    if backend_show(show):
+        plt.show()
+
+    return axes
+
+
+def _pp_helper(
+        ax,
+        data,
+        group,
+        var_name,
+        coord_name,
+        color,
+        linewidth,
+):
+    plot_kde(
+        data,
+        label="{} {} {}".format(group, var_name, coord_name),
+        plot_kwargs={"color": color, "linewidth": linewidth, },
+        fill_kwargs={"alpha": 0},
+        ax=ax,
+    )
---
+++ arviz/plots/ppplot.py
@@ -0,0 +1,143 @@
+"""Posterior-Prior plot."""
+from numbers import Integral
+import platform
+import logging
+import numpy as np
+
+from .plot_utils import (
+    xarray_var_iter,
+    _scale_fig_size,
+
+    get_plotting_function,
+)
+from ..rcparams import rcParams
+
+_log = logging.getLogger(__name__)
+
+
+def plot_pp(
+        data,
+        figsize=None,
+        textsize=None,
+        vars=None,
+        coords=None,
+        legend=True,
+        ax=None,
+        backend=None,
+        backend_kwargs=None,
+        show=None,
+):
+    """
+    Plot for posterior-prior predictive.
+
+    Parameters
+    ----------
+    data : az.InferenceData object
+        InferenceData object containing the observed and posterior/prior predictive data.
+    figsize : tuple
+        Figure size. If None it will be defined automatically.
+    textsize: float
+        Text size scaling factor for labels, titles and lines. If None it will be
+        autoscaled based on figsize.
+    data_pairs : dict
+        Dictionary containing relations between observed data and posterior/prior predictive data.
+        Dictionary structure:
+
+        - key = data var_name
+        - value = posterior/prior predictive var_name
+
+        For example, `data_pairs = {'y' : 'y_hat'}`
+        If None, it will assume that the observed data and the posterior/prior
+        predictive data have the same variable name.
+    var_names : list
+        List of variables to be plotted. Defaults to all observed variables in the
+        model if None.
+    coords : dict
+        Dictionary mapping dimensions to selected coordinates to be plotted.
+        Dimensions without a mapping specified will include all coordinates for
+        that dimension. Defaults to including all coordinates for all
+        dimensions if None.
+    legend : bool
+        Add legend to figure. By default True.
+    ax: axes, optional
+        Matplotlib axes or bokeh figures.
+    backend: str, optional
+        Select plotting backend {"matplotlib","bokeh"}. Default "matplotlib".
+    backend_kwargs: bool, optional
+        These are kwargs specific to the backend being used. For additional documentation
+        check the plotting method of the backend.
+    show : bool, optional
+        Call backend show function.
+
+    Returns
+    -------
+    axes : matplotlib axes or bokeh figures
+
+    Examples
+    --------
+    Plot the observed data KDE overlaid on posterior predictive KDEs.
+
+    .. plot::
+        :context: close-figs
+
+        >>> import arvizz as az
+        >>> data = az.load_arviz_data('radon')
+        >>> az.plot_pp(data, vars=["defs"], coords={"team" : ["Italy"]})
+        >>> #az.plot_ppc(data,data_pairs={"obs":"obs_hat"})
+
+    """
+
+    groups = ["prior", "posterior"]
+
+    if coords is None:
+        coords = {}
+
+    pp_plotters = []
+    for group in groups:
+
+        group = getattr(data, group)
+        coord = {}
+        for key in coords.keys():
+            coord[key] = np.where(np.in1d(group[key], coords[key]))[0]
+
+        plotters = [
+            tup
+            for tup in
+            xarray_var_iter(
+                group.isel(coord),
+                var_names=vars,
+                combined=True,
+            )
+
+        ]
+        pp_plotters.append(plotters)
+
+    cols = len(groups)
+    rows = 2 * len(pp_plotters[0])
+
+    (figsize, ax_labelsize, _, xt_labelsize, linewidth, markersize) = _scale_fig_size(
+        figsize, textsize, rows, cols
+    )
+
+    ppplot_kwargs = dict(
+        ax=ax,
+        length_plotters=(cols + 1) * rows,
+        rows=rows,
+        cols=cols,
+        figsize=figsize,
+        pp_plotters=pp_plotters,
+        linewidth=linewidth,
+        legend=legend,
+        groups=groups,
+        backend_kwargs=backend_kwargs,
+        show=show,
+    )
+
+    if backend is None:
+        backend = rcParams["plot.backend"]
+    backend = backend.lower()
+
+    # TODO: Add backend kwargs
+    plot = get_plotting_function("plot_pp", "ppplot", backend)
+    axes = plot(**ppplot_kwargs)
+    return axes
