47870ceee37b5d4e3bccaa5e4a34954c6a137a8d
AlexAndorra <andorra.alexandre@gmail.com>
2020-04-22 12:56:49 +0200

    Added tests for Oriol's use case

--- arviz/tests/base_tests/test_utils.py
+++ arviz/tests/base_tests/test_utils.py
@@ -62,26 +62,31 @@ def test_var_names_key_error(data):
     "var_args",
     [
         (["alpha", "beta"], ["alpha", "beta1", "beta2"], "like"),
-        (["~beta"], ["alpha", "p1", "p2", "phi"], "like"),
+        (["~beta"], ["alpha", "p1", "p2", "phi", "theta", "theta_t"], "like"),
+        (["theta"], ["theta", "theta_t"], "like"),
+        (["~theta"], ["alpha", "beta1", "beta2", "p1", "p2", "phi"], "like"),
         (["p"], ["alpha", "p1", "p2", "phi"], "like"),
-        (["~p"], ["beta1", "beta2"], "like"),
+        (["~p"], ["beta1", "beta2", "theta", "theta_t"], "like"),
         (["^bet"], ["beta1", "beta2"], "regex"),
         (["^p"], ["p1", "p2", "phi"], "regex"),
-        (["~^p"], ["alpha", "beta1", "beta2"], "regex"),
+        (["~^p"], ["alpha", "beta1", "beta2", "theta", "theta_t"], "regex"),
         (["p[0-9]+"], ["p1", "p2"], "regex"),
-        (["~p[0-9]+"], ["alpha", "beta1", "beta2", "phi"], "regex"),
+        (["~p[0-9]+"], ["alpha", "beta1", "beta2", "phi", "theta", "theta_t"], "regex"),
     ],
 )
 def test_var_names_filter(var_args):
     """Test var_names filter with partial naming or regular expressions."""
+    samples = np.random.randn(10)
     data = dict_to_dataset(
         {
-            "alpha": np.random.randn(10),
-            "beta1": np.random.randn(10),
-            "beta2": np.random.randn(10),
-            "p1": np.random.randn(10),
-            "p2": np.random.randn(10),
-            "phi": np.random.randn(10),
+            "alpha": samples,
+            "beta1": samples,
+            "beta2": samples,
+            "p1": samples,
+            "p2": samples,
+            "phi": samples,
+            "theta": samples,
+            "theta_t": samples,
         }
     )
     var_names, expected, filter = var_args
