ae8782d6cbd38f4a618c34a48752957f480e018b
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2020-06-08 17:01:23 +0200

    increase test coverage

--- arviz/tests/external_tests/test_data_pyro.py
+++ arviz/tests/external_tests/test_data_pyro.py
@@ -228,7 +228,13 @@ class TestDataPyro:
         nchains = inference_data.predictions.dims["chain"]
         assert nchains == chains
 
-    def test_log_likelihood_warning(self):
+    @pytest.mark.parametrize("log_likelihood", [True, False])
+    def test_log_likelihood(self, log_likelihood):
+        """Test behaviour when log likelihood cannot be retrieved.
+
+        If log_likelihood=True there is a warning to say log_likelihood group is skipped,
+        if log_likelihood=False there is no warning and log_likelihood is skipped.
+        """
         x = torch.randn((10, 2))
         y = torch.randn(10)
 
@@ -239,8 +245,11 @@ class TestDataPyro:
         nuts_kernel = pyro.infer.NUTS(model_constant_data)
         mcmc = pyro.infer.MCMC(nuts_kernel, num_samples=10)
         mcmc.run(x=x, y=y)
-        with pytest.warns(UserWarning, match="Could not get vectorized trace"):
-            inference_data = from_pyro(mcmc)
+        if log_likelihood:
+            with pytest.warns(UserWarning, match="Could not get vectorized trace"):
+                inference_data = from_pyro(mcmc, log_likelihood=log_likelihood)
+        else:
+            inference_data = from_pyro(mcmc, log_likelihood=log_likelihood)
         test_dict = {
             "posterior": ["beta"],
             "sample_stats": ["diverging"],
