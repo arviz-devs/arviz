c08f288851af0e4c148e99f95d987169141b0e31
percygautam <gautampiyus@gmail.com>
2020-06-09 01:37:50 +0530

    minor nits

--- arviz/tests/base_tests/test_data.py
+++ arviz/tests/base_tests/test_data.py
@@ -4,9 +4,9 @@ from collections import namedtuple
 import os
 from typing import Dict
 from urllib.parse import urlunsplit
+from html import escape
 import numpy as np
 import pytest
-from html import escape
 
 import xarray as xr
 from xarray.core.options import OPTIONS
@@ -467,12 +467,14 @@ class TestInferenceData:
 
         assert html is not None
         assert "<div" in html
-        for group in idata._groups:
+        for group in idata._groups:  # pylint: disable=protected-access
             assert group in html
             xr_data = getattr(idata, group)
-            xr_data._repr_html_() in html
+            for item, _ in xr_data.items():
+                assert item in html
         specific_style = ".xr-wrap{width:700px!important;}"
         assert specific_style in html
+
         xr.set_options(display_style="text")
         html = idata._repr_html_()  # pylint: disable=protected-access
         assert escape(idata.__repr__()) in html
