b6ee09b8de95d1a72abe616e42abeb60a6cd6943
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2020-06-17 22:08:15 +0200

    add backend to mpl and conversion to hex in bokeh

--- arviz/plots/hdiplot.py
+++ arviz/plots/hdiplot.py
@@ -6,7 +6,7 @@ from scipy.interpolate import griddata
 from scipy.signal import savgol_filter
 
 from ..stats import hdi
-from .plot_utils import get_plotting_function, matplotlib_kwarg_dealiaser
+from .plot_utils import get_plotting_function, matplotlib_kwarg_dealiaser, vectorized_to_hex
 from ..rcparams import rcParams
 from ..utils import credible_interval_warning
 
@@ -61,7 +61,7 @@ def plot_hdi(
         :meth:`bokeh:bokeh.plotting.figure.Figure.patch`.
     plot_kwargs : dict, optional
         HDI limits keyword arguments, passed to :meth:`mpl:matplotlib.axes.Axes.plot` or
-        :meth:`~bokeh:bokeh.plotting.figure.Figure.patch`.
+        :meth:`bokeh:bokeh.plotting.figure.Figure.patch`.
     hdi_kwargs : dict, optional
         Keyword arguments passed to :func:`~arviz.hdi`. Ingnored if ``hdi_data`` is present.
     ax : axes, optional
@@ -112,11 +112,11 @@ def plot_hdi(
     if hdi_kwargs is None:
         hdi_kwargs = {}
     plot_kwargs = matplotlib_kwarg_dealiaser(plot_kwargs, "plot")
-    plot_kwargs.setdefault("color", color)
+    plot_kwargs["color"] = vectorized_to_hex(plot_kwargs.get("color", color))
     plot_kwargs.setdefault("alpha", 0)
 
-    fill_kwargs = matplotlib_kwarg_dealiaser(fill_kwargs, "hexbin")
-    fill_kwargs.setdefault("color", color)
+    fill_kwargs = matplotlib_kwarg_dealiaser(fill_kwargs, "fill_between")
+    fill_kwargs["color"] = vectorized_to_hex(fill_kwargs.get("color", color))
     fill_kwargs.setdefault("alpha", 0.5)
 
     x = np.asarray(x)
@@ -175,7 +175,6 @@ def plot_hdi(
         backend = rcParams["plot.backend"]
     backend = backend.lower()
 
-    # TODO: Add backend kwargs
     plot = get_plotting_function("plot_hdi", "hdiplot", backend)
     ax = plot(**hdiplot_kwargs)
     return ax
