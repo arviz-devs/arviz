adc8e4e681bafe5329f5f22c02b17f7dbcaa6b7a
Michael Nowotny <nowotnym@gmail.com>
2020-06-18 23:38:59 -0700

    - added TestDataPyJAGS class

--- arviz/tests/external_tests/test_data_pyjags.py
+++ arviz/tests/external_tests/test_data_pyjags.py
@@ -43,48 +43,47 @@ def verify_equality_of_numpy_values_dictionaries(
     return True
 
 
-def test_convert_pyjags_samples_dictionary_to_arviz_samples_dictionary():
-    arviz_samples_dict_from_pyjags_samples_dict = _convert_pyjags_dict_to_arviz_dict(
-        PYJAGS_POSTERIOR_DICT
-    )
+class TestDataPyJAGS:
+    def test_convert_pyjags_samples_dictionary_to_arviz_samples_dictionary(self):
+        arviz_samples_dict_from_pyjags_samples_dict = _convert_pyjags_dict_to_arviz_dict(
+            PYJAGS_POSTERIOR_DICT
+        )
 
-    pyjags_dict_from_arviz_dict_from_pyjags_dict = _convert_arviz_dict_to_pyjags_dict(
-        arviz_samples_dict_from_pyjags_samples_dict
-    )
+        pyjags_dict_from_arviz_dict_from_pyjags_dict = _convert_arviz_dict_to_pyjags_dict(
+            arviz_samples_dict_from_pyjags_samples_dict
+        )
 
-    assert verify_equality_of_numpy_values_dictionaries(
-        PYJAGS_POSTERIOR_DICT, pyjags_dict_from_arviz_dict_from_pyjags_dict,
-    )
+        assert verify_equality_of_numpy_values_dictionaries(
+            PYJAGS_POSTERIOR_DICT, pyjags_dict_from_arviz_dict_from_pyjags_dict,
+        )
 
+    def test_extract_samples_dictionary_from_arviz_inference_data(self):
+        arviz_samples_dict_from_pyjags_samples_dict = _convert_pyjags_dict_to_arviz_dict(
+            PYJAGS_POSTERIOR_DICT
+        )
 
-def test_extract_samples_dictionary_from_arviz_inference_data():
-    arviz_samples_dict_from_pyjags_samples_dict = _convert_pyjags_dict_to_arviz_dict(
-        PYJAGS_POSTERIOR_DICT
-    )
+        arviz_inference_data_from_pyjags_samples_dict = from_pyjags(PYJAGS_POSTERIOR_DICT)
+        arviz_dict_from_idata_from_pyjags_dict = _extract_arviz_dict_from_inference_data(
+            arviz_inference_data_from_pyjags_samples_dict
+        )
 
-    arviz_inference_data_from_pyjags_samples_dict = from_pyjags(PYJAGS_POSTERIOR_DICT)
-    arviz_dict_from_idata_from_pyjags_dict = _extract_arviz_dict_from_inference_data(
-        arviz_inference_data_from_pyjags_samples_dict
-    )
+        assert verify_equality_of_numpy_values_dictionaries(
+            arviz_samples_dict_from_pyjags_samples_dict, arviz_dict_from_idata_from_pyjags_dict,
+        )
 
-    assert verify_equality_of_numpy_values_dictionaries(
-        arviz_samples_dict_from_pyjags_samples_dict, arviz_dict_from_idata_from_pyjags_dict,
-    )
+    def test_roundtrip_from_pyjags_via_arviz_to_pyjags(self):
+        arviz_inference_data_from_pyjags_samples_dict = from_pyjags(PYJAGS_POSTERIOR_DICT)
+        arviz_dict_from_idata_from_pyjags_dict = _extract_arviz_dict_from_inference_data(
+            arviz_inference_data_from_pyjags_samples_dict
+        )
 
+        pyjags_dict_from_arviz_idata = _convert_arviz_dict_to_pyjags_dict(
+            arviz_dict_from_idata_from_pyjags_dict
+        )
 
-def test_roundtrip_from_pyjags_via_arviz_to_pyjags():
-    arviz_inference_data_from_pyjags_samples_dict = from_pyjags(PYJAGS_POSTERIOR_DICT)
-    arviz_dict_from_idata_from_pyjags_dict = _extract_arviz_dict_from_inference_data(
-        arviz_inference_data_from_pyjags_samples_dict
-    )
-
-    pyjags_dict_from_arviz_idata = _convert_arviz_dict_to_pyjags_dict(
-        arviz_dict_from_idata_from_pyjags_dict
-    )
-
-    assert verify_equality_of_numpy_values_dictionaries(
-        PYJAGS_POSTERIOR_DICT, pyjags_dict_from_arviz_idata
-    )
+        assert verify_equality_of_numpy_values_dictionaries(
+            PYJAGS_POSTERIOR_DICT, pyjags_dict_from_arviz_idata
+        )
 
 
 @pytest.mark.parametrize("posterior", [None, PYJAGS_POSTERIOR_DICT])
