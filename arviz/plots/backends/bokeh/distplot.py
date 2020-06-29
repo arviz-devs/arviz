fix bin computation

arviz/plots/backends/bokeh/distplot.py
@@ -131,6 +131,9 @@ def _histplot_bokeh_op(values, values2, rotated, ax, hist_kwargs, is_circular):
 
     if is_circular:
 
+        if is_circular == "degrees":
+            edges = np.deg2rad(edges)
+
         start_angle = edges
         outer_radius = hist
 
@@ -152,16 +155,16 @@ def _histplot_bokeh_op(values, values2, rotated, ax, hist_kwargs, is_circular):
             outer_radius=np.max(outer_radius) * 1.1,
             start_angle=ticks[:-1],
             end_angle=ticks[:-1] + 0.002,
-            **hist_kwargs,
+            line_color="grey",
         )
 
         radii_circles = np.linspace(0, np.max(outer_radius) * 1.1, 4)
-        ax.circle(0, 0, radius=radii_circles, fill_color=None, **hist_kwargs)
+        ax.circle(0, 0, radius=radii_circles, fill_color=None, line_color="grey")
 
-        if is_circular == 'degrees':
+        if is_circular == "degrees":
             labels = ["0°", "45°", "90°", "135°", "180°", "225°", "270°", "315°"]
         else:
-            
+
             labels = [
                 r"0",
                 r"π/4",

arviz/plots/backends/matplotlib/distplot.py
@@ -98,26 +98,26 @@ def _histplot_mpl_op(values, values2, rotated, ax, hist_kwargs, is_circular):
 
     bins = hist_kwargs.pop("bins")
 
-    if is_circular:
+    if is_circular == "degrees":
+        if bins is None:
+            bins = get_bins(values)
+        values = np.deg2rad(values)
+        bins = np.deg2rad(bins)
+        ax.set_yticklabels([])
 
-        if is_circular == "degrees":
-            if bins is None:
-                bins = get_bins(values)
-            values = np.deg2rad(values)
-            bins = np.deg2rad(bins)
-        else:
-            labels = [
-                r"0",
-                r"π/4",
-                r"π/2",
-                r"3π/4",
-                r"π",
-                r"5π/4",
-                r"3π/2",
-                r"7π/4",
-            ]
+    elif is_circular == "radians":
+        labels = [
+            r"0",
+            r"π/4",
+            r"π/2",
+            r"3π/4",
+            r"π",
+            r"5π/4",
+            r"3π/2",
+            r"7π/4",
+        ]
 
-            ax.set_xticklabels(labels)
+        ax.set_xticklabels(labels)
         ax.set_yticklabels([])
 
     if values2 is not None:

arviz/plots/backends/matplotlib/kdeplot.py
@@ -79,19 +79,19 @@ def plot_kde(
 
         if is_circular:
 
-            if values.min() >= -np.pi and values.max() <= np.pi:
-                label = [
-                    r"$0$",
-                    r"$\pi/4$",
-                    r"$\pi/2$",
-                    r"$3\pi/4$",
-                    r"$\pi$",
-                    r"$5\pi/4$",
-                    r"$3\pi/2$",
-                    r"$7\pi/4$",
+            if is_circular == "radians":
+                labels = [
+                    r"0",
+                    r"π/4",
+                    r"π/2",
+                    r"3π/4",
+                    r"π",
+                    r"5π/4",
+                    r"3π/2",
+                    r"7π/4",
                 ]
 
-                ax.set_xticklabels(label)
+                ax.set_xticklabels(labels)
 
             x = np.linspace(-np.pi, np.pi, len(density))
             ax.set_yticklabels([])