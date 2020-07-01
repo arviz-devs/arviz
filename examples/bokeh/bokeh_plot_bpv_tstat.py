61b0b28918c19f9a9ceb18c8e7c7f6834dd4cfff
Ravin Kumar <ravinsdrive@gmail.com>
2020-06-30 18:12:02 -0700

    Fix the other broken example

--- examples/bokeh/bokeh_plot_bpv_tstat.py
+++ examples/bokeh/bokeh_plot_bpv_tstat.py
@@ -7,4 +7,4 @@ _thumb: .6, .5
 import arviz as az
 
 data = az.load_arviz_data("regression1d")
-az.plot_bpv(data, kind="t_stat", t_stat="0.5", backend="bokeh")
+ax = az.plot_bpv(data, kind="t_stat", t_stat="0.5", backend="bokeh")
