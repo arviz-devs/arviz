47054ed4e15bdc99ae8c92467b500c9e5b9e03c1
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2020-08-04 17:53:56 +0300

    Fix

--- arviz/plots/backends/matplotlib/energyplot.py
+++ arviz/plots/backends/matplotlib/energyplot.py
@@ -40,7 +40,7 @@ def plot_energy(
 
     if ax is None:
         _, ax = _create_axes_grid(
-            1, 1, 1, figsize=figsize, backend="matplotlib", backend_kwargs=backend_kwargs,
+            1, 1, 1, figsize=figsize, squeeze=True, backend="matplotlib", backend_kwargs=backend_kwargs,
         )
 
     fill_kwargs = matplotlib_kwarg_dealiaser(fill_kwargs, "hexbin")
