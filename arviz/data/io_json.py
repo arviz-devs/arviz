af54caf86dd4be842d4f9ef120a15dd88764c591
percygautam <gautampiyus@gmail.com>
2020-08-19 01:04:01 +0530

    remove args from to_json

--- arviz/data/io_json.py
+++ arviz/data/io_json.py
@@ -1,6 +1,5 @@
 """Input and output support for data."""
 
-from .converters import convert_to_inference_data
 from .io_dict import from_dict
 
 try:
@@ -28,29 +27,22 @@ def from_json(filename):
     return from_dict(**idata_dict, save_warmup=True)
 
 
-def to_json(data, filename, *, group="posterior", coords=None, dims=None):
+def to_json(idata, filename):
     """Save dataset as a json file.
 
     WARNING: Only idempotent in case `data` is InferenceData
 
     Parameters
     ----------
-    data : InferenceData, or any object accepted by `convert_to_inference_data`
+    idata : InferenceData
         Object to be saved
     filename : str
         name or path of the file to load trace
-    group : str (optional)
-        In case `data` is not InferenceData, this is the group it will be saved to
-    coords : dict (optional)
-        See `convert_to_inference_data`
-    dims : dict (optional)
-        See `convert_to_inference_data`
 
     Returns
     -------
     str
         filename saved to
     """
-    inference_data = convert_to_inference_data(data, group=group, coords=coords, dims=dims)
-    file_name = inference_data.to_json(filename)
+    file_name = idata.to_json(filename)
     return file_name
