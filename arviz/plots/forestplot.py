6b5343a0efac72488b14fce220c3513559008f9d
aloctavodia <aloctavodia@gmail.com>
2020-09-21 12:15:06 -0300

    fix multi rope

--- arviz/plots/forestplot.py
+++ arviz/plots/forestplot.py
@@ -136,9 +136,22 @@ def plot_forest(
         >>>                            var_names=["^the"],
         >>>                            filter_vars="regex",
         >>>                            combined=True,
-        >>>                            ridgeplot_overlap=3,
         >>>                            figsize=(9, 7))
         >>> axes[0].set_title('Estimated theta for 8 schools model')
+
+    Forestpĺot with ropes
+
+    .. plot::
+        :context: close-figs
+
+        >>> rope = {'theta': [{'school': 'Choate', 'rope': (2, 4)}], 'mu': [{'rope': (-2, 2)}]}
+        >>> axes = az.plot_forest(non_centered_data,
+        >>>                            rope=rope,
+        >>>                            var_names='~tau',
+        >>>                            combined=True,
+        >>>                            figsize=(9, 7))
+        >>> axes[0].set_title('Estimated theta for 8 schools model')
+
 
     Ridgeplot
 
