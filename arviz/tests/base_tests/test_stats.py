d7ff0ffbeb74d95fadc54228dee4dee99b21dfa9
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2020-10-15 14:04:10 +0200

    black

--- arviz/tests/base_tests/test_stats.py
+++ arviz/tests/base_tests/test_stats.py
@@ -586,11 +586,12 @@ def test_loo_pit_multidim(multidim_models, args):
         loo_pit_data = loo_pit(idata=None, y=y_arr, y_hat=y_hat_arr, log_weights=log_weights_arr)
     assert np.all((loo_pit_data >= 0) & (loo_pit_data <= 1))
 
+
 def test_loo_pit_multi_lik():
     idata = load_arviz_data("radon")
-    idata.log_likelihood["by_county"] = idata.log_likelihood["y"].groupby(
-        idata.constant_data["county_idx"]
-    ).sum()
+    idata.log_likelihood["by_county"] = (
+        idata.log_likelihood["y"].groupby(idata.constant_data["county_idx"]).sum()
+    )
     loo_pit_data = loo_pit(idata, y="y")
     assert np.all((loo_pit_data >= 0) & (loo_pit_data <= 1))
 
