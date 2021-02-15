957ec337f7607fb4bbe91e3f2c8998003ece701b
Rishabh261998 <rishabh.lfs@gmail.com>
2021-02-15 22:23:19 +0530

    Added coords argument examples in documentation

--- arviz/plots/posteriorplot.py
+++ arviz/plots/posteriorplot.py
@@ -149,6 +149,13 @@ def plot_posterior(
         >>> rope = {'mu': [{'rope': (-2, 2)}], 'theta': [{'school': 'Choate', 'rope': (2, 4)}]}
         >>> az.plot_posterior(data, var_names=['mu', 'theta'], rope=rope)
 
+    Adding Coordinates(coords) for distributions selection
+
+    .. plot::
+        :context: close-figs
+
+        >>> coords = {"school": ["Choate","Phillips Exeter"]}
+        >>> az.plot_posterior(data, var_names=["mu", "theta"], coords=coords, rope=(-1, 1))
 
     Add reference lines
 
