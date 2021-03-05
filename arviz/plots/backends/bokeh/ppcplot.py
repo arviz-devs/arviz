6ba3885b10eaf2c835b392a43712bb4b185b05e7
Rishabh261998 <rishabh.lfs@gmail.com>
2021-03-06 01:40:54 +0530

    Minor Change

--- arviz/plots/backends/bokeh/ppcplot.py
+++ arviz/plots/backends/bokeh/ppcplot.py
@@ -1,10 +1,10 @@
 """Bokeh Posterior predictive plot."""
 import numpy as np
+from bokeh.models.annotations import Legend
 
 from ....stats.density_utils import get_bins, histogram, kde
 from ...kdeplot import plot_kde
 from ...plot_utils import _scale_fig_size, vectorized_to_hex
-from bokeh.models.annotations import Legend
 
 from .. import show_layout
 from . import backend_kwarg_defaults, create_axes_grid
