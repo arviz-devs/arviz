fa80361fa5110a27c49a90aaf4db62b154478c41
Kim <kim.philipp.jablonski@gmail.com>
2021-03-19 15:53:10 +0100

    Fix `az.from_numpyro` crash when running MCMC with `thinning > 1` (#1619)
    * Fix crash when importing numpyro model with thinning > 1
    
    * Format code
    
    * Add test for numpyro import with thinning
    
    * Add entry to CHANGELOG.md
    
    * Update link to PR in CHANGELOG.md
    
    * Give model tested in 'test_mcmc_with_thinning' a more generic name
    
    * Test more MCMC parameters in 'test_mcmc_with_thinning'
    
    * Test correct quantity in 'test_mcmc_with_thinning'
    
    * Test 'test_mcmc_with_thinning' with multiple chain counts
    
    * Simplify implementation of numpyro thinning fix

--- arviz/tests/external_tests/test_data_numpyro.py
+++ arviz/tests/external_tests/test_data_numpyro.py
@@ -227,3 +227,29 @@ class TestDataNumPyro:
         inference_data = from_numpyro(predictions=predictions, num_chains=chains)
         nchains = inference_data.predictions.dims["chain"]
         assert nchains == chains
+
+    @pytest.mark.parametrize("nchains", [1, 2])
+    @pytest.mark.parametrize("thin", [1, 2, 3, 5, 10])
+    def test_mcmc_with_thinning(self, nchains, thin):
+        import numpyro
+        import numpyro.distributions as dist
+        from numpyro.infer import MCMC, NUTS
+
+        x = np.random.normal(10, 3, size=100)
+
+        def model(x):
+            numpyro.sample(
+                "x",
+                dist.Normal(
+                    numpyro.sample("loc", dist.Uniform(0, 20)),
+                    numpyro.sample("scale", dist.Uniform(0, 20)),
+                ),
+                obs=x,
+            )
+
+        nuts_kernel = NUTS(model)
+        mcmc = MCMC(nuts_kernel, num_warmup=100, num_samples=400, num_chains=nchains, thinning=thin)
+        mcmc.run(PRNGKey(0), x=x)
+
+        inference_data = from_numpyro(mcmc)
+        assert inference_data.posterior["loc"].shape == (nchains, 400 // thin)
