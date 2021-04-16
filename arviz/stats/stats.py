df87793c65a70e4db83d31674336b66a0b27726c
nrakocz <39495768+nrakocz@users.noreply.github.com>
2021-04-16 10:21:49 -0700

    r2_score: removed "if" statement regarding number of dimensions in y_pred
    y_pred should have 2 dimensions: 
    - at least more than 1 data point to fit a regression line
    - at least more than 1 posterior sample to get a Bayesian R2 distribution

--- arviz/stats/stats.py
+++ arviz/stats/stats.py
@@ -995,12 +995,12 @@ def r2_score(y_true, y_pred,dist=False):
 
     """
     _numba_flag = Numba.numba_flag
-    if y_pred.ndim == 1:
-        var_y_est = _numba_var(svar, np.var, y_pred)
-        var_e = _numba_var(svar, np.var, (y_true - y_pred))
-    else:
-        var_y_est = _numba_var(svar, np.var, y_pred,axis=1)
-        var_e = _numba_var(svar, np.var, (y_true - y_pred), axis=1)
+#     if y_pred.ndim == 1:
+#         var_y_est = _numba_var(svar, np.var, y_pred)
+#         var_e = _numba_var(svar, np.var, (y_true - y_pred))
+#     else:
+    var_y_est = _numba_var(svar, np.var, y_pred,axis=1)
+    var_e = _numba_var(svar, np.var, (y_true - y_pred), axis=1)
     r_squared = var_y_est / (var_y_est + var_e)
     
     if(dist):
