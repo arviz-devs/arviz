01adbb6b6c362ddf6b269cf12af0f2d2dff080e6
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2021-04-16 16:22:33 +0300

    lint

--- arviz/data/io_pymc3.py
+++ arviz/data/io_pymc3.py
@@ -1,6 +1,10 @@
+# pylint: disable=unused-import
 """PyMC3-specific conversion code."""
 
 try:
-    from pymc3 import to_inference_data as from_pymc3, predictions_to_inference_data as from_pymc3_predictions
+    from pymc3 import (
+        to_inference_data as from_pymc3,
+        predictions_to_inference_data as from_pymc3_predictions,
+    )
 except ImportError:
     from .io_pymc3_3x import from_pymc3, from_pymc3_predictions
