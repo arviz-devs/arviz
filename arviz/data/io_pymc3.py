b779159968d26c52cc56cd37fe1b0572cff3e95b
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2021-05-27 20:19:29 +0300

    update version check for pymc3
    I think this approach should not import pymc3 to check the version

--- arviz/data/io_pymc3.py
+++ arviz/data/io_pymc3.py
@@ -1,10 +1,42 @@
 # pylint: disable=unused-import
 """PyMC3-specific conversion code."""
+import pkg_resources
+import packaging
 
-try:
-    from pymc3 import (
-        to_inference_data as from_pymc3,
-        predictions_to_inference_data as from_pymc3_predictions,
-    )
-except ImportError:
+__all__ = ["from_pymc3", "from_pymc3_predictions"]
+
+pymc3_version = pkg_resources.get_distribution("pymc3").version
+
+if packaging.version.parse(pymc3_version) < packaging.version.parse("4.0"):
     from .io_pymc3_3x import from_pymc3, from_pymc3_predictions
+else:
+
+    def from_pymc3(
+        trace=None,
+        *,
+        prior=None,
+        posterior_predictive=None,
+        log_likelihood=None,
+        coords=None,
+        dims=None,
+        model=None,
+        save_warmup=None,
+        density_dist_obs=True,
+    ):
+        raise NotImplementedError(
+            "The converter has been moved to PyMC3 codebase, use pymc3.to_inference_data"
+        )
+
+    def from_pymc3_predictions(
+        predictions,
+        posterior_trace=None,
+        model=None,
+        coords=None,
+        dims=None,
+        idata_orig=None,
+        inplace = False,
+    ):
+        raise NotImplementedError(
+            "The converter has been moved to PyMC3 codebase, "
+            "use pymc3.to_inference_data_predictions"
+        )
