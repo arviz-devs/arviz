d90c79d1a5d4b58af9cb577c7b2c6ea3f9327b16
Utkarsh Mahweshwari <32706356+utkarsh-maheshwari@users.noreply.github.com>
2021-06-07 23:43:48 +0530

    Remove testing dependency on http download for radon dataset (#1717)

--- arviz/tests/base_tests/test_stats.py
+++ arviz/tests/base_tests/test_stats.py
@@ -605,9 +605,16 @@ def test_loo_pit_multidim(multidim_models, args):
 
 
 def test_loo_pit_multi_lik():
-    idata = load_arviz_data("radon")
-    idata.log_likelihood["by_county"] = (
-        idata.log_likelihood["y"].groupby(idata.constant_data["county_idx"]).sum()
+    rng = np.random.default_rng(0)
+    post_pred = rng.standard_normal(size=(4, 100, 10))
+    obs = np.quantile(post_pred, np.linspace(0, 1, 10))
+    obs[0] *= 0.9
+    obs[-1] *= 1.1
+    idata = from_dict(
+        posterior={"a": np.random.randn(4, 100)},
+        posterior_predictive={"y": post_pred},
+        observed_data={"y": obs},
+        log_likelihood={"y": -(post_pred ** 2), "decoy": np.zeros_like(post_pred)},
     )
     loo_pit_data = loo_pit(idata, y="y")
     assert np.all((loo_pit_data >= 0) & (loo_pit_data <= 1))
