8887ee55baad6532d4f9082661c945b636244114
Ravin Kumar <ravinsdrive@gmail.com>
2021-07-04 19:35:13 -0700

    Stan fix

--- arviz/wrappers/wrap_stan.py
+++ arviz/wrappers/wrap_stan.py
@@ -50,17 +50,17 @@ class StanSamplingWrapper(SamplingWrapper):
         """
         raise NotImplementedError("sel_observations must be implemented on a model basis")
 
-    def get_inference_data(self, fit): # pylint: disable=arguments-renamed
+    def get_inference_data(self, fitted_model): # pylint: disable=arguments-renamed
         """Convert the fit object returned by ``self.sample`` to InferenceData."""
-        if fit.__class__.__name__ == "CmdStanMCMC":
-            idata = from_cmdstanpy(posterior=fit, **self.idata_kwargs)
+        if fitted_model.__class__.__name__ == "CmdStanMCMC":
+            idata = from_cmdstanpy(posterior=fitted_model, **self.idata_kwargs)
         else:
-            idata = from_pystan(posterior=fit, **self.idata_kwargs)
+            idata = from_pystan(posterior=fitted_model, **self.idata_kwargs)
         return idata
 
-    def log_likelihood__i(self, excluded_obs_log_like, idata__i):
+    def log_likelihood__i(self, excluded_obs, idata__i):
         """Retrieve the log likelihood of the excluded observations from ``idata__i``."""
-        return idata__i.log_likelihood[excluded_obs_log_like]
+        return idata__i.log_likelihood[excluded_obs]
 
 
 class PyStan2SamplingWrapper(StanSamplingWrapper):
