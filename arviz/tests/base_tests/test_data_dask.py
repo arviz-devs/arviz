9581d4f13732e8ce13b7d507760f1302bcd90269
mortonjt <jamietmorton@gmail.com>
2021-08-31 14:53:14 -0400

    maybe we don't need the synchronize black

--- arviz/tests/base_tests/test_data_dask.py
+++ arviz/tests/base_tests/test_data_dask.py
@@ -15,34 +15,32 @@ class TestDataDask:
 
     def test_dask_chunk_group_kwds(self):
         Dask.enable_dask(dask_kwargs={"output_dtypes": [float]})
-        with dask.config.set(scheduler="synchronous"):
-            group_kwargs = {
-                'posterior': {'chunks': {'w_dim_0': 2, 'true_w_dim_0' : 2}},
-                'posterior_predictive': {'chunks': {'w_dim_0': 2, 'true_w_dim_0': 2}}
-            }
-            centered_data = az.load_arviz_data("regression10d", **group_kwargs)
-            exp = [('chain', (4,)),
-                   ('draw', (500,)),
-                   ('true_w_dim_0', (2, 2, 2, 2, 2,)),
-                   ('w_dim_0', (2, 2, 2, 2, 2,))]
-            res = list(centered_data.posterior.chunks.items())
-            res.sort()
-            exp.sort()
-            assert res ==  exp
+        group_kwargs = {
+            'posterior': {'chunks': {'w_dim_0': 2, 'true_w_dim_0' : 2}},
+            'posterior_predictive': {'chunks': {'w_dim_0': 2, 'true_w_dim_0': 2}}
+        }
+        centered_data = az.load_arviz_data("regression10d", **group_kwargs)
+        exp = [('chain', (4,)),
+               ('draw', (500,)),
+               ('true_w_dim_0', (2, 2, 2, 2, 2,)),
+               ('w_dim_0', (2, 2, 2, 2, 2,))]
+        res = list(centered_data.posterior.chunks.items())
+        res.sort()
+        exp.sort()
+        assert res ==  exp
 
 
     def test_dask_chunk_group_regex(self):
-        with dask.config.set(scheduler="synchronous"):
-            Dask.enable_dask(dask_kwargs={"output_dtypes": [float]})
-            group_kwargs = {
-                "posterior.*": {'chunks': {'w_dim_0': 10, 'true_w_dim_0' : 10}}
-            }
-            centered_data = az.load_arviz_data("regression10d", regex=True, **group_kwargs)
-            exp = [('chain', (4,)),
-                   ('draw', (500,)),
-                   ('true_w_dim_0', (10,)),
-                   ('w_dim_0', (10,))]
-            res = list(centered_data.posterior.chunks.items())
-            res.sort()
-            exp.sort()
-            assert res == exp
+        Dask.enable_dask(dask_kwargs={"output_dtypes": [float]})
+        group_kwargs = {
+            "posterior.*": {'chunks': {'w_dim_0': 10, 'true_w_dim_0' : 10}}
+        }
+        centered_data = az.load_arviz_data("regression10d", regex=True, **group_kwargs)
+        exp = [('chain', (4,)),
+               ('draw', (500,)),
+               ('true_w_dim_0', (10,)),
+               ('w_dim_0', (10,))]
+        res = list(centered_data.posterior.chunks.items())
+        res.sort()
+        exp.sort()
+        assert res == exp
