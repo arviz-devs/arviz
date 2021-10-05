ff8ce1882d5ddacea7f75a71ea1ef238a82ea49e
Rosheen Naeem <rosheennaeem4@gmail.com>
2021-10-05 17:55:07 +0500

    added raw html in index file

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -152,8 +152,7 @@ html_theme_options = {
             "icon": "fab fa-twitter-square",
         },
     ],
-    "navbar_start": ["navbar-logo", "navbar-version", "search-field.html"],
-    "navbar_end": ["navbar-icon-links.html", "search-field.html"],
+    "navbar_start": ["navbar-logo", "navbar-version"],
     "use_edit_page_button": False,  # TODO: see how to skip of fix for generated pages
     "google_analytics_id": "G-W1G68W77YV",
 }
@@ -165,6 +164,7 @@ html_context = {
 }
 html_sidebars: Dict[str, Any] = {
     "community": [],
+    "about_us": [],
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
