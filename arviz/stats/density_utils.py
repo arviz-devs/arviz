83d4bd80c5e2de499c857427db813f80d7f88e8c
Rosheen Naeem <rosheennaeem4@gmail.com>
2021-10-06 17:19:50 +0500

    incorporated suggestion

--- arviz/stats/density_utils.py
+++ arviz/stats/density_utils.py
@@ -445,7 +445,6 @@ def kde(x, circular=False, **kwargs):
         :context: close-figs
 
         # It accepts tuples and lists of length 2.
-
         >>> grid, pdf = kde(rvs, bound_correction=False, custom_lims=(0, 10))
         >>> plt.plot(grid, pdf)
         >>> plt.show()
