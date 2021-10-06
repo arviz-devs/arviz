382a7a152f58cf4ed49c931293acb7f5463f7042
Rosheen Naeem <rosheennaeem4@gmail.com>
2021-10-07 01:01:21 +0500

    wip

---
+++ doc/source/_static/js/custom.js
@@ -0,0 +1,8 @@
+function toggleView() {
+    var searchbar = document.getElementById("search-input");
+    if (searchbar.style.display === "none") {
+        searchbar.style.display = "block";
+    } else {
+        searchbar.style.display = "none";
+    }
+  }
