0c98e43934e466c6a96c36bb77834d073b5724a8
Rosheen Naeem <rosheennaeem4@gmail.com>
2021-11-01 20:45:03 +0500

    Fixed error

--- arviz/stats/stats.py
+++ arviz/stats/stats.py
@@ -602,7 +602,7 @@ def loo(data, pointwise=None, var_name=None, reff=None, scale=None):
     data: obj
         Any object that can be converted to an :class:`arviz.InferenceData` object.
         Refer to documentation of
-       :func:`arviz.convert_to_dataset` for details.
+        :func:`arviz.convert_to_dataset` for details.
     pointwise: bool, optional
         If True the pointwise predictive accuracy will be returned. Defaults to
         ``stats.ic_pointwise`` rcParam.
