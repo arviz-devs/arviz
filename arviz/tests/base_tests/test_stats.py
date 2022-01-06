e15449177c5bf4db57c34e87f38a926217d7f57c
Simon Mutch <smutch@unimelb.edu.au>
2022-01-06 02:51:01 +0000

    Add test for hdi with multimodal=True and >1 vars
    This test case ensures that differing numbers of modes and hdi values
    can be recovered for each variable.

--- arviz/tests/base_tests/test_stats.py
+++ arviz/tests/base_tests/test_stats.py
@@ -121,6 +121,18 @@ def test_hdi_multimodal():
     intervals = hdi(normal_sample, multimodal=True)
     assert_array_almost_equal(intervals, [[-5.8, -2.2], [0.9, 3.1]], 1)
 
+def test_hdi_multimodal_multivars():
+    size = 2500000
+    var1 = np.concatenate((np.random.normal(-4, 1, size), np.random.normal(2, 0.5, size)))
+    var2 = np.random.normal(8, 1, size*2)
+    sample = Dataset(
+        {"var1": (("chain", "draw"), var1[np.newaxis, :]), "var2": (("chain", "draw"), var2[np.newaxis, :])},
+        coords={"chain": [0], "draw": np.arange(size * 2)},
+    )
+    intervals = hdi(sample, multimodal=True)
+    assert_array_almost_equal(intervals.var1, [[-5.8, -2.2], [0.9, 3.1]], 1)
+    assert_array_almost_equal(intervals.var2, [[6.1, 9.9], [np.nan, np.nan]], 1)
+
 
 def test_hdi_circular():
     normal_sample = np.random.vonmises(np.pi, 1, 5000000)
