fixed implementation of R2 score. R2 now measures variance over the correct axis. also added an input to return the full distribution  (#1666)
* fixed bug related with bayesian R2: variance is now applied to the correct axis.

* fixed bug related with bayesian R2. variance is now applied to the correct axis. Also added the option to return the full R2 distribution

* added description to r2_score changes

description  about the new input and output

* r2_score: removed "if" statement regarding number of dimensions in y_pred

y_pred should have 2 dimensions: 
- at least more than 1 data point to fit a regression line
- at least more than 1 posterior sample to get a Bayesian R2 distribution

* changed dist to summary to add clearity. added description.

* Update logic and add a new function r2_samples

* Fixes

* Add space

* Update r2 tests

* Remove multivariate test

* Update stats.py

Co-authored-by: Ari Hartikainen <ahartikainen@users.noreply.github.com>

arviz/stats/stats.py
@@ -42,6 +42,7 @@ __all__ = [
     "loo",
     "loo_pit",
     "psislw",
+    "r2_samples",
     "r2_score",
     "summary",
     "waic",
@@ -985,14 +986,58 @@ def _gpinv(probs, kappa, sigma):
     return x
 
 
+def r2_samples(y_true, y_pred):
+    """R² samples for Bayesian regression models. Only valid for linear models.
+
+    Parameters
+    ----------
+    y_true: array-like of shape = (n_outputs,)
+        Ground truth (correct) target values.
+    y_pred: array-like of shape = (n_posterior_samples, n_outputs)
+        Estimated target values.
+
+    Returns
+    -------
+    Pandas Series with the following indices:
+    Bayesian R² samples.
+
+    See Also
+    --------
+    plot_lm : Posterior predictive and mean plots for regression-like data.
+
+    Examples
+    --------
+    Calculate R² samples for Bayesian regression models :
+
+    .. ipython::
+
+        In [1]: import arviz as az
+           ...: data = az.load_arviz_data('regression1d')
+           ...: y_true = data.observed_data["y"].values
+           ...: y_pred = data.posterior_predictive.stack(sample=("chain", "draw"))["y"].values.T
+           ...: az.r2_samples(y_true, y_pred)
+
+    """
+    _numba_flag = Numba.numba_flag
+    if y_pred.ndim == 1:
+        var_y_est = _numba_var(svar, np.var, y_pred)
+        var_e = _numba_var(svar, np.var, (y_true - y_pred))
+    else:
+        var_y_est = _numba_var(svar, np.var, y_pred, axis=1)
+        var_e = _numba_var(svar, np.var, (y_true - y_pred), axis=1)
+    r_squared = var_y_est / (var_y_est + var_e)
+
+    return r_squared
+
+
 def r2_score(y_true, y_pred):
     """R² for Bayesian regression models. Only valid for linear models.
 
     Parameters
     ----------
-    y_true: array-like of shape = (n_samples) or (n_samples, n_outputs)
+    y_true: array-like of shape = (n_outputs,)
         Ground truth (correct) target values.
-    y_pred: array-like of shape = (n_samples) or (n_samples, n_outputs)
+    y_pred: array-like of shape = (n_posterior_samples, n_outputs)
         Estimated target values.
 
     Returns
@@ -1018,15 +1063,7 @@ def r2_score(y_true, y_pred):
            ...: az.r2_score(y_true, y_pred)
 
     """
-    _numba_flag = Numba.numba_flag
-    if y_pred.ndim == 1:
-        var_y_est = _numba_var(svar, np.var, y_pred)
-        var_e = _numba_var(svar, np.var, (y_true - y_pred))
-    else:
-        var_y_est = _numba_var(svar, np.var, y_pred.mean(0))
-        var_e = _numba_var(svar, np.var, (y_true - y_pred), axis=0)
-    r_squared = var_y_est / (var_y_est + var_e)
-
+    r_squared = r2_samples(y_true=y_true, y_pred=y_pred)
     return pd.Series([np.mean(r_squared), np.std(r_squared)], index=["r2", "r2_std"])
 
 

arviz/tests/base_tests/test_stats.py
@@ -161,17 +161,9 @@ def test_hdi_skipna():
 def test_r2_score():
     x = np.linspace(0, 1, 100)
     y = np.random.normal(x, 1)
+    y_pred = x + np.random.randn(300, 100)
     res = linregress(x, y)
-    assert_allclose(res.rvalue ** 2, r2_score(y, res.intercept + res.slope * x).r2, 2)
-
-
-def test_r2_score_multivariate():
-    x = np.linspace(0, 1, 100)
-    y = np.random.normal(x, 1)
-    res = linregress(x, y)
-    y_multivariate = np.c_[y, y]
-    y_multivariate_pred = np.c_[res.intercept + res.slope * x, res.intercept + res.slope * x]
-    assert not np.isnan(r2_score(y_multivariate, y_multivariate_pred).r2)
+    assert_allclose(res.rvalue ** 2, r2_score(y, y_pred).r2, 2)
 
 
 @pytest.mark.parametrize("method", ["stacking", "BB-pseudo-BMA", "pseudo-BMA"])