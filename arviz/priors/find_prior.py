5a58dd501c9fb448662d0a36d1a0330a70b90a2e
aloctavodia <aloctavodia@gmail.com>
2022-02-23 11:07:54 +0200

    black

--- arviz/priors/find_prior.py
+++ arviz/priors/find_prior.py
@@ -2,19 +2,29 @@ from scipy import stats
 from scipy.optimize import least_squares
 import matplotlib.pyplot as plt
 
-from .prior_utils import (get_parametrization,
-                          check_boundaries,
-                          relative_error,
-                          sane_scipy,
-                          compute_xvals,
-                          func,
-                          get_normal,
-                          dist_dict,
-                          )
+from .prior_utils import (
+    get_parametrization,
+    check_boundaries,
+    relative_error,
+    sane_scipy,
+    compute_xvals,
+    func,
+    get_normal,
+    dist_dict,
+)
 
 
-
-def find_prior(name="normal", lower=-1, upper=1, mass=0.90, extra=None, parametrization="pymc", plot=True, figsize=(10,4), ax=None):
+def find_prior(
+    name="normal",
+    lower=-1,
+    upper=1,
+    mass=0.90,
+    extra=None,
+    parametrization="pymc",
+    plot=True,
+    figsize=(10, 4),
+    ax=None,
+):
     """
     Find parameters for a given distribution with `mass` between `lower` and `upper`.
 
@@ -32,7 +42,7 @@ def find_prior(name="normal", lower=-1, upper=1, mass=0.90, extra=None, parametr
         Whether to plot the distribution, and lower and upper bounds. Defautls to True.
     figsize: tuple
         size of the figure when ``plot=True``
-    ax: 
+    ax:
 
     Returns
     -------
@@ -40,29 +50,29 @@ def find_prior(name="normal", lower=-1, upper=1, mass=0.90, extra=None, parametr
     rv_frozen : scipy.stats.distributions.rv_frozen
         Notice that the returned rv_frozen object always use the scipy parametrization,
         irrespective of the value of `parametrization` argument.
-        Unlike standard rv_frozen objects this one has a name attribute    
+        Unlike standard rv_frozen objects this one has a name attribute
     opt: scipy.optimize.OptimizeResult
         Represents the optimization result.
     """
     check_boundaries(name, lower, upper)
-        
+
     opt = get_normal(lower, upper, mass)
     mu_init, sigma_init = opt["x"]
-        
+
     if name == "normal":
         rv_frozen = stats.norm(mu_init, sigma_init)
         rv_frozen.name = name
         a, b = opt["x"]
     else:
         if name == "beta":
-            kappa = (mu_init*(1-mu_init) / (sigma_init)**2) -1
+            kappa = (mu_init * (1 - mu_init) / (sigma_init) ** 2) - 1
             a = mu_init * kappa
-            b = (1-mu_init) * kappa
+            b = (1 - mu_init) * kappa
             dist = stats.beta
-    
+
         elif name == "lognormal":
-            a = np.log(mu_init**2 / (sigma_init**2 + mu_init**2)**0.5)
-            b = np.log(sigma_init**2 / mu_init**2 + 1)**0.5
+            a = np.log(mu_init ** 2 / (sigma_init ** 2 + mu_init ** 2) ** 0.5)
+            b = np.log(sigma_init ** 2 / mu_init ** 2 + 1) ** 0.5
             dist = stats.lognorm
 
         elif name == "exponential":
@@ -71,32 +81,32 @@ def find_prior(name="normal", lower=-1, upper=1, mass=0.90, extra=None, parametr
             dist = stats.expon
 
         elif name == "gamma":
-            a = mu_init**2 / sigma_init**2
-            b = sigma_init**2 / mu_init
+            a = mu_init ** 2 / sigma_init ** 2
+            b = sigma_init ** 2 / mu_init
             dist = stats.gamma
         elif name == "student":
             a = mu_init
             b = sigma_init
             dist = stats.t
-            
+
         opt = least_squares(func, x0=(a, b), args=(dist, lower, upper, mass, extra))
         a, b = opt["x"]
-        rv_frozen =  sane_scipy(dist, a, b, extra)
+        rv_frozen = sane_scipy(dist, a, b, extra)
 
     if plot:
         r_error = relative_error(rv_frozen, upper, lower, mass)
         x = compute_xvals(rv_frozen)
         if ax is None:
             _, ax = plt.subplots(figsize=figsize)
-        color = next(ax._get_lines.prop_cycler)['color']
+        color = next(ax._get_lines.prop_cycler)["color"]
         ax.plot([lower, upper], [0, 0], "o", color=color, alpha=0.5)
         title = get_parametrization(name, a, b, extra, dist_dict, parametrization)
         subtitle = f"relative error = {r_error:.2f}"
-        ax.plot(x, rv_frozen.pdf(x), label=title+"\n"+subtitle, color=color)
+        ax.plot(x, rv_frozen.pdf(x), label=title + "\n" + subtitle, color=color)
 
     box = ax.get_position()
     ax.set_position([box.x0, box.y0, box.width, box.height])
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
     ax.set_yticks([])
 
     return ax, rv_frozen, opt
--- arviz/priors/prior_utils.py
+++ arviz/priors/prior_utils.py
@@ -3,13 +3,15 @@ from scipy import stats
 from scipy.optimize import least_squares
 
 
-dist_dict = {"beta": ("alpha", "beta"),
-             "exponential":  ("lam",),
-             "gamma":  ("alpha", "beta"),
-             "lognormal": ("mu", "sigma"),
-             "normal": ("mu", "sigma"),
-             "student": ("nu", "mu", "sigma"),
-            } # This names could be different for different "parametrizations"
+dist_dict = {
+    "beta": ("alpha", "beta"),
+    "exponential": ("lam",),
+    "gamma": ("alpha", "beta"),
+    "lognormal": ("mu", "sigma"),
+    "normal": ("mu", "sigma"),
+    "student": ("nu", "mu", "sigma"),
+}  # This names could be different for different "parametrizations"
+
 
 def get_parametrization(name, a, b, extra, dist_dict, parametrization):
     if parametrization == "pymc":
@@ -32,20 +34,25 @@ def get_parametrization(name, a, b, extra, dist_dict, parametrization):
             title = f"{name}({dist_dict[name][0]}={extra:.2f}, {dist_dict[name][1]}={a:.2f}, {dist_dict[name][2]}={b:.2f})"
     return title
 
+
 def check_boundaries(name, lower, upper):
     DOMAIN_ERROR = f"The provided boundaries are outside the domain of the {name} distribution"
     if name == "beta":
         if lower == 0 and upper == 1:
-            raise ValueError("Given the provided boundaries, mass will be always 1. Please provide other values")
+            raise ValueError(
+                "Given the provided boundaries, mass will be always 1. Please provide other values"
+            )
         if lower < 0 or upper > 1:
-                raise ValueError(DOMAIN_ERROR)
+            raise ValueError(DOMAIN_ERROR)
     elif name in ["exponential", "gamma", "lognormal"]:
         if lower < 0:
             raise ValueError(DOMAIN_ERROR)
 
+
 def relative_error(rv_frozen, upper, lower, requiered_mass):
     computed_mass = rv_frozen.cdf(upper) - rv_frozen.cdf(lower)
-    return (computed_mass-requiered_mass) / requiered_mass * 100
+    return (computed_mass - requiered_mass) / requiered_mass * 100
+
 
 def sane_scipy(dist, a, b, extra=None):
     dist_name = dist.name
@@ -63,12 +70,13 @@ def sane_scipy(dist, a, b, extra=None):
     rv_frozen.name = dist_name
     return rv_frozen
 
+
 def compute_xvals(rv_frozen):
     if np.isfinite(rv_frozen.a):
         lq = rv_frozen.a
     else:
         lq = 0.001
-    
+
     if np.isfinite(rv_frozen.b):
         uq = rv_frozen.b
     else:
@@ -77,16 +85,18 @@ def compute_xvals(rv_frozen):
     x = np.linspace(rv_frozen.ppf(lq), rv_frozen.ppf(uq), 1000)
     return x
 
+
 def func(params, dist, lower, upper, mass, extra=None):
     a, b = params
     rv_frozen = sane_scipy(dist, a, b, extra)
     cdf0 = rv_frozen.cdf(lower)
     cdf1 = rv_frozen.cdf(upper)
-    cdf_loss = ((cdf1 - cdf0) - mass)
+    cdf_loss = (cdf1 - cdf0) - mass
     return cdf_loss
 
+
 def get_normal(lower, upper, mass):
-    mu_init = (lower+upper)/2
-    sigma_init = ((upper-lower) / 4) / mass
+    mu_init = (lower + upper) / 2
+    sigma_init = ((upper - lower) / 4) / mass
     opt = least_squares(func, x0=(mu_init, sigma_init), args=(stats.norm, lower, upper, mass))
     return opt
