22a963b216a08506525bd09c16d3a2807a166d39
prahasR <prahasranjan@gmail.com>
2022-03-01 00:15:34 +0530

    removed test from test_plots_matplotlib and added to test_utils

--- arviz/tests/base_tests/test_plots_matplotlib.py
+++ arviz/tests/base_tests/test_plots_matplotlib.py
@@ -108,25 +108,6 @@ def data_list():
 
 
 @pytest.mark.parametrize(
-    "var_args",
-    [
-        (["ta"], ["beta1", "beta2", "theta"], "like"),
-        (["~beta"], ["phi", "theta"], "like"),
-        (["beta[0-9]+"], ["beta1", "beta2"], "regex"),
-        (["^p"], ["phi"], "regex"),
-        (["~^t"], ["beta1", "beta2", "phi"], "regex"),
-    ],
-)
-def test_var_names_filter(var_args):
-    samples = np.random.randn(10)
-    data1 = dict_to_dataset({"beta1": samples, "beta2": samples, "phi": samples})
-    data2 = dict_to_dataset({"beta1": samples, "beta2": samples, "theta": samples})
-    data = [data1, data2]
-    var_names, expected, filter_vars = var_args
-    assert _var_names(var_names, data, filter_vars) == expected
-
-
-@pytest.mark.parametrize(
     "kwargs",
     [
         {"point_estimate": "mean"},
--- arviz/tests/base_tests/test_utils.py
+++ arviz/tests/base_tests/test_utils.py
@@ -69,6 +69,25 @@ def test_var_names_key_error(data):
 @pytest.mark.parametrize(
     "var_args",
     [
+        (["ta"], ["beta1", "beta2", "theta"], "like"),
+        (["~beta"], ["phi", "theta"], "like"),
+        (["beta[0-9]+"], ["beta1", "beta2"], "regex"),
+        (["^p"], ["phi"], "regex"),
+        (["~^t"], ["beta1", "beta2", "phi"], "regex"),
+    ],
+)
+def test_var_names_filter_multiple_input(var_args):
+    samples = np.random.randn(10)
+    data1 = dict_to_dataset({"beta1": samples, "beta2": samples, "phi": samples})
+    data2 = dict_to_dataset({"beta1": samples, "beta2": samples, "theta": samples})
+    data = [data1, data2]
+    var_names, expected, filter_vars = var_args
+    assert _var_names(var_names, data, filter_vars) == expected
+
+
+@pytest.mark.parametrize(
+    "var_args",
+    [
         (["alpha", "beta"], ["alpha", "beta1", "beta2"], "like"),
         (["~beta"], ["alpha", "p1", "p2", "phi", "theta", "theta_t"], "like"),
         (["theta"], ["theta", "theta_t"], "like"),
