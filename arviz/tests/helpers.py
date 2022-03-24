884f38492bebc630b7645164ed948c074c1d551b
Rob Zinkov <rob@zinkov.com>
2022-03-24 02:36:45 +0000

    Add docstrings to helper class

--- arviz/tests/helpers.py
+++ arviz/tests/helpers.py
@@ -19,10 +19,12 @@ _log = logging.getLogger(__name__)
 
 
 class TestRandomVariable:
+    """Example class for random variables"""
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
+        """Returns argument to constructor as string representation"""
         return self.name
 
 
