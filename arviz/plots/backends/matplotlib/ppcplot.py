91a7582c877471dd112ce733994d78b0d61ea649
charlesdong1991 <kaiqi.dong@traderepublic.com>
2022-06-12 14:50:57 +0200

    black

--- arviz/plots/backends/matplotlib/ppcplot.py
+++ arviz/plots/backends/matplotlib/ppcplot.py
@@ -407,9 +407,7 @@ def _set_animation(
             _, y_vals, x_vals = histogram(vals, bins="auto")
             (line,) = ax.plot(x_vals[:-1], y_vals, **plot_kwargs)
 
-            max_max = max(
-                max(histogram(pp_sampled_vals[i], bins="auto")[1]) for i in range(length)
-            )
+            max_max = max(max(histogram(pp_sampled_vals[i], bins="auto")[1]) for i in range(length))
 
             ax.set_ylim(0, max_max)
 
