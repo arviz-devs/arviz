b56a2b952351b95ca199bb70bc87b49faca9a73d
Hector Munoz <hectormz.git@gmail.com>
2022-07-04 19:02:32 -0700

    Simplify regular expression access

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def get_version():
     for line in lines:
         mo = re.search(version_regex, line, re.M)
         if mo:
-            return mo.group(1)
+            return mo[1]
     raise RuntimeError(f"Unable to find version in {VERSION_FILE}.")
 
 
