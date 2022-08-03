6c50e31c48ac9912d2f4398cbb15d823b0d3bfdf
Damee Olawuyi <dameeolawuyi@gmail.com>
2022-08-03 07:13:55 +0100

    updated densityplot

--- arviz/plots/densityplot.py
+++ arviz/plots/densityplot.py
@@ -110,7 +110,7 @@ def plot_density(
     Labeller : labeller instance, optional
         Class providing the method ``make_label_vert`` to generate the labels in the plot titles.
         Read the :ref:`label_guide` for more details and usage examples.
-    ax : 2D array-like of matplotlib axes or bokeh figures, optional
+    ax : 2D array-like of matplotlib _axes or bokeh _figure, optional
         A 2D array of locations into which to plot the densities. If not supplied, Arviz will create
         its own array of plot areas (and return it).
     backend : str, optional
