4d23d1a3ee4837787c1e7682416be91c24c3b3d8
Seth Axen <seth.axen@gmail.com>
2022-08-15 23:41:55 +0200

    Explicitly specify package data

--- setup.py
+++ setup.py
@@ -57,6 +57,7 @@ setup(
     extras_require=dict(all=get_requirements_optional()),  # test=get_requirements_dev(),
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
+    package_data={'data': ['*.json', '*.nc'], 'plots': ['*.mplstyle'], 'tests': ["*"]},
     include_package_data=True,
     python_requires='>=3.7',
     classifiers=[
