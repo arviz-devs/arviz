bd6e1340a855373fbd39ab0fbe73c1827880f3e4
Damee Olawuyi <dameeolawuyi@gmail.com>
2022-08-16 14:00:44 +0100

    edited an 'ax' description

--- arviz/plots/autocorrplot.py
+++ arviz/plots/autocorrplot.py
@@ -59,7 +59,7 @@ def plot_autocorr(
     labeller : Labeller instance, optional
         Class providing the method ``make_label_vert`` to generate the labels in the plot titles.
         Read the :ref:`label_guide` for more details and usage examples.
-    ax : 2D array-like of matplotlib _axes or bokeh _figure, optional
+    ax : 2D array-like of matplotlib_axes or bokeh_figure, optional
         A 2D array of locations into which to plot the densities. If not supplied, Arviz will create
         its own array of plot areas (and return it).
     backend : str, optional
