1b31e3fe885fb709d874013894dcb93c3d2f330f
Ari Hartikainen <hartikainen.ari@gmail.com>
2022-08-28 21:10:52 +0300

    update test function docstring

--- arviz/stats/stats.py
+++ arviz/stats/stats.py
@@ -1941,7 +1941,7 @@ def apply_test_function(
         >>> import arviz as az
         >>> idata = az.load_arviz_data("centered_eight")
         >>> az.apply_test_function(idata, lambda y, theta: np.min(y))
-        >>> T = np.asscalar(idata.observed_data.T)
+        >>> T = idata.observed_data.T.item()
         >>> az.plot_posterior(idata, var_names=["T"], group="posterior_predictive", ref_val=T)
 
     """
