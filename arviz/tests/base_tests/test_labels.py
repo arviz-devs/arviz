8d3fe1d3d8000502d271be2d19dbee3736065ad1
Agustina Arroyuelo <agustinaarroyuelo@gmail.com>
2022-09-24 11:19:37 -0300

    run pylint

--- arviz/tests/base_tests/test_labels.py
+++ arviz/tests/base_tests/test_labels.py
@@ -8,7 +8,6 @@ from ...labels import (
     MapLabeller,
     NoModelLabeller,
     NoVarLabeller,
-    mix_labellers,
 )
 
 
@@ -54,7 +53,10 @@ def labellers():
     ],
 )
 class TestLabellers:
+    # pylint: disable=no-self-use
+    # pylint: disable=redefined-outer-name
     def test_make_label_vert(self, args, multidim_sels, labellers):
-        labeller_arg = labellers.labellers[args[0]]
+        name, expected_label = args
+        labeller_arg = labellers.labellers[name]
         label = labeller_arg.make_label_vert("theta", multidim_sels.sel, multidim_sels.isel)
-        assert label == args[1]
+        assert label == expected_label
