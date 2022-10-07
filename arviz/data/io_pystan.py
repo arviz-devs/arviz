8bd0c3bd95d317d944f5b8e0f6b9ef8e737d311b
Ari Hartikainen <hartikainen.ari@gmail.com>
2022-10-07 19:10:42 +0300

    fix calculation

--- arviz/data/io_pystan.py
+++ arviz/data/io_pystan.py
@@ -867,9 +867,8 @@ def get_draws_stan3(fit, model=None, variables=None, ignore=None, warmup=False, 
     if not fit.save_warmup:
         warmup = False
 
-    num_samples_saved = ceil((fit.num_samples + fit.num_warmup * fit.save_warmup) / fit.num_thin)
     num_samples = ceil(fit.num_samples / fit.num_thin)
-    num_warmup = num_samples_saved - num_samples
+    num_warmup = ceil((fit.num_warmup * fit.save_warmup) / fit.num_thin)
 
     if variables is None:
         variables = fit.param_names
@@ -923,9 +922,8 @@ def get_sample_stats_stan3(fit, variables=None, ignore=None, warmup=False, dtype
     if not fit.save_warmup:
         warmup = False
 
-    num_samples_saved = ceil((fit.num_samples + fit.num_warmup * fit.save_warmup) / fit.num_thin)
     num_samples = ceil(fit.num_samples / fit.num_thin)
-    num_warmup = num_samples_saved - num_samples
+    num_warmup = ceil((fit.num_warmup * fit.save_warmup) / fit.num_thin)
 
     data = OrderedDict()
     data_warmup = OrderedDict()
