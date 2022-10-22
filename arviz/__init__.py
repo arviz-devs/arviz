e1d6f71cddacf7d273f06190304c9156b33681a5
Agustina Arroyuelo <agustinaarroyuelo@gmail.com>
2022-10-22 12:12:49 -0300

    post release changes to v0.13.0dev1 (#2145)

--- arviz/__init__.py
+++ arviz/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wildcard-import,invalid-name,wrong-import-position
 """ArviZ is a library for exploratory analysis of Bayesian models."""
-__version__ = "0.13.0rc1"
+__version__ = "0.13.0dev1"
 
 import logging
 import os
