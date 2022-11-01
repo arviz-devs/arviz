0cb4486c4bbbf15ef4a79e4fe209e71735e32541
Or Duek <orduek@gmail.com>
2022-11-01 17:27:12 +0200

    Added suggested changed

--- arviz/plots/bf_plot.py
+++ arviz/plots/bf_plot.py
@@ -1,44 +1,61 @@
 # Plotting and reporting Bayes Factor given idata, var name, prior distribution and reference value
-from scipy import stats
+import logging
+
+
 import matplotlib.pyplot as plt
 import numpy as np
-import logging
+from scipy import stats
 # 
 _log = logging.getLogger(__name__)
 
-def plot_bf(idata, var_name, prior, family = 'normal',  ref_val=0, xlim=None, ax=None):
-    """
-    Bayes Factor approximated as the Savage-Dickey density ratio.
+def plot_bf(
+    idata,
+    var_name,
+    prior,
+    ref_val=0,
+    xlim=None,
+    ax=None):
+   
+    """Bayes Factor approximated as the Savage-Dickey density ratio.
     The Bayes factor is estimated by comparing a model 
     against a model in which the parameter of interest has been restricted to a point-null.
+   
+    Parameters
+    -----------
+    :idata: obj
+        a :class:`arviz.InferenceData` object
+    :var_name: str 
+        Name of variable we want to test.
+    :prior: numpy.array, optional
+        In case we want to use diffent prior (for sensitivity analysis of BF), 
+        we can define one and sent it to the function.
+    :ref_val: int, default is 0
+        Reference value for BF testing
+    :xlim: numpy.array, optional
+        limit the x axis (might be used for visualization porpuses sometimes)
 
-    :idata: The "trace" of model, after sampling the
-    :var_name: [str] Name of variable we want to test.
-    :prior: In case we want to use diffent prior (for sensitivity analysis of BF), we can define one and sent it to the function.
-    :family: for now, supports only the normal distribution.
-    :ref_val: reference value for BF testing
-    :xlim: limit the x axis (might be used for visualization porpuses sometimes)
+    Returns
+    -------
+    Bayes Factor 10, BF01 and ax
 
-    # grab trace, a variable name to compute difference and prior.
-    # ref_val is the parameter we want to compare
-    # test some elemtns
-    # varName should be string
-    if not isinstance(var_name, str):
-        print('varName is not a string')
-    # BFs based on density estimation (using kernel smoothing instead of spline)
+    Examples
+    --------
+    TBD
+    
     """
+    if not isinstance(var_name, str):
+        _log.warning('varName is not a string')
     post = extract(idata, var_names=var_name)
     if prior is None:
         # grab prior from the data in case it wasn't defined by the user
         prior = extract(idata, var_names=var_name, group="prior")
     if post.ndim > 1:
-        print("Posterior distribution has {post.ndim} dimensions")
+        _log.info("Posterior distribution has {post.ndim} dimensions")
     # generate vector
     if xlim is None:
         x = np.linspace(np.min(prior), np.max(prior),prior.shape[0])
     else:
         x = np.linspace(xlim[0], xlim[1],prior.shape[0])
-    #x = np.linspace(np.min(post), np.max(post),prior.shape[0])
     my_pdf = stats.gaussian_kde(post)
     prior_pdf = stats.gaussian_kde(prior)
     if ax is None:
@@ -48,17 +65,17 @@ def plot_bf(idata, var_name, prior, family = 'normal',  ref_val=0, xlim=None, ax
     )  # distribution function
     ax.plot(x, prior_pdf(x), "r-", lw=2.5, alpha=0.6, label="Prior")
     if ref_val>np.max(post) | ref_val<np.min(post):
-        print('Reference value is out of bounds of posterior')
+        _log.warning('Reference value is out of bounds of posterior')
     else:
         posterior = my_pdf(ref_val) # this gives the pdf at ref_val
     prior = prior_pdf(ref_val)
     BF10 = posterior / prior
     BF01 = prior / posterior
-    _log.warning("the Bayes Factor 10 is %.3f" % (BF10)"
+    _log.info("the Bayes Factor 10 is %.3f" % (BF10)"
                 ("the Bayes Factor 01 is %.3f" % (BF01)
     )
-    ax.plot(ref_val, posterior, "ko", lw=1.5, alpha=1)
-    ax.plot(ref_val, prior, "ko", lw=1.5, alpha=1)
+    ax.plot(ref_val, posterior, "ko", lw=1.5)
+    ax.plot(ref_val, prior, "ko", lw=1.5)
     ax.set_xlabel(var_name)
     ax.set_ylabel("Density")
     plt.legend()
