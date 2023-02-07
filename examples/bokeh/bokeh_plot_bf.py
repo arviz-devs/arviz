88d0d255da94b3aba4795c992a38ae598b366908
Asael A Matamoros <asael_am@hotmail.com>
2023-02-07 14:48:36 -0600

    Add Bayes Factors plots to the website
    Adding Bayes Factor plots for the current website.

---
+++ examples/bokeh/bokeh_plot_bf.py
@@ -0,0 +1,12 @@
+"""
+Baye Factor Plot
+=====================
+"""
+import numpy as np
+import arviz as az
+
+idata = az.from_dict(posterior={"a":np.random.normal(1, 0.5, 5000)},
+                     prior={"a":np.random.normal(0, 1, 5000)})
+
+az.plot_bf(idata, var_name="a", ref_val=0,backend="bokeh")
+
