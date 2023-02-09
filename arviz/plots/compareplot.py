20f015a353c10a0da42b815249651f3e38391a41
Asael A Matamoros <asael_am@hotmail.com>
2023-02-09 17:28:08 -0600

    correct reference
    Correct reference  in plot_compare()

--- arviz/plots/compareplot.py
+++ arviz/plots/compareplot.py
@@ -89,6 +89,7 @@ def plot_compare(
     ----------
     .. [1] Vehtari et al. (2016). Practical Bayesian model evaluation using leave-one-out
     cross-validation and WAIC https://arxiv.org/abs/1507.04544
+
     .. [2] McElreath R. (2022). Statistical Rethinking A Bayesian Course with Examples in
     R and Stan, Second edition, CRC Press.
 
