39879b12e77dd8f968e7ab09e8b2f62feccfe5a0
Asael A Matamoros <asael_am@hotmail.com>
2023-02-15 14:58:14 -0600

    Update densityplot.py

--- arviz/plots/densityplot.py
+++ arviz/plots/densityplot.py
@@ -58,7 +58,7 @@ def plot_density(
     var_names : list of str, optional
         List of variables to plot. If multiple datasets are supplied and `var_names` is not None,
         will print the same set of variables for each dataset. Defaults to None, which results in
-        all the variables being plotted. See :ref:`this section <common_var_names>` for usage examples.
+        all the variables being plotted.
     filter_vars : {None, "like", "regex"}, default None
         If `None` (default), interpret `var_names` as the real variables names. If "like",
         interpret `var_names` as substrings of the real variables names. If "regex",
