d51333997508294c1c399b0c9600a920f94e54ba
Asael A Matamoros <asael_am@hotmail.com>
2023-02-17 07:43:59 -0600

    Update forestplot.py
    remove the forest plot with ropes examples from the `plot_forest()` documentation

--- arviz/plots/forestplot.py
+++ arviz/plots/forestplot.py
@@ -182,20 +182,6 @@ def plot_forest(
         >>>                            figsize=(9, 7))
         >>> axes[0].set_title('Estimated theta for 8 schools models')
 
-    Forestplot with ropes
-
-    .. plot::
-        :context: close-figs
-
-        >>> rope = {'theta': [{'school': 'Choate', 'rope': (2, 4)}], 'mu': [{'rope': (-2, 2)}]}
-        >>> axes = az.plot_forest(non_centered_data,
-        >>>                            rope=rope,
-        >>>                            var_names='~tau',
-        >>>                            combined=True,
-        >>>                            figsize=(9, 7))
-        >>> axes[0].set_title('Estimated theta for 8 schools model')
-
-
     Ridgeplot
 
     .. plot::
