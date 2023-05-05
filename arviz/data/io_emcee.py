1538b1f94bdac2abb42ce34f1ed0b55f95122b73
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2023-05-05 10:37:07 +0300

    debug emcee behaviour

--- arviz/data/io_emcee.py
+++ arviz/data/io_emcee.py
@@ -191,7 +191,10 @@ class EmceeConverter:
             if int(self.emcee.__version__[0]) >= 3:
                 blobs = self.sampler.get_blobs()
             else:
-                blobs = np.array(self.sampler.blobs)
+                # debug print
+                print(np.array(self.sampler.blobs, dtype=object), flush=True)
+                print(np.array(self.sampler.blobs), flush=True)
+                blobs = np.array(self.sampler.blobs, dtype=object)
             if (blobs is None or blobs.size == 0) and self.blob_names:
                 raise ValueError("No blobs in sampler, blob_names must be None")
             if len(blobs.shape) == 2:
