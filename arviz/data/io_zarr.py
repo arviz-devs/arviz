6b9dd72c491e0f4528010a4a0a9918e848d3155b
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2023-07-11 22:01:34 +0200

    update PR with review comments

--- arviz/data/io_zarr.py
+++ arviz/data/io_zarr.py
@@ -13,27 +13,28 @@ from_zarr.__doc__ = InferenceData.from_zarr.__doc__
 
 def to_zarr(data, store=None, **kwargs):
     """
-    Convert data to zarr, optionally saving to disk if `store` is provided.
+    Convert data to zarr, optionally saving to disk if ``store`` is provided.
 
     The zarr storage is using the same group names as the InferenceData.
 
-    Raises
-    ------
-    TypeError
-        If no valid store is found.
-
     Parameters
     ----------
-    store : zarr.storage i.e MutableMapping or str, optional
+    store : zarr.storage, MutableMapping or str, optional
         Zarr storage class or path to desired DirectoryStore.
         Default (None) a store is created in a temporary directory.
-    **kwargs :
+    **kwargs : dict, optional
         Passed to :py:func:`convert_to_inference_data`.
 
     Returns
     -------
     zarr.hierarchy.group
         A zarr hierarchy group containing the InferenceData.
+
+    Raises
+    ------
+    TypeError
+        If no valid store is found.
+
 
     References
     ----------
