b2791c7cacf733e3d64648cc9be235fb10d6be3f
Oriol Abril-Pla <oriol.abril.pla@gmail.com>
2023-12-14 18:03:18 +0100

    fix stan model (#2297)

--- arviz/tests/helpers.py
+++ arviz/tests/helpers.py
@@ -432,18 +432,18 @@ def pystan_noncentered_schools(data, draws, chains):
     schools_code = """
         data {
             int<lower=0> J;
-            real y[J];
-            real<lower=0> sigma[J];
+            array[J] real y;
+            array[J] real<lower=0> sigma;
         }
 
         parameters {
             real mu;
             real<lower=0> tau;
-            real eta[J];
+            array[J] real eta;
         }
 
         transformed parameters {
-            real theta[J];
+            array[J] real theta;
             for (j in 1:J)
                 theta[j] = mu + tau * eta[j];
         }
@@ -456,8 +456,8 @@ def pystan_noncentered_schools(data, draws, chains):
         }
 
         generated quantities {
-            vector[J] log_lik;
-            vector[J] y_hat;
+            array[J] real log_lik;
+            array[J] real y_hat;
             for (j in 1:J) {
                 log_lik[j] = normal_lpdf(y[j] | theta[j], sigma[j]);
                 y_hat[j] = normal_rng(theta[j], sigma[j]);
