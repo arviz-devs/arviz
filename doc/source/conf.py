597c3076133d470a559ee0343dd736ca1c049a0e
Oriol (ProDesk) <oriol.abril.pla@gmail.com>
2023-12-21 20:07:34 +0100

    doc fixes
    add dataarray and dataset as type aliases
    fix css for homepage

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -102,12 +102,13 @@ numpydoc_xref_ignore = {
     "of", "or", "optional", "default", "1D", "2D", "3D", "n-dimensional", "M", "N", "K",
 }
 numpydoc_xref_aliases = {
+    "DataArray": ":class:`~xarray.DataArray`",
+    "Dataset": ":class:`~xarray.Dataset`",
     "Labeller": ":ref:`Labeller <labeller_api>`",
     "ndarray": ":class:`~numpy.ndarray`",
     "InferenceData": ":class:`~arviz.InferenceData`",
     "matplotlib_axes": ":class:`matplotlib Axes <matplotlib.axes.Axes>`",
     "bokeh_figure": ":class:`Bokeh Figure <bokeh.plotting.figure>`",
-
 }
 
 # The base toctree document.
