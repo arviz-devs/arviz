aece323c8f58345a501f97139fef3aa2e7d481cf
Oriol (ProDesk) <oriol.abril.pla@gmail.com>
2024-03-11 17:48:34 +0100

    remove conditional_jit from plot_forest label_idx

--- arviz/plots/backends/bokeh/forestplot.py
+++ arviz/plots/backends/bokeh/forestplot.py
@@ -15,7 +15,6 @@ from ....rcparams import rcParams
 from ....stats import hdi
 from ....stats.density_utils import get_bins, histogram, kde
 from ....stats.diagnostics import _ess, _rhat
-from ....utils import conditional_jit
 from ...plot_utils import _scale_fig_size
 from .. import show_layout
 from . import backend_kwarg_defaults
@@ -277,7 +276,6 @@ class PlotHandler:
         """Collect labels and ticks from plotters."""
         val = self.plotters.values()
 
-        @conditional_jit(forceobj=True, nopython=False)
         def label_idxs():
             labels, idxs = [], []
             for plotter in val:
--- arviz/plots/backends/matplotlib/forestplot.py
+++ arviz/plots/backends/matplotlib/forestplot.py
@@ -11,7 +11,6 @@ from ....stats import hdi
 from ....stats.density_utils import get_bins, histogram, kde
 from ....stats.diagnostics import _ess, _rhat
 from ....sel_utils import xarray_var_iter
-from ....utils import conditional_jit
 from ...plot_utils import _scale_fig_size
 from . import backend_kwarg_defaults, backend_show
 
@@ -236,7 +235,6 @@ class PlotHandler:
         """Collect labels and ticks from plotters."""
         val = self.plotters.values()
 
-        @conditional_jit(forceobj=True, nopython=False)
         def label_idxs():
             labels, idxs = [], []
             for plotter in val:
