f47afb2cdbe529c5ded1ebe6e29d31f73608998d
aloctavodia <aloctavodia@gmail.com>
2025-03-07 08:16:31 +0200

    changes after release

--- arviz/__init__.py
+++ arviz/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wildcard-import,invalid-name,wrong-import-position
 """ArviZ is a library for exploratory analysis of Bayesian models."""
-__version__ = "0.21.0"
+__version__ = "0.22.0dev"
 
 import logging
 import os
