28edbdf4dce0e08668224b0b5f1844ef26ed3b7d
Oriol (ProDesk) <oriol.abril.pla@gmail.com>
2025-12-09 19:13:32 +0100

    check matching versions for arviz-xyz

--- src/arviz/__init__.py
+++ src/arviz/__init__.py
@@ -2,6 +2,7 @@
 """Expose features from _ArviZverse_ refactored packages together in the ``arviz`` namespace."""
 
 import logging
+import re
 
 _log = logging.getLogger(__name__)
 
@@ -16,12 +17,8 @@ try:
         "exposing its functions as part of the `arviz` namespace"
     )
     _log.info(_status)
-except ModuleNotFoundError:
-    _status = "arviz_base not installed"
-    _log.exception(_status)
-except ImportError:
-    _status = "Unable to import arviz_base"
-    _log.exception(_status)
+except ModuleNotFoundError as err:
+    raise ImportError("arviz's dependency arviz_base is not installed", name="arviz") from err
 
 info += _status + "\n"
 
@@ -33,17 +30,15 @@ try:
     import arviz_base as base
     import arviz_stats as stats
 
+    # TODO: remove patch. 0.7 version of arviz-stats didn't expose the __version__ attribute
     _status = (
-        f"arviz_stats {getattr(stats, '__version__', '0.0')} available, "
+        f"arviz_stats {getattr(stats, '__version__', '0.7.0')} available, "
         "exposing its functions as part of the `arviz` namespace"
     )
     _log.info(_status)
-except ModuleNotFoundError:
-    _status = "arviz_stats not installed"
-    _log.exception(_status)
-except ImportError:
-    _status = "Unable to import arviz_stats"
-    _log.exception(_status)
+except ModuleNotFoundError as err:
+    raise ImportError("arviz's dependency arviz_stats is not installed", name="arviz") from err
+
 info += _status + "\n"
 
 try:
@@ -55,12 +50,8 @@ try:
         "exposing its functions as part of the `arviz` namespace"
     )
     _log.info(_status)
-except ModuleNotFoundError:
-    _status = "arviz_plots not installed"
-    _log.exception(_status)
-except ImportError:
-    _status = "Unable to import arviz_plots"
-    _log.exception(_status)
+except ModuleNotFoundError as err:
+    raise ImportError("arviz's dependency arviz_plots is not installed", name="arviz") from err
 
 info += _status
 
@@ -69,5 +60,13 @@ __version__ = "1.0.0.dev0"
 
 info = f"Status information for ArviZ {__version__}\n\n{info}"
 
+pat = re.compile(r"arviz_(base|stats|plots)\s([0-9]+\.[0-9]+)")
+matches = pat.findall(info)
+if any(matches[0][1] != match[1] for match in matches[1:]):
+    raise ImportError(
+        "Versions of arviz-xyz packages don't match to the minor version. "
+        f"The versions found are: {matches}"
+    )
+
 # clean namespace
-del logging, _status
+del logging, matches, pat, re, _status
